db6362712573293f8e97b07c79bafc67085ada62
Farangiz17 <farangizismatova24@gmail.com>
2023-08-23 15:58:17 +0500

    kirish

--- components/partials/account/Login.jsx
+++ components/partials/account/Login.jsx
@@ -139,7 +139,7 @@ class Login extends Component {
                                     <button
                                         type="submit"
                                         className="ps-btn ps-btn--fullwidth">
-                                        Login
+                                       Kirish
                                     </button>
                                 </div>
 
