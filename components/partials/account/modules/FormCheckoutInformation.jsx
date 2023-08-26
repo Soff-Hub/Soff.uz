90b22a8442b894e59e1e6de6d9a11d6893d220a4
Farangiz17 <farangizismatova24@gmail.com>
2023-08-26 19:05:03 +0500

    click page add

--- components/partials/account/modules/FormCheckoutInformation.jsx
+++ components/partials/account/modules/FormCheckoutInformation.jsx
@@ -75,6 +75,7 @@ function FormCheckoutInformation() {
         }
         return ids;
 
+
     }
     const ids = extractIds(data);
     console.log(
--- components/shared/footers/modules/FooterWidgets.jsx
+++ components/shared/footers/modules/FooterWidgets.jsx
@@ -112,4 +112,5 @@ const FooterWidgets = () => {
 
 
 
+
 export default FooterWidgets;
