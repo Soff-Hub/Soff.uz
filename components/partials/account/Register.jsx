c1b949b41da245167a6fcf6041a85a08d24c7fc2
Farangiz17 <farangizismatova24@gmail.com>
2023-08-28 15:57:59 +0500

    user data redux save

--- components/partials/account/Register.jsx
+++ components/partials/account/Register.jsx
@@ -41,7 +41,7 @@ class Register extends Component {
             }
     };
 
-
+    
     render() {
         return (
             <div className="ps-my-account">
--- repositories/PostRepository.js
+++ repositories/PostRepository.js
@@ -90,7 +90,6 @@ class PostRepository {
     async getCartData(header) {
         const endPoint = 'customer/cart-list/';
         const response = await Repository.get(baseUrl + endPoint, header)
-
             .then((response) => {
                 return response.data;
             })
