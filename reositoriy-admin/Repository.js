df35ccb04ef2646f25abe9b6a6e77ad51dce22b1
Ozodbek Abdisamatov <[abdisamatovozodbek003@gmail.com]>
2023-08-28 15:55:28 +0500

    Chiiq

--- reositoriy-admin/Repository.js
+++ reositoriy-admin/Repository.js
@@ -7,15 +7,8 @@ export const baseUrl = `${baseDomain}`;
 export default axios.create({
     baseUrl,
 
-    // headers: {
-    //     "Authorization":`Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjk0MTgwMDk1LCJpYXQiOjE2OTMzMTYwOTUsImp0aSI6Ijg4YzgyZTViMDk5ODRhY2VhNmU4MGYyMzc4MTZjNmZkIiwidXNlcl9pZCI6NTV9.nOxHOHJgnw1DwJ5SpJ4nXT0dPCcDF2Mkdu8xAOpIcQE`
-    // }
-
-
-
-
-
-
-
+    headers: {
+        "Authorization":`Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjk0MDgzOTUzLCJpYXQiOjE2OTMyMTk5NTMsImp0aSI6IjNkYjAxNDA1NGJkNDRhMDZiYWRmOTM0YjdiNTI0OWVjIiwidXNlcl9pZCI6OTh9.A2bpoAH4MuDls_5pzic4uezdFUK7JlkrxBBZcAGAlAU`
+    }
     
 });
