1ee37c8bc4ca41cd1ba50b133d4a2e6a3e866704
Ozodbek Abdisamatov <[abdisamatovozodbek003@gmail.com]>
2023-08-29 16:39:05 +0500

    Sellerdagi o'zgarishlar

--- reositoriy-admin/Repository.js
+++ reositoriy-admin/Repository.js
@@ -8,7 +8,7 @@ export default axios.create({
     baseUrl,
     headers: {
 
-        "Authorization":`Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjk0MDgzOTUzLCJpYXQiOjE2OTMyMTk5NTMsImp0aSI6IjNkYjAxNDA1NGJkNDRhMDZiYWRmOTM0YjdiNTI0OWVjIiwidXNlcl9pZCI6OTh9.A2bpoAH4MuDls_5pzic4uezdFUK7JlkrxBBZcAGAlAU`
+        "Authorization":`Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjk0MDkwNDAxLCJpYXQiOjE2OTMyMjY0MDEsImp0aSI6ImIxZGE4MTUwNjZkNTQ1M2Y5YzIyYzNkYzhjOGYxZGVkIiwidXNlcl9pZCI6OTh9.FA1PO-FzORGRY2a4vrqEY9sn7blGBIndSTiBAgf3BDo`
     }
     
 });
