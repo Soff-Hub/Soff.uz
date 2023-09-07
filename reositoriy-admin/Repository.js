e348c6d0f5fb57f1db503a31c5c58ce56341c899
Ozodbek Abdisamatov <[abdisamatovozodbek003@gmail.com]>
2023-09-07 18:42:12 +0500

    BaseUrl

--- reositoriy-admin/Repository.js
+++ reositoriy-admin/Repository.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const baseDomain = 'http://allldataaa.pythonanywhere.com/api/v1/seller/'; 
+const baseDomain = 'https://allldaataaa.pythonanywhere.com/api/v1/seller/'; 
 export const baseUrlCustomer = 'https://allldaataaa.pythonanywhere.com/api/v1/'
 
 
@@ -7,9 +7,4 @@ export const baseUrl = `${baseDomain}`;
 
 export default axios.create({
     baseUrl,
-
-    headers: {
-        "Authorization":`Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjk0MDkwNDAxLCJpYXQiOjE2OTMyMjY0MDEsImp0aSI6ImIxZGE4MTUwNjZkNTQ1M2Y5YzIyYzNkYzhjOGYxZGVkIiwidXNlcl9pZCI6OTh9.FA1PO-FzORGRY2a4vrqEY9sn7blGBIndSTiBAgf3BDo`
-    }
-    
 });
