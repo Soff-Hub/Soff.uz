b1231a75b09c1f9cd96f011d991f061da4718880
Ozodbek Abdisamatov <[abdisamatovozodbek003@gmail.com]>
2023-09-08 11:51:24 +0500

    BaseUrl2

--- reositoriy-admin/GetRepository.js
+++ reositoriy-admin/GetRepository.js
@@ -285,7 +285,7 @@ class GetRepository {
     }
     async getProfile(token) {
         const reponse = await Repository({
-            url: "https://allldaataaa.pythonanywhere.com/auth/profile/",
+            url: "https://alldataaa.pythonanywhere.com/auth/profile/",
             method: 'GET',
             headers: {
                 Authorization: `Bearer ${token}`,
--- reositoriy-admin/Repository.js
+++ reositoriy-admin/Repository.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-const baseDomain = 'https://allldaataaa.pythonanywhere.com/api/v1/seller/'; 
-export const baseUrlCustomer = 'https://allldaataaa.pythonanywhere.com/api/v1/'
+const baseDomain = 'https://alldataaa.pythonanywhere.com/api/v1/seller/'; 
+export const baseUrlCustomer = 'https://alldataaa.pythonanywhere.com/api/v1/'
 
 
 export const baseUrl = `${baseDomain}`;
