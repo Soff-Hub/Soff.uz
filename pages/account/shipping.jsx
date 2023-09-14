66f41a9c31fda59c3df9338d8a0116bc40e095f4
Ozodbek Abdisamatov <[abdisamatovozodbek003@gmail.com]>
2023-09-14 14:47:30 +0500

    Edit products

--- pages/account/shipping.jsx
+++ pages/account/shipping.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 import { connect } from 'react-redux';
 import BreadCrumb from '~/components/elements/BreadCrumb';
-import Shipping from '~/components/partials/account/Shipping';
 import PageContainer from '~/components/layouts/PageContainer';
 import FooterDefault from '~/components/shared/footers/FooterDefault';
 import Newletters from '~/components/partials/commons/Newletters';
@@ -31,7 +30,6 @@ const ShippingPage = () => {
             <PageContainer footer={<FooterDefault />} title="Shipping">
                 <div className="ps-page--simple">
                     <BreadCrumb breacrumb={breadCrumb} />
-                    <Shipping />
                 </div>
                 <Newletters layout="container" />
             </PageContainer>
