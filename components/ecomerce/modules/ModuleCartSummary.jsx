7f6021b9a9b35f46139657949aedbeaaf9c673ab
Farangiz17 <farangizismatova24@gmail.com>
2023-09-22 20:00:23 +0500

    fixed price

--- components/ecomerce/modules/ModuleCartSummary.jsx
+++ components/ecomerce/modules/ModuleCartSummary.jsx
@@ -25,19 +25,23 @@ const ModuleCartSummary = ({ source }) => {
     // View
     let productItemsView;
     if (source && source.length > 0) {
-
         productItemsView = source.map((item, i) => (
             <li key={item.id}>
-                <div className="ps-block__estimate " style={{display:'flex', aliginContent:'center', justifyContent:'space-between'}}>
-                    <Link href="/product/[pid]" as={`/product/${item.id}`}>
+                <div
+                    className="ps-block__estimate "
+                    style={{
+                        display: 'flex',
+                        aliginContent: 'center',
+                        justifyContent: 'space-between',
+                    }}>
+                    <Link style={{width:'70%'}} href="/product/[pid]" as={`/product/${item.id}`}>
                         <a className="ps-product__title">
                             {i + 1}. {item.title}
                         </a>
-                        
                     </Link>
                     <Link href="/product/[pid]" as={`/product/${item.id}`}>
                         <a className="ps-product__price">
-                           {addPeriodToThousands(item.price)} so'm
+                            {addPeriodToThousands(item.price)} so'm
                         </a>
                     </Link>
                 </div>
@@ -56,7 +60,7 @@ const ModuleCartSummary = ({ source }) => {
                 <div className="ps-block__content">
                     <ul className="ps-block__product">{productItemsView}</ul>
                     <h3>
-                        Jami:  <span>{hisob} so'm</span>
+                        Jami: <span>{hisob} so'm</span>
                     </h3>
                 </div>
             </div>
