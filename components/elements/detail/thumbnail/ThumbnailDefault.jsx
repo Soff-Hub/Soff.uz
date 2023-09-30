3909e463d899c4714d272a023af661b1efd4038c
Zufarbek <zufarbek.abdurakhmonov@gmail.com>
2023-09-30 20:40:09 +0500

    product image view fixed

--- components/elements/detail/thumbnail/ThumbnailDefault.jsx
+++ components/elements/detail/thumbnail/ThumbnailDefault.jsx
@@ -16,22 +16,6 @@ const ThumbnailDefault = ({ product, vertical = true }) => {
         <div
             className="ps-product__thumbnail"
             data-vertical={vertical ? 'true' : 'false'}>
-
-            <figure >
-                <div className="ps-wrapper" >
-                    {
-                        product?.iamges  ?
-                        product?.iamges?.map(item=>(
-                            <img src={item.image_url} alt="doc" className='border mb-3 img-fluid' />
-                        )) 
-                            
-                        :
-                        <img src="/static/img/docCopy.jpg" alt="doc" />
-                       
-
-                    }
-                </div>
-            </figure>
         </div>
     );
 };
