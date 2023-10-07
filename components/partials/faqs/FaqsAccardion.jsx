32919ea36bd4c6cbadb35cb9ccd6829e54f01357
Farangiz17 <farangizismatova24@gmail.com>
2023-10-07 14:45:15 +0500

    finish SEO

--- components/partials/faqs/FaqsAccardion.jsx
+++ components/partials/faqs/FaqsAccardion.jsx
@@ -42,69 +42,41 @@ const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
     borderTop: '1px solid rgba(0, 0, 0, .125)',
 }));
 
-export default function FaqsAccardion(data) {
+export default function FaqsAccardion() {
     const [expanded, setExpanded] = React.useState('panel1');
 
     const handleChange = (panel) => (event, newExpanded) => {
         setExpanded(newExpanded ? panel : false);
     };
-
     return (
         <div>
-            {/* <Accordion
-                expanded={expanded === 'panel1'}
-                onChange={handleChange('panel1')}>
-                <AccordionSummary
-                    aria-controls="panel1d-content"
-                    id="panel1d-header">
-                    <Typography className='acc-title'>Collapsible Group Item #1</Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                    <Typography className='acc-desc'>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Suspendisse malesuada lacus ex, sit amet blandit leo
-                        lobortis eget. Lorem ipsum dolor sit amet, consectetur
-                        adipiscing elit. Suspendisse malesuada lacus ex, sit
-                        amet blandit leo lobortis eget.
-                    </Typography>
-                </AccordionDetails>
-            </Accordion> */}
-            <Accordion
-                expanded={expanded === 'panel2'}
-                onChange={handleChange('panel2')}>
-                <AccordionSummary
-                    aria-controls="panel2d-content"
-                    id="panel2d-header">
-                    <Typography className='acc-title'>Collapsible Group Item #2</Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                    <Typography className='acc-desc'>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Suspendisse malesuada lacus ex, sit amet blandit leo
-                        lobortis eget. Lorem ipsum dolor sit amet, consectetur
-                        adipiscing elit. Suspendisse malesuada lacus ex, sit
-                        amet blandit leo lobortis eget.
-                    </Typography>
-                </AccordionDetails>
-            </Accordion>
-            <Accordion
-                expanded={expanded === 'panel3'}
-                onChange={handleChange('panel3')}>
-                <AccordionSummary
-                    aria-controls="panel3d-content"
-                    id="panel3d-header">
-                    <Typography className='acc-title'>Collapsible Group Item #3</Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                    <Typography className='acc-desc'>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Suspendisse malesuada lacus ex, sit amet blandit leo
-                        lobortis eget. Lorem ipsum dolor sit amet, consectetur
-                        adipiscing elit. Suspendisse malesuada lacus ex, sit
-                        amet blandit leo lobortis eget.
-                    </Typography>
-                </AccordionDetails>
-            </Accordion>
+            {/* {data &&
+                data.map((item) => {
+                    return (
+                        <Accordion
+                        key={item}
+                            expanded={expanded === 'panel2'}
+                            onChange={handleChange('panel2')}>
+                            <AccordionSummary
+                                aria-controls="panel2d-content"
+                                id="panel2d-header">
+                                <Typography className="acc-title">
+                                    Collapsible Group Item #2
+                                </Typography>
+                            </AccordionSummary>
+                            <AccordionDetails>
+                                <Typography className="acc-desc">
+                                    Lorem ipsum dolor sit amet, consectetur
+                                    adipiscing elit. Suspendisse malesuada lacus
+                                    ex, sit amet blandit leo lobortis eget.
+                                    Lorem ipsum dolor sit amet, consectetur
+                                    adipiscing elit. Suspendisse malesuada lacus
+                                    ex, sit amet blandit leo lobortis eget.
+                                </Typography>
+                            </AccordionDetails>
+                        </Accordion>
+                    );
+                })} */}
         </div>
     );
 }
--- components/partials/faqs/FaqsContent.jsx
+++ components/partials/faqs/FaqsContent.jsx
@@ -1,66 +1,56 @@
 import React, { useState } from 'react';
 import FaqsAccardion from './FaqsAccardion';
-import ProductRepository from '~/repositories/ProductRepository';
+import { useEffect } from 'react';
+import PostRepository from '~/repositories/PostRepository';
 
 const FaqsContent = () => {
-    const [data, setData] = useState(null)
+    const [categoryData, setCategoryData] = useState(null);
+    const [descriptionData, setDescriptionData] = useState(null);
 
-    const handleClick = () => {
-        console.log('ishladi');
-    }
+    const handleClick = () => {};
 
-    const getFAQCategorys = async () =>{
-        const respons = await ProductRepository.getFAQCategorys()
-    }
+    const getFAQCategorysData = async () => {
+        const respons = await PostRepository.getFAQCategorys();
+        if (respons) {
+            setCategoryData(respons?.results);
+        }
+    };
 
+    const getFAQDescriptionData = async (id) => {
+        const respons = await PostRepository.getFAQDescription(id);
+        if (respons) {
+            setDescriptionData(respons);
+        }
+    };
+
+    useEffect(() => {
+        getFAQCategorysData();
+        // getFAQDescriptionData()
+    }, []);
     return (
         <div>
             <div className="faqs-category">
                 <h3>Kategoriyalar bo'yicha savollar</h3>
                 <div className="row ">
-                    <div className="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-xs-4 my-3" onClick={() => handleClick()}>
-                        <div className="faqs-category__card">
-                            <i class="fa-regular fa-address-book faq-icon"></i>
-                            <span className="faq-category-name">
-                                Tizimdan ro'yxatdan o'tish
-                            </span>
-                        </div>
-                    </div>
-                    <div className="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-xs-4 my-3">
-                        <div className="faqs-category__card">
-                            <i class="fa-regular fa-address-book faq-icon"></i>
-                            <span className="faq-category-name">
-                                Tizimdan ro'yxatdan o'tish
-                            </span>
-                        </div>
-                    </div>
-                    <div className="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-xs-4 my-3">
-                        <div className="faqs-category__card">
-                            <i class="fa-regular fa-address-book faq-icon"></i>
-                            <span className="faq-category-name">
-                                Tizimdan ro'yxatdan o'tish
-                            </span>
-                        </div>
-                    </div>
-                    <div className="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-xs-4 my-3">
-                        <div className="faqs-category__card">
-                            <i class="fa-regular fa-address-book faq-icon"></i>
-                            <span className="faq-category-name">
-                                Tizimdan ro'yxatdan o'tish
-                            </span>
-                        </div>
-                    </div>
-                    <div className="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-xs-4 my-3">
-                        <div className="faqs-category__card">
-                            <i class="fa-regular fa-address-book faq-icon"></i>
-                            <span className="faq-category-name">
-                                Tizimdan ro'yxatdan o'tish
-                            </span>
-                        </div>
-                    </div>
+                    {categoryData?.length > 0 &&
+                        categoryData?.map((item) => {
+                            return (
+                                <div
+                                    key={item.id}
+                                    className="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-xs-4 my-3"
+                                    onClick={() => handleClick(item.id)}>
+                                    <div className="faqs-category__card">
+                                        <i class={`${item.icon} faq-icon`}></i>
+                                        <span className="faq-category-name">
+                                            {item.name}
+                                        </span>
+                                    </div>
+                                </div>
+                            );
+                        })}
                 </div>
             </div>
-            <FaqsAccardion  />
+            <FaqsAccardion data={descriptionData} />
         </div>
     );
 };
