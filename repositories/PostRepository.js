84f8c620d5dce89fb73ec9fa71f5af1770d032e1
Farangiz17 <farangizismatova24@gmail.com>
2023-10-07 13:05:15 +0500

    edited scss

--- repositories/PostRepository.js
+++ repositories/PostRepository.js
@@ -5,52 +5,6 @@ class PostRepository {
         this.callback = callback;
     }
 
-    // async getPosts(payload) {
-    //     const endPoint = `posts?${serializeQuery(payload)}`;
-    //     const reponse = await Repository.get(`${basePostUrl}/${endPoint}`)
-    //         .then((response) => {
-    //             if (response.data.length > 0) {
-    //                 return response.data;
-    //             } else {
-    //                 return null;
-    //             }
-    //         })
-    //         .catch((error) => ({ error: JSON.stringify(error) }));
-    //     return reponse;
-    // }
-
-    // async getPostBySlug(payload) {
-    //     const reponse = await Repository.get(
-    //         `${basePostUrl}/posts?slug=${payload}`
-    //     )
-    //         .then((response) => {
-    //             if (response.data.length > 0) {
-    //                 return response.data[0];
-    //             } else {
-    //                 return null;
-    //             }
-    //         })
-    //         .catch((error) => ({ error: JSON.stringify(error) }));
-    //     return reponse;
-    // }
-
-    // async getPostsByCollectionSlug(payload) {
-    //     const endPoint = `collections?${serializeQuery(payload)}`;
-    //     const reponse = await Repository.get(`${basePostUrl}/${endPoint}`)
-    //         .then((response) => {
-    //             if (response.data && response.data[0].posts.length > 0) {
-    //                 return response.data[0].posts;
-    //             } else {
-    //                 return null;
-    //             }
-    //         })
-    //         .catch((error) => {
-    //             console.log(JSON.stringify(error));
-    //             return null;
-    //         });
-    //     return reponse;
-    // }
-
     async getPostItemsByKeyword(payload) {
         const reponse = await Repository.get(
             `${basePostUrl}/posts?title_contains=${payload}`
@@ -98,11 +52,19 @@ class PostRepository {
         return reponse;
     }
 
-
     async getCartData(token) {
         const endPoint = 'customer/cart-list/';
 
         const response = await Repository.get(baseUrl + endPoint, token)
+            .then((response) => {
+                return response.data;
+            })
+            .catch((error) => ({ error: JSON.stringify(error) }));
+        return response;
+    }
+    async getFAQCategorys() {
+        const endPoint = 'customer/question-category/';
+        const response = await Repository.get(baseUrl + endPoint)
             .then((response) => {
                 return response.data;
             })
