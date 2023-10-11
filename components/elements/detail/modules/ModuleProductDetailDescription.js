7ca4039132311b7966c541498f4e40b487e7de63
Farangiz17 <farangizismatova24@gmail.com>
2023-10-11 16:03:35 +0500

    modal

--- components/elements/detail/modules/ModuleProductDetailDescription.js
+++ components/elements/detail/modules/ModuleProductDetailDescription.js
@@ -6,7 +6,7 @@ const ModuleProductDetailDescription = ({ product }) => (
         <ul className="ps-list--dot" style={{ listStyleType: 'revert' }}>
             <li>
                 {' '}
-                <strong>Betlar soni : </strong> {product?.document?.page_count}
+                <strong>Betlar soni : </strong> {product?.document?.page_count} ta
             </li>
             <li>
                 {' '}
