fb8f264f83aeacae1ee7093abdac8d14a2811aa3
Farangiz17 <farangizismatova24@gmail.com>
2023-12-26 19:42:58 +0500

    added dating terms

--- components/partials/account/Register.jsx
+++ components/partials/account/Register.jsx
@@ -147,7 +147,7 @@ class Register extends Component {
     componentDidMount() {
         if (this.props.url === 'auth/seller-register/') {
             this.setState({ role: 'seller' });
-        }else if (this.props.url === 'auth/register/') {
+        } else if (this.props.url === 'auth/register/') {
             this.setState({ role: 'customer' });
         }
     }
@@ -286,7 +286,9 @@ class Register extends Component {
                                                     Ro'yxatdan o'tish
                                                 </button>
                                             ) : (
-                                                <Tooltip className='ps-btn ps-btn--fullwidth' title="Ro'yxatdan o'tishingiz uchun tanishuv shartlariga rozilik bildirishingiz zarur">
+                                                <Tooltip
+                                                    className="ps-btn ps-btn--fullwidth"
+                                                    title="Ro'yxatdan o'tishingiz uchun tanishuv shartlariga rozilik bildirishingiz zarur">
                                                     <button
                                                         disabled={true}
                                                         style={{
@@ -388,10 +390,299 @@ class Register extends Component {
                     <ModalTanishuv
                         dataBsTarget="exampleModalToggleEditCategory2"
                         formID={'modal-tanishuv'}>
-                        <div className="container">
-                            Tanishuv shartlari...
-                            <br /> <br />
-                            Biz bilan o'z biznesingizni boshlang!!!
+                        <div className="modal-dating">
+                            <h3>
+                                Soff.uz platformasidan foydalanish
+                                qonun-qoidalari
+                            </h3>
+                            <h4>1. Umumiy qoidalar</h4>
+                            <p>
+                                1.1. Siz Soff.uz platformasiga tashrif buyurar
+                                ekansiz quyida belgilangan qonun-qoidalar va
+                                shartlarga o’z roziligingizni bildirgan
+                                hisoblanasiz.
+                            </p>
+                            <p>
+                                1.2. Soff.uz platformasida (keyingi o’rinlarda
+                                «Platforma» deb yuritiladi) taqdim etilayotgan
+                                xizmatlar, biznes rejalar, ma’lumotlar,
+                                qo’llanmalar, referatlar, prezentatsiyalar va
+                                boshqalar shartli ravishda{' '}
+                                <strong>mahsulot</strong> deb nomlanadi. Ular
+                                faqatgina ma’lumotlar hisoblanib,
+                                o’qib-o’rganish uchun mo’ljallangan. Biznes
+                                rejalar esa tavsiyaviy harakterga ega bo’lib,
+                                undagi hisob-kitoblar tahminiy olingan.
+                            </p>
+                            <p>
+                                1.3. Platformaning «Biznes xizmatlar» bo’limida
+                                keltirilgan xizmatlar tomonlar o’rtasida
+                                tuzilgan, o’zaro tasdiqlangan shartnomalar
+                                asosida tartibga solinadi.
+                            </p>
+                            <h4>2. Foydalanuvchi bo’lib ro’yxatdan o’tish</h4>
+                            <p>
+                                Platformada mavjud mahsulotlarni xarid qilish,
+                                foydalanish va boshqalarni amalga oshirish uchun
+                                Platformadan «Foydalanuvchi» bo’lib ro’yxatdan
+                                o’tish lozim. Ro’yxatdan o’tish davomida
+                                foydalanuvchi barcha ma’lumotlar haqqoniy va
+                                to’g’ri to’ldirilishiga javobgar hisoblanadi.
+                            </p>
+                            <h4>3. Sotuvchi bo’lib ro’yxatdan o’tish</h4>
+                            <p>
+                                Platformada o’z mahsulotlarini sotish orqali
+                                daromad qilish va boshqalarni amalga oshirish
+                                uchun Platformadan «Sotuvchi» bo’lib ro’yxatdan
+                                o’tish lozim. Ro’yxatdan o’tish davomida
+                                sotuvchi barcha ma’lumotlar haqqoniy va to’g’ri
+                                to’ldirilishiga javobgar hisoblanadi.
+                            </p>
+                            <h4>
+                                4. Foydalanuvchilarning huquq va majburiyatlari
+                            </h4>
+                            <h5>
+                                4.1. Foydalanuvchilar quyidagi huquqlarga ega:
+                            </h5>
+                            <p>
+                                4.1.1. Platforma tizimida erkin ro’yxatdan
+                                o’tish;
+                            </p>
+                            <p>
+                                4.1.2. Platforma tomonidan taqdim etilayotgan
+                                xizmatlardan foydalanish;
+                            </p>
+                            <p>
+                                4.1.3. Xarid qilingan mahsulotlardan erkin
+                                foydalanish (mahsulotlardan tijorat hamda foyda
+                                olish maqsadida foydalanish taqiqlanadi);
+                            </p>
+                            <p>
+                                4.1.4. Xarid qilingan mahsulot bo’yicha
+                                xabarnomalarni ro’yxatdan o’tish davomida
+                                foydalanuvchi tomonidan kiritilgan elektron
+                                pochta (e-mail) orqali qabul qilib olish (4.2.4.
+                                shart bajarilgan taqdirda) yoki o’zining shaxsiy
+                                profilidagi «Sotib olingan» sahifasiga kirish
+                                orqali qabul qilib olish;
+                            </p>
+                            <p>
+                                4.1.5. Foydalanuvchi xarid qilingan mahsulotni
+                                yuklab olinish jarayonida muammo yuzaga kelgan
+                                holatda platforma ma’muriyatiga amalga
+                                oshirilgan to’lovni qaytarib berish bo’yicha
+                                murjaat qilgan taqdirdagini pulini qaytarib
+                                olish;
+                            </p>
+                            <p>
+                                4.1.6. Platforma ma’muriyatiga murojaat qilish
+                                huquqlariga ega.
+                            </p>
+                            <h5>
+                                4.2. Foydalanuchilar quyidagi majburiyatlarni
+                                bajarishlari shart:
+                            </h5>
+                            <p>
+                                4.2.1. Foydalanuvchi Platforma tizimida
+                                ro’yxatdan o’tish davmida so’ralgan
+                                ma’lumotlarni to’g’ri kiritishi shart;
+                            </p>
+                            <p>
+                                4.2.2. Platforma foydalanuvchilari o’zaro
+                                aloqalar davomida beadab, haqoratli va behurmat
+                                qiladigan jumlalardan foydalanish hamda
+                                O’zbekiston Respublikasining amaldagi
+                                qonunchiligida taqiqlangan boshqa ma’lumotlarni
+                                o’z ichiga oluvchi xabarlarni jo’natish
+                                taqiqlanadi.
+                            </p>
+                            <p>
+                                4.2.4. Ro’yxatdan o’tish jarayonida amalda
+                                mavjud bo’lgan, ishlaydigan elektron manzil
+                                (e-mail) kiritish shart;
+                            </p>
+                            <p>
+                                4.2.5. Foydalanuvchi Platformaning foydalanish
+                                qonun-qoidalarida belgilangan talablarni to’liq
+                                bajarishi shart.
+                            </p>
+                            <h4>5. Sotuvchining huquq va majburiyatlari</h4>
+                            <h5>5.1. Sotuvchilar quyidagi huquqlarga ega:</h5>
+                            <p>
+                                5.1.1. Platforma tizimida erkin ro’yxatdan
+                                o’tish;
+                            </p>
+                            <p>
+                                5.1.2. Platforma tomonidan taqdim etilayotgan
+                                xizmatlarga o’z mahsulotlarini qo’shish;
+                            </p>
+                            <p>
+                                5.1.3. Mahsulotlaridan bepulga yoki tijorat
+                                hamda foyda olish maqsadida platformada
+                                ulashishlariga ruxsat etiladi (5.1.7. shart
+                                bajarilgan taqdirda);
+                            </p>
+                            <p>
+                                5.1.4. Sotuvchinig sotilgan har bir mahsulotlari
+                                bo’yicha xabarnomalarni ro’yxatdan o’tish
+                                davomida foydalanuvchi tomonidan kiritilgan
+                                elektron pochta (e-mail) orqali qabul qilib
+                                olish (4.2.4. shart bajarilgan taqdirda) yoki
+                                o’zining shaxsiy profilidagi «Buyurtmalar»
+                                sahifasiga kirish orqali qabul qilib olish;
+                            </p>
+                            <p>
+                                5.1.5. Sotuvchining sotilgan mahsulotlari
+                                yig’indisidan platforma ulishining (sotilgan
+                                mahsulotning 10% i) qiymatini ayirgan holatda
+                                hosil bo’lgan summani o’z kartalariga tashlab
+                                olish (5.2.5. shart bajarilgan taqdirda);
+                            </p>
+                            <p>5.1.6.</p>
+                            <p>
+                                5.1.7. Sotuvchi mahsulotlarining muallifi o’zi
+                                yoki boshqa bir inson muallifidagi mahsulot
+                                bo’lsa uning roziligi bilan olingan mahsulot
+                                bo’lishi shart;
+                            </p>
+                            <p>
+                                5.1.7. Platforma ma’muriyatiga murojaat qilish
+                                huquqlariga ega
+                            </p>
+                            <h5>
+                                5.2. Foydalanuchilar quyidagi majburiyatlarni
+                                bajarishlari shart:
+                            </h5>
+                            <p>
+                                5.2.1. Foydalanuvchi Platforma tizimida
+                                ro’yxatdan o’tish davmida so’ralgan
+                                ma’lumotlarni to’g’ri kiritishi shart;
+                            </p>
+                            <p>
+                                5.2.2. Platforma sotuvchilari o’zaro aloqalar
+                                davomida beadab, haqoratli va behurmat qiladigan
+                                jumlalardan foydalanish hamda O’zbekiston
+                                Respublikasining amaldagi qonunchiligida
+                                taqiqlangan boshqa ma’lumotlarni o’z ichiga
+                                oluvchi xabarlarni jo’natish taqiqlanadi.
+                            </p>
+                            <p>
+                                5.2.4. Ro’yxatdan o’tish jarayonida amalda
+                                mavjud bo’lgan, ishlaydigan elektron manzil
+                                (e-mail) kiritish shart;
+                            </p>
+                            <p>
+                                5.2.5. Sotuvchining yig’ilgan summasini
+                                kartasiga tashlab olish jarayonidan uning
+                                miqdori 10 ming so’mdan kam bo’lmasligi shart.
+                            </p>
+                            <p>
+                                5.2.5. Sotuvchi Platformaning foydalanish
+                                qonun-qoidalarida belgilangan talablarni to’liq
+                                bajarishi shart
+                            </p>
+                            <p>
+                                5.2.6. Sotuvchi platformada sotilgan har bir
+                                mahsulotining 10% ini platforma ma’muriyati olib
+                                qolishi shart.
+                            </p>
+                            <h4>6. Javobgarlik</h4>
+                            <p>
+                                6.1. Platforma ma’muriyati Foydalanuvchining
+                                noto’g’ri ma’lumot kiritishi va amallarni
+                                bajarishi sababli xarid qilishi jarayonida
+                                yuzaga kelgan zarar, ziyon yoki xarajatlar uchun
+                                javobgar emas.
+                            </p>
+                            <p>
+                                6.2. Foydalanuvchi tomonidan xarid qilingan
+                                mahsulot foydalanuvchining Platformadagi o’z
+                                shaxsiy kabinetiga joylashtiriladi hamda
+                                elektron manziliga xabarnoma yuboriladi. Taqdim
+                                etilgan mahsulot foydalanuvchi tomonidan yuklab
+                                olinish yoki olinmasligidan qat’iy nazar,
+                                mahsulot foydalanuvchi tomonidan xarid qilingan
+                                va Platforma tizimida yetkazib berildi deya
+                                belgilanadi hamda amalga oshirilgan summa
+                                qaytarilmaydi. Shu sababli ro’yxatdan o’tish
+                                davomi amalda mavjud bo’lgan, ishlaydigan
+                                elektron manzil (Email) kiriting hamda to’lovni
+                                muvaffaqiyatli amalga oshirib bo’lganingizdan
+                                so’ng e-mailingizni ham tekshirib turing.
+                            </p>
+                            <p>
+                                6.3. Platformada ro’yxatdan o’tish davomida
+                                kiritilgan ma’lumotlar haqqoniyligi uchun butun
+                                javobgarlik foydalanuvch hamda sotuvchining o’z
+                                bo’ynida bo’ladi.
+                            </p>
+                            <p>
+                                6.4. Platforma ma’muriyati to’lovni amalga
+                                oshirish jarayonida yuzaga kelgan zarar uchun
+                                javobgar emas.
+                            </p>
+                            <h4>
+                                7. Foydalanuvchining shaxsiy ma’lumotlar
+                                daxlsizligi
+                            </h4>
+                            <p>
+                                Foydalanuvchilarning shaxsiy ma’lumotlarining
+                                daxlsizligi biz uchun juda muhim.
+                            </p>
+                            <p>
+                                Biz quyidagi ma’lumotlarni statistik
+                                ko’rsatgichlarni aniqlash maqsadida to’playmiz
+                                va saqlaymiz:
+                            </p>
+                            <p>
+                                – foydalanuvchilar Saytga kirish uchun
+                                foydalangan IP-manzili;
+                            </p>
+                            <p>
+                                – Platformaga kirish uchun foydalanuvchi
+                                tomonidan ishlatilgan brauzer va operatsion
+                                tizim turi;
+                            </p>
+                            <p>– Platformaga kirish muddatlari;</p>
+                            <p>
+                                – foydalanuvchi ko’rib chiqqan sahifalar va
+                                havolalar;
+                            </p>
+                            <p>
+                                Biz ushbu manzillardan foydalanuvchining
+                                shaxsini aniqlash maqsadida foydalanmaymiz,
+                                Saytga qilingan hurujlar bundan mustasno.
+                            </p>
+                            <h4>
+                                8. Sotuvchining shaxsiy ma’lumotlar daxlsizligi
+                            </h4>
+                            <p>
+                                Sotuvchining shaxsiy ma’lumotlarining
+                                daxlsizligi biz uchun juda muhim.
+                            </p>
+                            <p>
+                                Biz quyidagi ma’lumotlarni statistik
+                                ko’rsatgichlarni aniqlash maqsadida to’playmiz
+                                va saqlaymiz:
+                            </p>
+                            <p>
+                                – sotuvchining Platformaga kirish uchun
+                                foydalangan IP-manzili;
+                            </p>
+                            <p>
+                                – Platformaga kirish uchun sotuvchi tomonidan
+                                ishlatilgan brauzer va operatsion tizim turi;
+                            </p>
+                            <p>– Platformaga kirish muddatlari;</p>
+                            <p>
+                                – sotuvchi ko’rib chiqqan sahifalar va
+                                havolalar;
+                            </p>
+                            <p>
+                                Biz ushbu manzillardan foydalanuvchining
+                                shaxsini aniqlash maqsadida foydalanmaymiz,
+                                Saytga qilingan hurujlar bundan mustasno.
+                            </p>
                         </div>
                     </ModalTanishuv>
                 </div>
