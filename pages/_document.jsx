3699b4545a4888914026cd65e457bd84ad9a0756
lazywk <edoniyorbekdev@gmail.com>
2024-01-08 13:03:15 +0500

    sasa

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -9,7 +9,7 @@ export default function Document() {
             <Head>
 
                 <link rel="shortcut icon" href={'/static/img/soff logo.png'} />
-                <link rel="icon" href={'/static/img/soff logo.png'}  sizes="32x32" />
+                <link rel="icon" href={'/static/img/soff logo.png'} sizes="32x32" />
                 <link
                     rel="icon"
                     href={'/static/img/soff logo.png'}
@@ -34,7 +34,33 @@ export default function Document() {
                 ></script>
                 <script
                     dangerouslySetInnerHTML={{
-                    __html: `
+                        __html: `
+                        (function(m, e, t, r, i, k, a) {
+                            m[i] = m[i] || function() {
+                                (m[i].a = m[i].a || []).push(arguments)
+                            }
+                            ;
+                            m[i].l = 1 * new Date();
+                            k = e.createElement(t),
+                            a = e.getElementsByTagName(t)[0],
+                            k.async = 1,
+                            k.src = r,
+                            a.parentNode.insertBefore(k, a)
+                        }
+                        )(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+            
+                        ym(88719185, "init", {
+                            clickmap: true,
+                            trackLinks: true,
+                            accurateTrackBounce: true,
+                            webvisor: true
+                        });
+                    `,
+                    }}
+                ></script>
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
                         window.dataLayer = window.dataLayer || [];
                         function gtag(){dataLayer.push(arguments);}
                         gtag('js', new Date());
@@ -44,7 +70,7 @@ export default function Document() {
                 ></script>
                 <script
                     dangerouslySetInnerHTML={{
-                    __html: `
+                        __html: `
                         (function(h,o,t,j,a,r){
                             h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
                             h._hjSettings={hjid:3805854,hjsv:6};
@@ -61,7 +87,7 @@ export default function Document() {
             <body>
                 <Main />
                 <NextScript />
-             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
             </body>
         </Html>
     );
