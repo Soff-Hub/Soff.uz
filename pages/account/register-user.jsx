5cdea51c9ce9cd5de447c2e984a3fef140037059
lazywk <edoniyorbekdev@gmail.com>
2024-01-09 16:54:04 +0500

    resote pages

---
+++ pages/account/register-user.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import BreadCrumb from '~/components/elements/BreadCrumb';
+import Register from '~/components/partials/account/Register';
+import PageContainer from '~/components/layouts/PageContainer';
+import FooterDefault from '~/components/shared/footers/FooterDefault';
+import { useSelector } from 'react-redux';
+import Page404 from '../page/page-404';
+
+const RegisterPage = () => {
+    const breadCrumb = [
+        {
+            text: 'Asosiy sahifa',
+            url: '/',
+        },
+        {
+            text: "Ro'yxatdan o'tish",
+        },
+    ];
+    const { user } = useSelector(state => state.auth)
+
+    const userEndPoint = 'auth/register/';
+    return (
+           user?.access ? 
+           <Page404/> :
+            <PageContainer footer={<FooterDefault />} title="Register">
+                <div className="ps-page--my-account">
+                    <BreadCrumb breacrumb={breadCrumb} />
+                    <Register url={userEndPoint} />
+                </div>
+            </PageContainer>
+    );
+};
+
+export default RegisterPage;
---
+++ pages/product/[pid].jsx
@@ -0,0 +1,137 @@
+import React from 'react';
+import BreadCrumb from '~/components/elements/BreadCrumb';
+import ProductDetailFullwidth from '~/components/elements/detail/ProductDetailFullwidth';
+import RelatedProduct from '~/components/partials/product/RelatedProduct';
+import PageContainer from '~/components/layouts/PageContainer';
+
+import Meta from '~/components/shared/headers/Meta';
+import SkeletonProductDetail from '~/components/elements/skeletons/SkeletonProductDetail';
+import { baseUrl } from '~/repositories/Repository';
+import { useEffect } from 'react';
+
+const ProductDefaultPage = ({ product, similar }) => {
+    // const router = useRouter();
+    // const { pid } = router.query;
+    // const [product, setProduct] = useState(null);
+    // const [similar, setSimilar] = useState([]);
+    // const [loading, setLoading] = useState(false);
+
+    // async function getProduct() {
+    //     setLoading(true);
+    //     const responseData = await ProductRepository.getProductsById(pid);
+    //     if (responseData) {
+    //         setProduct(responseData);
+    //     }
+    // }
+    // async function getSimilar() {
+    //     setLoading(true);
+    //     const responsSimilar = await ProductRepository.getProductSimilarSlug(pid)
+    //     if (responsSimilar) {
+    //         setSimilar(responsSimilar);
+    //     }
+    // }
+
+    // useEffect(() => {
+    //     getProduct();
+    //     getSimilar()
+    // }, [pid]);
+
+
+    const breadCrumb = [
+        {
+            text: 'Asosiy sahifa',
+            url: '/',
+        },
+        {
+            text: product.title
+                ? product.title
+                : 'Loading...',
+        },
+    ];
+    
+    useEffect(()=>{
+        document.addEventListener('selectstart', function (e) {
+            e.preventDefault();
+        });
+    
+    },[])
+
+    return (
+        <>
+            <PageContainer title={product ? product.title : 'Loading...'}>
+                <Meta
+                    title={product.title}
+                    image={product.document.images[0].image_url}
+                />
+
+                <BreadCrumb breacrumb={breadCrumb} layout="fullwidth" />
+
+                <div className="container">
+                    <div className="ps-page--product">
+                        <div className="ps-container">
+                            <div className="ps-page__container">
+                                <div className="ps-page__left">
+                                    {product ? (
+                                        <ProductDetailFullwidth
+                                            product={product}
+                                        />
+                                    ) : (
+                                        <SkeletonProductDetail />
+                                    )}
+                                </div>
+                            </div>
+
+                            {similar?.length > 0 ? (
+                                <RelatedProduct
+                                    data={similar}
+                                    collectionSlug="shop-recommend-items"
+                                />
+                            ) : (
+                                ''
+                            )}
+                        </div>
+                    </div>
+                </div>
+            </PageContainer>
+        </>
+    );
+};
+
+// export async function getStaticPaths() {
+//     const res = await fetch(baseUrl + 'customer/documents/');
+//     const documentSlug = await res.json();
+//     console.log('doc slug', documentSlug);
+//     const path = documentSlug.results.map((item) => ({
+//         params: { pid: item.slug },
+//     }));
+//     return { path, fallback: false };
+// }
+
+export async function getServerSideProps(context) {
+    try {
+        const request = await fetch(baseUrl + `customer/documents/${context.query.pid}/`);
+        const product = await request.json();
+
+        const SimilarRes = await fetch(baseUrl + `customer/similar/${context.query.pid}/`);
+        const similar = await SimilarRes.json();
+
+        return {
+            props: {
+                product,
+                similar
+            }
+        };
+    } catch (error) {
+        console.error("Error fetching data:", error);
+
+        return {
+            props: {
+                product: null,
+                similar: null
+            }
+        };
+    }
+}
+
+
+export default ProductDefaultPage;
