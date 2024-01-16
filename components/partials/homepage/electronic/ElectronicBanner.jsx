2faab91e5a3b99341fda744f3a7a9fcefa62ddc6
lazywk <edoniyorbekdev@gmail.com>
2024-01-16 16:38:02 +0500

    banner url

--- components/partials/homepage/electronic/ElectronicBanner.jsx
+++ components/partials/homepage/electronic/ElectronicBanner.jsx
@@ -53,6 +53,7 @@ function ElectronicBanner() {
         }
     }, []);
 
+
     return (
         <>
 
@@ -61,7 +62,7 @@ function ElectronicBanner() {
                     <div className="ps-section__left">
                         <div className="item banner-left-one">
                             {bannerItem?.[0]?.image ? (
-                                <Link href={`${bannerItem?.[0]?.url}`}>
+                                <Link href={`${bannerItem?.[0]?.url.replace('https://soff.uz', '')}`}>
                                     <a>
                                         {/* <img
                                     style={{
@@ -108,7 +109,7 @@ function ElectronicBanner() {
                     <div className="ps-section__right">
                         {bannerItem?.[1] ? (
                             <div className="banner-right">
-                                <Link href={`${bannerItem?.[1].url}`}>
+                                <Link href={`${bannerItem?.[1].url.replace('https://soff.uz', '')}`}>
                                     <a className="ps-collection">
                                         <div
                                             style={{
@@ -121,7 +122,7 @@ function ElectronicBanner() {
                                         {/* <NextImage url={bannerItem?.[1]?.image} width='400px' height='170px' /> */}
                                     </a>
                                 </Link>
-                                <Link href={`${bannerItem?.[2].url}`}>
+                                <Link href={`${bannerItem?.[2].url.replace('https://soff.uz', '')}`}>
                                     <a className="ps-collection">
                                         <div
                                             style={{
@@ -162,7 +163,7 @@ function ElectronicBanner() {
                     </div>
                 </div>
             </section>
-            { modal ? (
+            {modal ? (
                 <>
                     <div
                         onClick={closeBanner}
@@ -185,7 +186,7 @@ function ElectronicBanner() {
                                 top: ' -9px',
                                 zIndex: 1
                             }}
-                            >
+                        >
                             <span
                                 className="fs-3"
                                 style={{ cursor: 'pointer' }}>
