567072513beb4a4ba902576e34a98e0a3674e109
lazywk <edoniyorbekdev@gmail.com>
2024-01-19 16:44:59 +0500

    referall

--- components/shared/panel/UserMenuPanel.jsx
+++ components/shared/panel/UserMenuPanel.jsx
@@ -4,6 +4,7 @@ import { useRouter } from 'next/router';
 import { useSelector } from 'react-redux';
 import GetRepository from '~/reositoriy-admin/GetRepository';
 import { BeatLoader } from 'react-spinners';
+import { Modal, Tooltip } from 'antd';
 
 
 
@@ -94,6 +95,17 @@ const UserMenuPanel = ({
 
     const { asPath } = useRouter();
 
+
+    const handleAboutReffer = () => {
+        handleDrawerClose()
+        const modal = Modal.info({
+            centered: true,
+            title: 'Taklif havlasi bu?',
+            content: `Siz o'z taklif havolangiz orqali ro'yxatdan o'tgan har bir sotuvchi foydasidan, 10% sizga beriladigan bonus`,
+        });
+        modal.update
+    }
+
     return (
         <aside className="ps-widget--account-dashboard">
             <div className="ps-widget__header  p-2 pb-4 py-4">
@@ -120,6 +132,15 @@ const UserMenuPanel = ({
                     ?
                     <div className='pb-3'>
                         <h4 className='w-100  border m-0 p-3 rounded-3  text-truncate ' ><strong className={`fs-3 text-${profile?.is_payment === false ? "danger" : "success"}`} ><i className="fa-solid fa-wallet mx-2"></i>  Balans: {addPeriodToThousands(profile?.wallet)} so'm</strong></h4>
+                        <h5 className='w-100  border m-0 p-3 rounded-3  text-truncate' >
+
+                            <p className='m-0' style={{ fontWeight: 600, color: 'black' }}>Taklif qilingan foydalanuvhcilar: <span className='text-success'>0</span> ta</p>
+                            <p className='m-0' style={{ fontWeight: 600, color: 'black' }}>Taklif orqali daromadingiz: <span className='text-success'>0</span> so'm</p>
+                            <p className='m-0 mb-3' style={{ display: 'flex', alignItems: 'flex-start', gap: 20 }}>
+                                <a href={'https://t.me/soff_uz_bot'} target='_blank'>Taklif havolani olish <i class="fa-brands fa-telegram fa-beat fa-xl mt-4 mt-lg-3 mt-md-3 mt-sm-3" style={{ color: '#6492e3' }}></i></a>
+                            </p>
+                            <u onClick={() => handleAboutReffer()} className='m-0' style={{ cursor: 'pointer' }}>Taklif havola nima?</u>
+                        </h5>
                     </div>
                     :
                     <></>
@@ -131,9 +152,9 @@ const UserMenuPanel = ({
                             <Link href={link.url}>
                                 <a>
                                     <i className={link.icon}></i>
-                                    {link.text} {user?.role==="admin" ? (link?.url==="/account/application" && webdata?.is_avaiable===true ? <strong className='text-white bg-warning  border px-3 py-2  fs-5 rounded-circle' style={{marginLeft:"15rem"}}>{webdata?.count}</strong> : "" ) : ""}
-                                    {user?.role==="admin" ? (link?.url==="/account/products" && webdata1?.is_avaiable===true ? <strong className='text-white bg-warning  border px-3 py-2  fs-5 rounded-circle' style={{marginLeft:"15rem"}}>{webdata1?.count}</strong> : "" ) : ""} 
-                                    {user?.role==="seller" ? (link?.url==="/account/myproducts" && webdata2?.is_avaiable===true ? <strong className='text-white bg-warning  border px-3 py-2  fs-5 rounded-circle' style={{marginLeft:"5rem"}}>{webdata2?.count}</strong> : "" ) : ""}
+                                    {link.text} {user?.role === "admin" ? (link?.url === "/account/application" && webdata?.is_avaiable === true ? <strong className='text-white bg-warning  border px-3 py-2  fs-5 rounded-circle' style={{ marginLeft: "15rem" }}>{webdata?.count}</strong> : "") : ""}
+                                    {user?.role === "admin" ? (link?.url === "/account/products" && webdata1?.is_avaiable === true ? <strong className='text-white bg-warning  border px-3 py-2  fs-5 rounded-circle' style={{ marginLeft: "15rem" }}>{webdata1?.count}</strong> : "") : ""}
+                                    {user?.role === "seller" ? (link?.url === "/account/myproducts" && webdata2?.is_avaiable === true ? <strong className='text-white bg-warning  border px-3 py-2  fs-5 rounded-circle' style={{ marginLeft: "5rem" }}>{webdata2?.count}</strong> : "") : ""}
                                 </a>
                             </Link>
                         </li>
