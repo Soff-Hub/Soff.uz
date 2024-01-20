9e13e6d5bad8764518636d226d291ca59d876550
lazywk <edoniyorbekdev@gmail.com>
2024-01-20 17:47:49 +0500

    invite

--- components/shared/panel/UserMenuPanel.jsx
+++ components/shared/panel/UserMenuPanel.jsx
@@ -102,7 +102,7 @@ const UserMenuPanel = ({
         const modal = Modal.info({
             centered: true,
             title: 'Taklif havlasi bu?',
-            content: `Siz o'z taklif havolangiz orqali ro'yxatdan o'tgan har bir sotuvchi foydasidan, ${+profile?.inviter_percentage} % sizga beriladigan bonus`,
+            content: `Sizning taklif havolangiz orqali ro'yxatdan o'tgan har bir sotuvchining daromadidan, ${+profile?.inviter_percentage} % qismi sizga tushadigan daromad`,
         });
         modal.update
     }
@@ -135,7 +135,7 @@ const UserMenuPanel = ({
                         <h4 className='w-100  border m-0 p-3 rounded-3  text-truncate ' ><strong className={`fs-3 text-${profile?.is_payment === false ? "danger" : "success"}`} ><i className="fa-solid fa-wallet mx-2"></i>  Balans: {addPeriodToThousands(profile?.wallet)} so'm</strong></h4>
                         <h5 className='w-100  border m-0 p-3 rounded-3  text-truncate' >
 
-                            <p className='m-0' style={{ fontWeight: 600, color: 'black' }}>Taklif qilingan foydalanuvhcilar: <span className='text-success'>{profile?.invited_users}</span> ta</p>
+                            <p className='m-0' style={{ fontWeight: 600, color: 'black' }}>Taklif qilingan foydalanuvchilar: <span className='text-success'>{profile?.invited_users}</span> ta</p>
                             <p className='m-0' style={{ fontWeight: 600, color: 'black' }}>Taklif orqali daromadingiz: <span className='text-success'>{formatCurrency(profile?.inviter_wallet)}</span> so'm</p>
                             <p className='m-0 mb-3' style={{ display: 'flex', alignItems: 'flex-start', gap: 20 }}>
                                 <a href={'https://t.me/soff_uz_bot'} target='_blank'>Taklif havolani olish <i class="fa-brands fa-telegram fa-beat fa-xl mt-4 mt-lg-3 mt-md-3 mt-sm-3" style={{ color: '#6492e3' }}></i></a>
