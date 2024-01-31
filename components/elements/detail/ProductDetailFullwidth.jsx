329e1ceb9e41a98e2940a9463312fb86b772e58d
Farangiz17 <farangizismatova24@gmail.com>
2024-01-31 16:12:06 +0500

    add client page in seller image

--- components/elements/detail/ProductDetailFullwidth.jsx
+++ components/elements/detail/ProductDetailFullwidth.jsx
@@ -24,54 +24,74 @@ const ProductDetailFullwidth = ({ product, document }) => {
     }, []);
     return (
         <>
-        <Meta  title={product?.title} image={product?.iamges?.map(item=>(item?.image_url))}/>
-        <div className="ps-product--detail ps-product--fullwidth">
-            <div className="ps-product__header ">
-                <ThumbnailDefault product={product} />
-                <div className="ps-product__info">
-                    <ModuleDetailTopInformation product={product} />
-                    <div>
-                        {product?.seller?.first_name && (
-                            <h4
-                                style={{
-                                    cursor: 'pointer',
-                                }}
-                                onClick={() =>
-                                    SellerPage(product?.seller?.id)
-                                }>
-                                Muallif : {product?.seller?.first_name}  {product?.seller?.last_name} 
-                            </h4>
-                        )}
-                    </div>
-                    <ModuleProductDetailDescription product={product} />
-                    <ModuleDetailShoppingActions product={product} document={document} />
-                    <div className=" d-flex justify-content-start align-content-center flex-wrap">
-                        {tag?.length > 0 &&
-                            tag.map((item, i) => (
-                                <div key={i} className="mx-2">
-                                    <Link href="#" as="#">
-                                        <a
-                                            onClick={() =>
-                                                searchTag(item?.name)
-                                            }>
-                                            {' '}
-                                            #{item.name}{' '}
-                                        </a>
-                                    </Link>
+            <Meta
+                title={product?.title}
+                image={product?.iamges?.map((item) => item?.image_url)}
+            />
+            <div className="ps-product--detail ps-product--fullwidth">
+                <div className="ps-product__header ">
+                    <ThumbnailDefault product={product} />
+                    <div className="ps-product__info">
+                        <ModuleDetailTopInformation product={product} />
+                        <div>
+                            {product?.seller?.first_name && (
+                                <div
+                                    className="document-seller-about mb-3 product_detail__seller_name"
+                                    onClick={() =>
+                                        SellerPage(product?.seller?.id)
+                                    }>
+                                    <div>
+                                        {product?.seller?.image ? (
+                                            <img
+                                                src={`${product?.seller?.image}`}
+                                                className="profile__image-client"
+                                            />
+                                        ) : (
+                                            <i
+                                                className=" fa-2x text-info fa-solid fa-circle-user"
+                                                style={{
+                                                    fontSize: '35px',
+                                                }}></i>
+                                        )}
+                                    </div>
+                                    <h4
+                                      >
+                                        {product?.seller?.first_name}{' '}
+                                        {product?.seller?.last_name}
+                                    </h4>
                                 </div>
-                            ))}
+                            )}
+                        </div>
+                        <ModuleProductDetailDescription product={product} />
+                        <ModuleDetailShoppingActions
+                            product={product}
+                            document={document}
+                        />
+                        <div className=" d-flex justify-content-start align-content-center flex-wrap">
+                            {tag?.length > 0 &&
+                                tag.map((item, i) => (
+                                    <div key={i} className="mx-2">
+                                        <Link href="#" as="#">
+                                            <a
+                                                onClick={() =>
+                                                    searchTag(item?.name)
+                                                }>
+                                                {' '}
+                                                #{item.name}{' '}
+                                            </a>
+                                        </Link>
+                                    </div>
+                                ))}
+                        </div>
                     </div>
                 </div>
+                {product?.description ? (
+                    <DefaultDescription product={product} />
+                ) : (
+                    ''
+                )}
             </div>
-            {
-                product?.description ? 
-                <DefaultDescription product={product} />
-                :
-                ''
-            }
-        </div>
         </>
-
     );
 };
 
