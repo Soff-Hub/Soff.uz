376b0280010994e9932fba3f909a650d751a1173
Farangiz17 <farangizismatova24@gmail.com>
2024-02-07 15:14:34 +0500

    notification count

--- components/shared/headers/modules/ElectronicHeaderActions.jsx
+++ components/shared/headers/modules/ElectronicHeaderActions.jsx
@@ -9,50 +9,90 @@ import { SmileOutlined } from '@ant-design/icons';
 import { notification } from 'antd';
 import { useState } from 'react';
 import { useEffect } from 'react';
+import GetRepository from '~/reositoriy-admin/GetRepository';
 
 const ElectronicHeaderActions = ({ auth, ecomerce }) => {
     const { wishlist } = useWishlist();
-    const [socket, setSocket] = useState(0);
+    const [socket, setSocket] = useState(null);
     const [api, contextHolder] = notification.useNotification();
     const { user } = useSelector((state) => state.auth);
+    const [notifications, setNotifications] = useState(null);
+
+    const getNotification = async (token) => {
+        const respons = await GetRepository.getNotificationData(token);
+        if (respons) {
+            setNotifications(respons.results);
+            console.log('notification list', respons.results);
+        }
+    };
+
     const openNotification = () => {
         api.open({
-            message: 'Notification Title',
-            description:
-                'This is the content of the notification. This is the content of the notification. This is the content of the notification.',
+            message: 'Soff.uz da yangiliklar',
+            description: (
+                <div>
+                    <Link href={`/account/notification`}>
+                        <a>Yangiliklarni batafsil ko'rish</a>
+                    </Link>
+                </div>
+            ),
             icon: (
                 <SmileOutlined
                     style={{
-                        color: '#108ee9',
+                        color: '#00A44F',
                     }}
                 />
             ),
         });
     };
 
+    useEffect(() => {
+        if (user?.access) {
+            // Agar user?.access mavjud bo'lsa
+            const newSocket = new WebSocket(
+                `wss://api.soff.uz/ws/user-notification/?token=${user?.access}`
+            );
+    
+            // Yangi WebSocket ulanishini yaratish
+            newSocket.onopen = function () {
+                console.log('WebSocket ulanishi amalga oshirildi.');
+            };
+    
+            // Xabarlarni qabul qilish uchun funksiya
+            newSocket.onmessage = function (event) {
+                setSocket(event?.data.count);
+            };
+    
+            // WebSocket ulanishida xatolik bo'lganida ishlaydigan funksiya
+            newSocket.onerror = function (error) {
+                console.error('WebSocket xatosi:', error);
+            };
+    
+            // useEffect funksiyasiga qaytariladigan cleanup funksiya
+            return () => {
+                // WebSocket ulanishini yopish
+                newSocket.close();
+            };
+        }
+    }, [user?.access]);
+    
 
-    // useEffect(() => {
-    //     if (user?.access) {
-    //         const respons = new WebSocket(
-    //             `wss://api.soff.uz/ws/user-notification/?token=${user?.access}`
-    //         );
-    //         setSocket(respons);
-    //     }
-    // }, [user?.access]);
-    console.log('notification', socket);
+    useEffect(() => {
+        socket > 0 && openNotification();
+    }, [socket]);
+
+    console.log('not', socket);
+
     return (
         <div className="header__actions">
             {contextHolder}
-            <Link href="/account/notification">
-                <a className="header__extra" style={{ cursor: 'pointer' }}>
-                    <i class="fa-regular fa-bell fa-lg"></i>
-                    {
-                        socket ?
-                        <span className="socket_navbar">{socket}</span>
-                        : ''
-                    }
-                </a>
-            </Link>
+            <span
+                className="header__extra"
+                style={{ cursor: 'pointer' }}
+                onClick={openNotification}>
+                <i class="fa-regular fa-bell fa-lg"></i>
+                { <span className="socket_navbar">{socket}</span> }
+            </span>
             <Link href="/account/wishlist">
                 <a className="header__extra">
                     <i className="icon-heart"></i>
