796e3518463bbb776d675be3e94699458e427bea
Farangiz17 <farangizismatova24@gmail.com>
2024-02-09 18:25:46 +0500

    view and product desigin

--- components/elements/detail/ProductDetailFullwidth.jsx
+++ components/elements/detail/ProductDetailFullwidth.jsx
@@ -8,15 +8,11 @@ import Link from 'next/link';
 import Router from 'next/router';
 import Meta from '~/components/shared/headers/Meta';
 
-const ProductDetailFullwidth = ({ product, document }) => {
+const ProductDetailFullwidth = ({ product, document, views }) => {
     const [tag, setTag] = useState([]);
 
     const searchTag = (e) => {
         Router.push(`/search?keyword=${e}`);
-    };
-
-    const SellerPage = (e) => {
-        Router.push(`/seller/${e}`);
     };
 
     useEffect(() => {
@@ -30,48 +26,23 @@ const ProductDetailFullwidth = ({ product, document }) => {
             />
             <div className="ps-product--detail ps-product--fullwidth">
                 <div className="ps-product__header ">
-                    <ThumbnailDefault product={product} />
+                    <ThumbnailDefault product={product} views={views} />
                     <div className="ps-product__info">
                         <ModuleDetailTopInformation product={product} />
-                        <div>
-                            {product?.seller?.first_name && (
-                                <div
-                                    className="document-seller-about mb-3 product_detail__seller_name"
-                                    onClick={() =>
-                                        SellerPage(product?.seller?.id)
-                                    }>
-                                    <div>
-                                        {product?.seller?.image ? (
-                                            <img
-                                            alt='soff'
-                                                src={`${product?.seller?.image}`}
-                                                className="profile__image-client"
-                                            />
-                                        ) : (
-                                            <i
-                                                className=" fa-2x text-info fa-solid fa-circle-user"
-                                                style={{
-                                                    fontSize: '35px',
-                                                }}></i>
-                                        )}
-                                    </div>
-                                    <h4
-                                      >
-                                        {product?.seller?.first_name}{' '}
-                                        {product?.seller?.last_name}
-                                    </h4>
-                                </div>
-                            )}
-                        </div>
-                        <ModuleProductDetailDescription product={product} />
+
+                        <ModuleProductDetailDescription
+                            product={product}
+                            views={views}
+                        />
                         <ModuleDetailShoppingActions
                             product={product}
                             document={document}
                         />
+                        <p>Tezkor teglar</p>
                         <div className=" d-flex justify-content-start align-content-center flex-wrap">
                             {tag?.length > 0 &&
                                 tag.map((item, i) => (
-                                    <div key={i} className="mx-2">
+                                    <div key={i} className="m-2 tag-product">
                                         <Link href="#" as="#">
                                             <a
                                                 onClick={() =>
--- components/elements/detail/modules/ModuleProductDetailDescription.js
+++ components/elements/detail/modules/ModuleProductDetailDescription.js
@@ -1,25 +1,38 @@
 import React from 'react';
 
-const ModuleProductDetailDescription = ({ product }) => (
-    <div className="ps-product__desc">
-        <strong className="fs-3"> Qisqa tavsif </strong>
-        <ul style={{ listStyleType: 'revert' }}>
-            <li>
-                <strong>Betlar s : </strong> {product?.document?.page_count} ta
-            </li>
-            <li>
-                <strong>Hajmi : </strong> {product?.document?.file_size}
-            </li>
-            <li>
-                <strong>Turi : </strong> {product?.document?.file_type}
-            </li>
-            {product?.category?.name && (
+const ModuleProductDetailDescription = ({ product, views }) => {
+    console.log(views);
+    return (
+        <div className="ps-product__desc">
+            <strong className="fs-4"> Qisqa tavsif </strong>
+            <ul style={{ listStyleType: 'revert' }}>
                 <li>
-                    <strong> Kategoriyasi</strong> : {product?.category?.name}
+                    <strong>Betlar soni : </strong> <div></div>{' '}
+                    <span>{product?.document?.page_count} bet</span>
                 </li>
-            )}
-        </ul>
-    </div>
-);
+                <li>
+                    <strong>Hajmi : </strong> <div></div>{' '}
+                    <span>{product?.document?.file_size}</span>
+                </li>
+                <li>
+                    <strong>Turi : </strong> <div></div>{' '}
+                    <span className='file_type-color' >{product?.document?.file_type}</span>
+                </li>
+                {product?.category?.name && (
+                    <li>
+                        <strong> Kategoriyasi</strong> : <div></div>{' '}
+                        <span> {product?.category?.name}</span>
+                    </li>
+                )}
+                {views?.approved > 0 && (
+                    <li>
+                        <strong>Xarid qilishlar soni : </strong> <div></div>{' '}
+                        <span>{views?.approved}</span>
+                    </li>
+                )}
+            </ul>
+        </div>
+    );
+};
 
 export default ModuleProductDetailDescription;
