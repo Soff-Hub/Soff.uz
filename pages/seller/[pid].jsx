383e9131cbfc81adee3208f101f1bd82e3a53ec2
Farangiz17 <farangizismatova24@gmail.com>
2024-03-21 13:40:06 +0500

    seller page soon

--- pages/seller/[pid].jsx
+++ pages/seller/[pid].jsx
@@ -4,7 +4,7 @@ import PageContainer from '~/components/layouts/PageContainer';
 import { baseUrl } from '~/repositories/Repository';
 import Product from '~/components/elements/products/Product';
 import Meta from '~/components/shared/headers/Meta';
-import { Pagination } from 'antd';
+import { Modal, Pagination } from 'antd';
 import { useState } from 'react';
 import { useRouter } from 'next/router';
 import ProductRepository from '~/repositories/ProductRepository';
@@ -15,6 +15,21 @@ const SellerPage = ({ seller }) => {
     const [page, setPage] = useState(1);
     const router = useRouter();
     const { pid } = router.query;
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [isModalOpenDonate, setIsModalOpenDonate] = useState(false);
+    const showModal = () => {
+        setIsModalOpen(true);
+    };
+    const showModalDonate = () => {
+        setIsModalOpenDonate(true);
+    };
+    const handleOk = () => {
+        setIsModalOpen(false);
+    };
+    const handleOkDonate = () => {
+        setIsModalOpenDonate(false);
+    };
 
     const getSellerProduct = async (slug) => {
         const respons = await ProductRepository.getSellerProductSlug(
@@ -42,8 +57,6 @@ const SellerPage = ({ seller }) => {
         }
     };
 
-    console.log(seller);
-
     const breadCrumb = [
         {
             text: 'Asosiy sahifa',
@@ -55,13 +68,6 @@ const SellerPage = ({ seller }) => {
                 : 'Loading...',
         },
     ];
-
-    useEffect(() => {
-        if (pid) {
-            getSellerProduct(pid);
-            getSellerUser(pid);
-        }
-    }, [pid]);
 
     function addPeriodToThousands(number) {
         const numStr = String(number);
@@ -81,6 +87,12 @@ const SellerPage = ({ seller }) => {
         return formattedNumber;
     }
 
+    useEffect(() => {
+        if (pid) {
+            getSellerProduct(pid);
+            getSellerUser(pid);
+        }
+    }, [pid]);
     console.log('data', sellerr);
     // let productView = <SkeletonProductDetail />;
     return (
@@ -90,25 +102,35 @@ const SellerPage = ({ seller }) => {
                 title={`Soff | Sotuvchi ${sellerr?.seller?.full_name}  `}
                 description={`Saytimizga o'z mahsulotlarini sotuvga qo'yayotgan ${sellerr?.seller?.full_name} ning barcha mahsulotlarini ko'rishingiz mumkin`}
             />
-
+            <Modal
+                title="Buyurtma"
+                open={isModalOpen}
+                onOk={handleOk}
+                cancelButtonProps={{ style: { display: 'none' } }}
+                okButtonProps={{ style: { backgroundColor: '#00A44F' } }}>
+                <p>Some contents...</p>
+                <p>Some contents...</p>
+                <p>Some contents...</p>
+            </Modal>
+            <Modal
+                title="Qo'llab quvvatlash"
+                open={isModalOpenDonate}
+                onOk={handleOkDonate}
+                cancelButtonProps={{ style: { display: 'none' } }}
+                okButtonProps={{ style: { backgroundColor: '#c5ab35' } }}>
+                <p>Some contents...</p>
+                <p>Some contents...</p>
+                <p>Some contents...</p>
+            </Modal>
             <div className="ps-product-list mb-5">
                 <div className="seller-account-page">
-                    {/* <div
-                        className="seller-cover"
-                        style={{
-                            backgroundImage: 'url(/static/img/soff/ss.jpg)',
-                            backgroundSize: 'cover',
-                            backgroundRepeat: 'no-repeat',
-                            width: '100%',
-                            backgroundPositionY: 'top',
-                        }}></div> */}
                     <div className="container">
                         <div className="document-seller-about my-5 row">
                             <div className="text-center col-12 col-md-4">
-                                {seller?.results?.[0]?.seller?.image ? (
+                                {sellerr?.seller?.image ? (
                                     <img
                                         alt="soff"
-                                        src={`${seller?.results?.[0]?.seller?.image}`}
+                                        src={`${sellerr?.seller?.image}`}
                                         className="profile__image"
                                     />
                                 ) : (
@@ -120,16 +142,23 @@ const SellerPage = ({ seller }) => {
                                 )}
                             </div>
                             <div className="col-12 col-md-8">
-                                <p className="my-4 text-center text-md-left h1">{sellerr?.seller?.full_name}</p>
+                                <p className="my-4 text-center text-md-left h1">
+                                    {sellerr?.seller?.full_name}
+                                </p>
                                 <div className="row justify-content-center">
                                     <div className="col-10 col-sm-6 col-md-4 my-3">
                                         <div className="d-flex align-items-center">
                                             <i class="fa-regular fa-hard-drive fa-2x mr-4"></i>
                                             <div>
                                                 <p className="h1">
-                                                {sellerr?.total_approved_documents} ta
+                                                    {
+                                                        sellerr?.total_approved_documents
+                                                    }{' '}
+                                                    ta
                                                 </p>
-                                                <p className="h4">Jami mahsulotlar soni</p>
+                                                <p className="h4">
+                                                    Jami mahsulotlar soni
+                                                </p>
                                             </div>
                                         </div>
                                     </div>
@@ -138,9 +167,14 @@ const SellerPage = ({ seller }) => {
                                             <i class="fa-regular fa-handshake fa-2x mr-4"></i>
                                             <div>
                                                 <p className="h1">
-                                                    {sellerr?.total_sold_documents} ta
+                                                    {
+                                                        sellerr?.total_sold_documents
+                                                    }{' '}
+                                                    ta
                                                 </p>
-                                                <p className="h4">Sotilgan mahsulotlari soni</p>
+                                                <p className="h4">
+                                                    Sotilgan mahsulotlari soni
+                                                </p>
                                             </div>
                                         </div>
                                     </div>
@@ -149,12 +183,31 @@ const SellerPage = ({ seller }) => {
                                             <i class="fa-regular fa-gem fa-2x mr-4"></i>
                                             <div>
                                                 <p className="h1">
-                                                    {addPeriodToThousands( sellerr?.total_income )} so'm
+                                                    {addPeriodToThousands(
+                                                        sellerr?.total_income
+                                                    )}{' '}
+                                                    so'm
                                                 </p>
                                                 <p className="h4">Daromad</p>
                                             </div>
                                         </div>
                                     </div>
+                                </div>
+                                <div className="d-xl-flex d-lg-flex d-md-flex d-sm-flex justify-content-center align-items-center gap-5 py-4 ">
+                                    <button
+                                        className="text-white ps-btn w-100"
+                                        onClick={showModal}>
+                                        Buyurtma
+                                        {' '}
+                                        <i class="fa-regular fa-pen-to-square"></i>{' '}
+                                    </button>
+                                    <button
+                                        className="text-white ps-btn w-100 donate-color-btn mt-3 mt-xl-0 mt-lg-0 mt-md-0 mt-sm-0"
+                                        onClick={showModalDonate}>
+                                        Qo'llab quvvatlash
+                                        {' '}
+                                        <i class="fa-solid fa-hand-holding-medical"></i>{" "}
+                                    </button>
                                 </div>
                             </div>
                         </div>
