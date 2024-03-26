70c0ad3377535e9f4066b55e43fb1cf0b49a3d59
Farangiz17 <farangizismatova24@gmail.com>
2024-03-26 18:15:58 +0500

    selection page

---
+++ pages/account/myproducts/product-selection.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import BreadCrumb from '~/components/elements/BreadCrumb';
+import PageContainer from '~/components/layouts/PageContainer';
+import Meta from '~/components/shared/headers/Meta';
+import Page404 from '~/pages/page/page-404';
+import LoginPage from '../login';
+import FooterDefault from '~/components/shared/footers/FooterDefault';
+
+const Posts = () => {
+    const { user } = useSelector((state) => state.auth);
+
+    const breadCrumb = [
+        {
+            text: 'Asosiy Sahifa',
+            url: '/',
+        },
+        {
+            text: 'Mahsulot qo’shishni tanlash',
+        },
+    ];
+
+    return user?.role === 'seller' || user?.role === 'customer' ? (
+        <PageContainer
+            footer={<FooterDefault />}
+            title="Recent Viewed Products">
+            <div className="ps-page--my-account">
+                <Meta title={'Soff | Yangi mahsulot yaratishni tanlash'} />
+                <BreadCrumb breacrumb={breadCrumb} />
+            </div>
+        </PageContainer>
+    ) : user?.access ? (
+        <Page404 />
+    ) : (
+        <LoginPage />
+    );
+};
+
+export default Posts;
