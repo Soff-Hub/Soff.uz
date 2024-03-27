f7df6c2550e029da9bb8da31b24b797f6a78d27d
Farangiz17 <farangizismatova24@gmail.com>
2024-03-27 18:26:17 +0500

    audio

--- pages/product/[pid].jsx
+++ pages/product/[pid].jsx
@@ -1,3 +1,154 @@
+// import React from 'react';
+// import BreadCrumb from '~/components/elements/BreadCrumb';
+// import ProductDetailFullwidth from '~/components/elements/detail/ProductDetailFullwidth';
+// import RelatedProduct from '~/components/partials/product/RelatedProduct';
+// import PageContainer from '~/components/layouts/PageContainer';
+
+// import Meta from '~/components/shared/headers/Meta';
+// import SkeletonProductDetail from '~/components/elements/skeletons/SkeletonProductDetail';
+// import { baseUrl } from '~/repositories/Repository';
+// import { useEffect } from 'react';
+// import { useRouter } from 'next/router';
+// import { useState } from 'react';
+// import { useSelector } from 'react-redux';
+// import ProductRepository from '~/repositories/ProductRepository';
+// import { v4 as uuidv4 } from 'uuid';
+// import PostRepository from '~/repositories/PostRepository';
+
+// export async function getServerSideProps(context) {
+
+//     try {
+//         const request = await fetch(
+//             baseUrl + `customer/documents/${context.query.pid}/`
+//         );
+//         const product = await request.json();
+
+//         const SimilarRes = await fetch(
+//             baseUrl + `customer/similar/${context.query.pid}/`
+//         );
+//         const similar = await SimilarRes.json();
+
+//         return {
+//             props: {
+//                 product,
+//                 similar,
+//             },
+//         };
+//     } catch (error) {
+//         console.error('Error fetching data:', error);
+
+//         return {
+//             props: {
+//                 product: null,
+//                 similar: null,
+//             },
+//         };
+//     }
+// }
+
+// const ProductDefaultPage = ({ product, similar }) => {
+//     // console.log('product', product);
+//     const router = useRouter();
+//     const { pid } = router.query;
+//     const [views, setViews] = useState(null)
+//     const [document, setDocument] = useState('');
+//     const { user } = useSelector((state) => state.auth);
+
+//     async function getDocument() {
+//         const responsDocumentFile = await ProductRepository.getProductFileSlug(
+//             pid,
+//             user?.access,
+//             localStorage.getItem("uuid") ? localStorage.getItem("uuid") : uuidv4()
+//         );
+//         if (responsDocumentFile) {
+//             setDocument(responsDocumentFile);
+//         }
+//     }
+//     async function getUUID(uuid) {
+//         const respons = await PostRepository.postProductUUID (
+//             pid,
+//             uuid,
+//         );
+//         if (respons) {
+//             setViews(respons)
+//         }
+//     }
+
+//     useEffect(() => {
+//         if (user?.access) {
+//             getDocument();
+//         }
+        
+//     }, [user?.access]);
+
+//     useEffect(() => {
+//         localStorage.getItem("uuid") ? '' : localStorage.setItem("uuid", uuidv4())
+        
+//     }, [])
+
+//     useEffect(() => {
+//         getUUID(localStorage.getItem("uuid") ? localStorage.getItem("uuid") : uuidv4())
+//     }, [pid])
+
+//     const breadCrumb = [
+//         {
+//             text: 'Asosiy sahifa',
+//             url: '/',
+//         },
+//         {
+//         text: product?.title ? product?.title : 'Loading...',
+//         },
+//     ];
+
+
+//     return (
+//         <>
+//             <PageContainer title={product ? product.title : 'Loading...'}>
+//                 <Meta
+//                     title={`Soff | ${product?.title} `}
+//                     image={product?.document?.images?.[0]?.image_url}
+//                     description={`${product?.title} mahsulotni saytimizdan Soff.uz bepul yuklab yoki sotib olishingiz mumkin`}
+//                 />
+
+//                 <BreadCrumb breacrumb={breadCrumb} layout="fullwidth" />
+
+//                 <div className="container">
+//                     <div className="ps-page--product">
+//                         <div className="ps-container">
+//                             <div className="ps-page__container">
+//                                 <div className="ps-page__left">
+//                                     {product ? (
+//                                         <ProductDetailFullwidth
+//                                             product={product}
+//                                             document={document}
+//                                             views={views}
+//                                         />
+//                                     ) : (
+//                                         <SkeletonProductDetail />
+//                                     )}
+//                                 </div>
+//                             </div>
+
+//                             {similar?.length > 0 ? (
+//                                 <RelatedProduct
+//                                     data={similar}
+//                                     collectionSlug="shop-recommend-items"
+//                                 />
+//                             ) : (
+//                                 ''
+//                             )}
+//                         </div>
+//                     </div>
+//                 </div>
+//             </PageContainer>
+//         </>
+//     );
+// };
+
+// export default ProductDefaultPage;
+
+
+
 import React from 'react';
 import BreadCrumb from '~/components/elements/BreadCrumb';
 import ProductDetailFullwidth from '~/components/elements/detail/ProductDetailFullwidth';
@@ -14,9 +165,10 @@ import { useSelector } from 'react-redux';
 import ProductRepository from '~/repositories/ProductRepository';
 import { v4 as uuidv4 } from 'uuid';
 import PostRepository from '~/repositories/PostRepository';
+import ProductVideoDetailFullWidth from '~/components/elements/detail/ProductVideoDetailFullWidth';
+import ProductAudioDetailFullWidth from '~/components/elements/detail/ProductAudioDetailFullWidth';
 
 export async function getServerSideProps(context) {
-
     try {
         const request = await fetch(
             baseUrl + `customer/documents/${context.query.pid}/`
@@ -47,10 +199,9 @@ export async function getServerSideProps(context) {
 }
 
 const ProductDefaultPage = ({ product, similar }) => {
-    // console.log('product', product);
     const router = useRouter();
     const { pid } = router.query;
-    const [views, setViews] = useState(null)
+    const [views, setViews] = useState(null);
     const [document, setDocument] = useState('');
     const { user } = useSelector((state) => state.auth);
 
@@ -58,19 +209,19 @@ const ProductDefaultPage = ({ product, similar }) => {
         const responsDocumentFile = await ProductRepository.getProductFileSlug(
             pid,
             user?.access,
-            localStorage.getItem("uuid") ? localStorage.getItem("uuid") : uuidv4()
+            localStorage.getItem('uuid')
+                ? localStorage.getItem('uuid')
+                : uuidv4()
         );
         if (responsDocumentFile) {
             setDocument(responsDocumentFile);
         }
     }
+
     async function getUUID(uuid) {
-        const respons = await PostRepository.postProductUUID (
-            pid,
-            uuid,
-        );
+        const respons = await PostRepository.postProductUUID(pid, uuid);
         if (respons) {
-            setViews(respons)
+            setViews(respons);
         }
     }
 
@@ -78,17 +229,17 @@ const ProductDefaultPage = ({ product, similar }) => {
         if (user?.access) {
             getDocument();
         }
-        
     }, [user?.access]);
-
     useEffect(() => {
-        localStorage.getItem("uuid") ? '' : localStorage.setItem("uuid", uuidv4())
-        
-    }, [])
-
-    useEffect(() => {
-        getUUID(localStorage.getItem("uuid") ? localStorage.getItem("uuid") : uuidv4())
-    }, [pid])
+        localStorage.getItem('uuid')
+            ? ''
+            : localStorage.setItem('uuid', uuidv4());
+        getUUID(
+            localStorage.getItem('uuid')
+                ? localStorage.getItem('uuid')
+                : uuidv4()
+        );
+    }, []);
 
     const breadCrumb = [
         {
@@ -96,16 +247,15 @@ const ProductDefaultPage = ({ product, similar }) => {
             url: '/',
         },
         {
-        text: product?.title ? product?.title : 'Loading...',
+            text: product?.title ? product?.title : 'Loading...',
         },
     ];
-
 
     return (
         <>
             <PageContainer title={product ? product.title : 'Loading...'}>
                 <Meta
-                    title={`Soff | ${product?.title} `}
+                    title={`Soff | ${product?.title}`}
                     image={product?.document?.images?.[0]?.image_url}
                     description={`${product?.title} mahsulotni saytimizdan Soff.uz bepul yuklab yoki sotib olishingiz mumkin`}
                 />
@@ -116,17 +266,46 @@ const ProductDefaultPage = ({ product, similar }) => {
                     <div className="ps-page--product">
                         <div className="ps-container">
                             <div className="ps-page__container">
-                                <div className="ps-page__left">
-                                    {product ? (
+                                {product?.document?.content_type === 'file' ? (
+                                    <div className="ps-page__left">
                                         <ProductDetailFullwidth
                                             product={product}
                                             document={document}
                                             views={views}
                                         />
-                                    ) : (
+                                    </div>
+                                ) : product?.document?.content_type ===
+                                  'video' ? (
+                                    <div className="">
+                                        <ProductVideoDetailFullWidth
+                                            product={product}
+                                            document={document}
+                                            views={views}
+                                        
+                                        />
+                                    </div>
+                                ) :  product?.document?.content_type ===
+                                'audio' ? 
+                                <div className="">
+                                <ProductAudioDetailFullWidth
+                                    product={product}
+                                    document={document}
+                                    views={views}
+                                />
+                            </div> :  product?.document?.content_type ===
+                                'article' ? 
+                                <div>
+                                <ProductAudioDetailFullWidth
+                                    product={product}
+                                    document={document}
+                                    views={views}
+                                />
+                            </div>
+                                : (
+                                    <div className="ps-page__left">
                                         <SkeletonProductDetail />
-                                    )}
-                                </div>
+                                    </div>
+                                )}
                             </div>
 
                             {similar?.length > 0 ? (
@@ -146,3 +325,4 @@ const ProductDefaultPage = ({ product, similar }) => {
 };
 
 export default ProductDefaultPage;
+
