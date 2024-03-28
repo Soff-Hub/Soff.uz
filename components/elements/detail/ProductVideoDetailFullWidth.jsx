9d2110da44b89d1575f80d01b944763443fbe563
Farangiz17 <farangizismatova24@gmail.com>
2024-03-28 12:45:25 +0500

    rew

---
+++ components/elements/detail/ProductVideoDetailFullWidth.jsx
@@ -0,0 +1,93 @@
+import React, { useEffect, useState } from 'react';
+import DefaultDescription from '~/components/elements/detail/description/DefaultDescription';
+import ModuleProductDetailDescription from '~/components/elements/detail/modules/ModuleProductDetailDescription';
+import ModuleDetailShoppingActions from '~/components/elements/detail/modules/ModuleDetailShoppingActions';
+import Link from 'next/link';
+import Router from 'next/router';
+import Meta from '~/components/shared/headers/Meta';
+import DefaultVideo from './thumbnail/DefaultVideo';
+import ModuleVideoDetailTopInformation from './modules/ModuleVideoDetailTopInformation';
+
+const ProductVideoDetailFullWidth = ({
+    product,
+    document,
+    views,
+    admin,
+    ActiveTag,
+}) => {
+    const [tag, setTag] = useState([]);
+
+    const searchTag = (e) => {
+        Router.push(`/search?keyword=${e}`);
+    };
+
+    useEffect(() => {
+        setTag(product?.tag);
+    }, []);
+
+    return (
+        <>
+            <Meta
+                title={product?.title}
+                image={product?.iamges?.map((item) => item?.image_url)}
+            />
+            <div className="ps-product--detail ">
+                <div className="row">
+                    <div className="col-xl-8 col-lg-8 col-md-12 col-sm-12 col-12">
+                        <DefaultVideo product={product} />
+                        <ModuleVideoDetailTopInformation
+                            product={product}
+                            views={views}
+                            admin={admin}
+                            />
+                    </div>
+                    <div className="col-xl-4 col-lg-4 col-md-12 col-sm-12 col-12">
+                        <ModuleProductDetailDescription
+                            product={product}
+                            views={views}
+                        />
+                        <ModuleDetailShoppingActions
+                            product={product}
+                            document={document}
+                            admin={admin}
+                        />
+                        {admin && ActiveTag}
+                        {product?.tag?.length > 0 && (
+                            <div className="">
+                                <p>Tezkor teglar</p>
+                                <div className=" d-flex justify-content-start align-content-center flex-wrap">
+                                    {product?.tag?.length > 0 &&
+                                        product?.tag.map((item, i) => (
+                                            <div
+                                                key={i}
+                                                className="m-2 tag-product">
+                                                <Link href="#" as="#">
+                                                    <a
+                                                        onClick={() =>
+                                                            searchTag(
+                                                                item?.name
+                                                            )
+                                                        }>
+                                                        {' '}
+                                                        #{item.name}{' '}
+                                                    </a>
+                                                </Link>
+                                            </div>
+                                        ))}
+                                </div>
+                            </div>
+                        )}
+                    </div>
+                </div>
+
+                {product?.description ? (
+                    <DefaultDescription product={product} />
+                ) : (
+                    ''
+                )}
+            </div>
+        </>
+    );
+};
+
+export default ProductVideoDetailFullWidth;
