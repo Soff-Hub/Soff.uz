b56f97723ce39986d5197fa980f134ef67be1168
Farangiz17 <farangizismatova24@gmail.com>
2024-03-28 15:55:29 +0500

    replased base url

--- repositories/Repository.js
+++ repositories/Repository.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
-// const baseDomain = 'https://api.soff.uz/api/v1/'; // API for products
-const baseDomain = 'http://75.119.147.222:8002/api/v1/'; 
-// export const basePostUrl = 'https://api.soff.uz'; // API for post
-export const basePostUrl = 'http://75.119.147.222:8002'; 
-// export const baseStoreURL = 'https://api.soff.uz'; // API for vendor(store)
-export const baseStoreURL = 'http://75.119.147.222:8002';
-// export const baseUrlAuth = 'https://api.soff.uz/'
-export const baseUrlAuth = 'http://75.119.147.222:8002/'
+const baseDomain = 'https://api.soff.uz/api/v1/'; // API for products
+// const baseDomain = 'http://75.119.147.222:8002/api/v1/'; 
+export const basePostUrl = 'https://api.soff.uz'; // API for post
+// export const basePostUrl = 'http://75.119.147.222:8002'; 
+export const baseStoreURL = 'https://api.soff.uz'; // API for vendor(store)
+// export const baseStoreURL = 'http://75.119.147.222:8002';
+export const baseUrlAuth = 'https://api.soff.uz/'
+// export const baseUrlAuth = 'http://75.119.147.222:8002/'
 
 export const customHeaders = {
     Accept: 'application/json',
