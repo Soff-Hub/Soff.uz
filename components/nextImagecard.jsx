cc6b60351955011aaf8aecba0f57bbebe96ed035
Farangiz17 <farangizismatova24@gmail.com>
2024-03-29 14:21:40 +0500

    detail

--- components/nextImagecard.jsx
+++ components/nextImagecard.jsx
@@ -1,20 +1,98 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from 'next/image';
+import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
-export default function NextImageCard({url, width, height, clasS}) {
-    const loaderProp =({ src }) => {
+export default function NextImageCard({
+    url,
+    width,
+    height,
+    clasS,
+    payload,
+    detail,
+}) {
+    const loaderProp = ({ src }) => {
         return src;
-      }
-  return (
-    <Image
-    src={url}
-    width={width}
-    height={height}
-    alt={url}
-    loader={loaderProp}
-    className={clasS}
-    objectFit='contain'
-    
-  />
-  )
+    };
+
+    const [up , setUp] = useState(true)
+
+    const handleUp = () => {
+        setUp(false)
+    }
+
+    useEffect(() => {
+        setTimeout(() => {
+            setUp(false)
+        }, 3000);
+    }, [up])
+
+    return (
+        <div
+            className={`${
+                (payload?.document?.content_type === 'video' ||
+                    payload?.document?.content_type === 'audio') &&
+                'video_poster'
+            }`}>
+            {payload?.document?.content_type === 'video' ? (
+                <>
+                    <div className="video_poster_fon">
+                        <i class="fa-regular fa-circle-play"></i>
+                    </div>
+                    <Image
+                        src={url}
+                        width={width}
+                        height={height}
+                        alt={url}
+                        loader={loaderProp}
+                        className={clasS}
+                        objectFit="cover"
+                    />
+                </>
+            ) : payload?.document?.content_type === 'audio' ? (
+                <>
+                    <div className="video_poster_fon">
+                        <i class="fa-solid fa-music"></i>
+                    </div>
+                    <Image
+                        src={url}
+                        width={width}
+                        height={height}
+                        alt={url}
+                        loader={loaderProp}
+                        className={clasS}
+                        objectFit="cover"
+                    />
+                </>
+            ) : detail ? (
+                <div onClick={() => handleUp()} className={` ${up && 'product_priview' } `}>
+                    <Image
+                        src={url}
+                        width={width}
+                        height={height}
+                        alt={url}
+                        loader={loaderProp}
+                        className={clasS}
+                        objectFit="contain"
+                    />
+                    {
+                        up &&
+                    <div className="up">
+                        <i class="fa-solid fa-angles-up fa-bounce"></i>
+                    </div>
+                    }
+                </div>
+            ) : (
+                <Image
+                    src={url}
+                    width={width}
+                    height={height}
+                    alt={url}
+                    loader={loaderProp}
+                    className={clasS}
+                    objectFit="contain"
+                />
+            )}
+        </div>
+    );
 }
