7a23d0bbdc770f3a84248e9f7f6d69f1d21c0831
Ozodbek Abdisamatov <abdisamatovozodbek003@gmail.com>
2024-04-08 16:40:56 +0500

    repo

--- repositories/Repository.js
+++ repositories/Repository.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 // const baseDomain = 'https://api.soff.uz/api/v1/'; // API for products
-const baseDomain = 'https://testapi.soff.uz/api/v1/'; 
+const baseDomain = 'https://api.soff.uz/api/v1/'; 
 // export const basePostUrl = 'https://api.soff.uz'; // API for post
-export const basePostUrl = 'https://testapi.soff.uz'; 
+export const basePostUrl = 'https://api.soff.uz'; 
 // export const baseStoreURL = 'https://api.soff.uz'; // API for vendor(store)
-export const baseStoreURL = 'https://testapi.soff.uz';
+export const baseStoreURL = 'https://api.soff.uz';
 // export const baseUrlAuth = 'https://api.soff.uz/'
-export const baseUrlAuth = 'https://testapi.soff.uz/'
+export const baseUrlAuth = 'https://api.soff.uz/'
 
 export const customHeaders = {
     Accept: 'application/json',
