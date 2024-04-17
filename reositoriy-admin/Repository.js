f958057d27024b393b8bed1fc8321700e823ec0c
doniyoroka <edoniyorbekdev@gmail.com>
2024-04-17 18:10:23 +0500

    gh

--- reositoriy-admin/Repository.js
+++ reositoriy-admin/Repository.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-const baseDomain = 'https://test.soff.uz/api/v1/sezller/'; 
+const baseDomain = 'https://api.soff.uz/api/v1/sezller/'; 
 // const baseDomain = 'https://testapi.soff.uz/api/v1/seller/'; 
-const baseDomainProfile = 'https://test.soff.uz/'; 
+const baseDomainProfile = 'https://api.soff.uz/'; 
 // const baseDomainProfile = 'https://testapi.soff.uz/'; 
-export const baseUrlCustomer = 'https://test.soff.uz/api/v1/'
+export const baseUrlCustomer = 'https://api.soff.uz/api/v1/'
 // export const baseUrlCustomer = 'https://testapi.soff.uz/api/v1/'
 
 
