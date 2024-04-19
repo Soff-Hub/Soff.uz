581b540cda607389c71c7131839c9f76444cd5e1
Ozodbek Abdisamatov <abdisamatovozodbek003@gmail.com>
2024-04-19 15:33:45 +0500

    repository

--- reositoriy-admin/Repository.js
+++ reositoriy-admin/Repository.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-const baseDomain = 'https://testapi.soff.uz/api/v1/seller/'; 
+const baseDomain = 'https://api.soff.uz/api/v1/seller/'; 
 // const baseDomain = 'https://testapi.soff.uz/api/v1/seller/'; 
-const baseDomainProfile = 'https://testapi.soff.uz/'; 
+const baseDomainProfile = 'https://api.soff.uz/'; 
 // const baseDomainProfile = 'https://testapi.soff.uz/'; 
-export const baseUrlCustomer = 'https://testapi.soff.uz/api/v1/'
+export const baseUrlCustomer = 'https://api.soff.uz/api/v1/'
 // export const baseUrlCustomer = 'https://testapi.soff.uz/api/v1/'
 
 
