6f36cef6c1ac02ade4e33f70907b8d82478af1ce
Farangiz17 <farangizismatova24@gmail.com>
2024-04-29 16:32:52 +0500

    runtime.js

--- components/elements/detail/thumbnail/DefaultAudio.jsx
+++ components/elements/detail/thumbnail/DefaultAudio.jsx
@@ -1,5 +1,5 @@
 
-import { jsxs } from 'react/jsx-runtime';
+import { jsxs } from 'react/jsx-runtime.js';
 import WavesurferPlayer from '@wavesurfer/react';
 import React, { useState } from 'react';
 
