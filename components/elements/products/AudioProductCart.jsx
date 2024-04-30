678a245056b1b4f9ac3a78ba73bb4d129794c66c
Farangiz17 <farangizismatova24@gmail.com>
2024-04-30 17:01:24 +0500

    fixed bug of meta tag

--- components/elements/products/AudioProductCart.jsx
+++ components/elements/products/AudioProductCart.jsx
@@ -9,15 +9,15 @@ const AudioWaveform = ({ product, inCategory }) => {
 
     const [wavesurferObj, setWavesurferObj] = useState();
 
-    const [playing, setPlaying] = useState(false); 
-    const [volume, setVolume] = useState(1); 
+    const [playing, setPlaying] = useState(false);
+    const [volume, setVolume] = useState(1);
 
     // create the waveform inside the correct component
     useEffect(() => {
         if (wavesurferRef.current && !wavesurferObj) {
             setWavesurferObj(
                 wavesurfer.create({
-                    container:  wavesurferRef.current,
+                    container: wavesurferRef.current,
                     scrollParent: true,
                     autoCenter: true,
                     loopSelection: true,
@@ -25,26 +25,21 @@ const AudioWaveform = ({ product, inCategory }) => {
                     waveColor: '#00A44F',
                     progressColor: '#ccc',
                     responsive: true,
-					height: 40
+                    height: 40,
                 })
             );
         }
     }, [wavesurferObj]);
-	
 
     useEffect(() => {
         if (wavesurferObj) {
-            wavesurferObj.load(
-                product?.document?.short_content_url
-            );
+            wavesurferObj.load(product?.document?.short_content_url);
         }
     }, [product?.document?.short_content_url, wavesurferObj]);
 
-
-
-	useEffect(() => {
+    useEffect(() => {
         if (wavesurferObj) wavesurferObj.setVolume(volume);
-    }, [volume, wavesurferObj])
+    }, [volume, wavesurferObj]);
 
     const handlePlayPause = (e) => {
         wavesurferObj.playPause();
@@ -56,22 +51,21 @@ const AudioWaveform = ({ product, inCategory }) => {
             wavesurferObj.on('finish', handleAudioFinish);
         }
     }, [wavesurferObj]);
-    
+
     const handleAudioFinish = () => {
-        // Audioning tugagan paytidagi harakatlar
         console.log('Audio tugab ketdi');
-        // Boshqa harakatlar
         setPlaying(false);
     };
 
-
     return (
         <>
-           
             <div className="audio-cart-container">
                 <div className="audio-cart-content">
                     <div className="row">
-                        <div className={`${!inCategory ? 'col-3' : 'col-4'} align-content-center`}>
+                        <div
+                            className={`${
+                                !inCategory ? 'col-3' : 'col-4'
+                            } align-content-center`}>
                             <div className="d-flex gap-5 audio-list-left">
                                 <div
                                     className="col-xl-1 col-lg-1 col-md-1  audio-play-list"
@@ -86,30 +80,30 @@ const AudioWaveform = ({ product, inCategory }) => {
                                         </>
                                     )}
                                 </div>
-                               <div>
-							   <p>
+                                <div>
+                                    <p>
+                                        <Link
+                                            style={{ display: 'flex' }}
+                                            href="/product/[pid]"
+                                            as={`/product/${product.slug}`}>
+                                            <a className="audio-name text-truncate fw-bolder">
+                                                {product?.title}
+                                            </a>
+                                        </Link>
+                                    </p>
                                     <Link
-                                        style={{ display: 'flex' }}
-                                        href="/product/[pid]"
-                                        as={`/product/${product.slug}`}>
-                                        <a className="audio-name text-truncate fw-bolder">
-                                            {product?.title}
+                                        href="/seller/[pid]"
+                                        as={`/seller/${product?.seller?.id}`}>
+                                        <a className="text-truncate">
+                                            {product?.seller?.first_name}{' '}
+                                            {product?.seller?.last_name}
                                         </a>
                                     </Link>
-                                           
-                                </p>
-								<Link  href="/seller/[pid]"
-                                        as={`/seller/${product?.seller?.id}`} >
-								<a className='text-truncate' > 
-									{product?.seller?.first_name}{" "}
-									{product?.seller?.last_name}
-								</a>
-								</Link>
-							   </div>
+                                </div>
                             </div>
                         </div>
-                        <div className="col-xl-7 col-lg-9" >
-                            <div ref={wavesurferRef} id="waveform" ></div>
+                        <div className="col-xl-7 col-lg-9">
+                            <div ref={wavesurferRef} id="waveform"></div>
                         </div>
                         <div className="col-xl-2 col-lg-9 d-flex align-content-center justify-content-start justify-content-xl-center pl-xl-0 pl-5 p pt-2">
                             <ModuleProductActions
--- components/shared/headers/Meta.js
+++ components/shared/headers/Meta.js
@@ -2,49 +2,32 @@ import Head from 'next/head';
 import React from 'react';
 
 const Meta = ({ title, image, description, keywords, author }) => {
- 
-  const removeHTMLTags = (html) => {
-    return html.replace(/<[^>]+>/g, '');
-  };
+    const removeHTMLTags = (html) => {
+        return html.replace(/<[^>]+>/g, '');
+    };
 
     return (
         <Head>
-            <meta  property="og:title" content={title} />
+            <meta property="og:title" content={title} />
+            <title>{title}</title>
             <meta
                 property="og:description"
                 content={
-                  description
-                      ? removeHTMLTags(description)
+                    description
+                        ? removeHTMLTags(description)
                         : `${title} | Soff | Soff online hujjatlar bazasi`
                 }
             />
-            <meta property="og:image"  content={image} />
+            <meta property="og:image" content={image} />
             <meta property="og:site_name" content="Soff.uz" />
             <meta
                 property="og:keywords"
                 content={keywords ? keywords?.map((e) => e) : title}
             />
-            <meta name="site_name" content="Soff.uz" />
-            <title>{title}</title>
-            <meta name="title"  content={title} />
-            <meta  name="image" content={image} />
-            <meta
-                name="description"
-                content={
-                  description
-                        ? removeHTMLTags(description)
-                        : `${title}  Soff.uz - Intellektual mulk marketi, Intellektual mahsulotlarini soting va xarid qiling.`
-                }
-            />
             <meta
                 name="author"
                 property="author"
                 content={author ? author : 'nouthemes'}
-            />
-            <meta
-                name="keywords"
-                property="keywords"
-                content={keywords ? keywords?.map((e) => e) : title}
             />
         </Head>
     );
