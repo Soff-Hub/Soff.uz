31349d3bdfce09346c5262fb991ded4710ca0e4b
Farangiz17 <farangizismatova24@gmail.com>
2024-05-04 14:26:38 +0500

    extra document

--- components/partials/account/modules/FormCheckoutInformationOne.jsx
+++ components/partials/account/modules/FormCheckoutInformationOne.jsx
@@ -13,7 +13,7 @@ function FormCheckoutInformationOne() {
         <div className="tolov-usullari">
             <div style={{ padding: "0 15px" }}>
                 <h3 className='tolov-usullari-h3' style={{ fontWeight: '600', margin: '0', padding: '0' }}>To'lov turini tanlang</h3>
-                <p>Mahsulotni sotib olganingizdan so'ng   <a href="/account/sellerproducts" ><strong className='text-success'>Sotib olingan</strong></a> lar <br /> sahifasidan  yokida o'sha mahsulot ichida yuklab olish  <br /> knobkasi  chiqadi shu  orqali yuklab olishingiz mumkin!</p>
+                <p>Mahsulotni sotib olganingizdan so'ng   <a href="/account/sellerproducts" ><strong className='text-success'>Sotib olingan</strong></a> lar <br /> sahifasidan  yokida o'sha mahsulot ichida yuklab olish  <br /> tugmasi  chiqadi shu  orqali yuklab olishingiz mumkin!</p>
             </div>
             <div className="d-flex aligin-content-center  rounded-5 px-3">
                 <CreditCard2 document={arr} />
