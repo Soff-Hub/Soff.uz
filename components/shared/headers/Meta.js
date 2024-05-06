d0bee326c6d6e85367ad5db89d25a4db71a26e3a
Farangiz17 <farangizismatova24@gmail.com>
2024-05-06 17:57:36 +0500

    meta tag

--- components/shared/headers/Meta.js
+++ components/shared/headers/Meta.js
@@ -14,6 +14,7 @@ const Meta = ({ title, image, description, keywords, author }) => {
 
     return (
         <Head>
+            <meta property="og:type" content="website" />
             <meta property="og:title" content={title} />
             <meta
                 property="og:description"
@@ -24,18 +25,31 @@ const Meta = ({ title, image, description, keywords, author }) => {
                 }
             />
             <meta property="og:image" content={image} />
-            <meta name="twitter:image" content={image}></meta>
-            <meta property="og:url" content={image} />
+            <meta property="og:url" content="soff.uz" />
             <meta property="og:site_name" content="Soff.uz" />
             <meta
                 property="og:keywords"
-                content={
-                    keywords ? keywords?.map((e) => e?.name) : title
-                }
+                content={keywords ? keywords?.map((e) => e?.name) : title}
             />
 
-            <meta name="viewport" />
-           
+
+            <meta property="twitter:image" content={image}></meta>
+            <meta property="twitter:type" content="website" />
+            <meta property="twitter:title" content={title} />
+            <meta
+                property="twitter:description"
+                content={
+                    description
+                        ? removeHTMLTags(description)
+                        : `${title} | Soff | Soff online hujjatlar bazasi`
+                }
+            />
+            <meta property="twitter:url" content="soff.uz" />
+            <meta property="twitter:site_name" content="Soff.uz" />
+            <meta
+                property="twitter:keywords"
+                content={keywords ? keywords?.map((e) => e?.name) : title}
+            />
         </Head>
     );
 };
