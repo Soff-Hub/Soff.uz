f76fcbb9b98a4705f4c6dbb5b293a72229f2e4be
Ozodbek Abdisamatov <abdisamatovozodbek003@gmail.com>
2024-05-14 15:23:25 +0500

    home pages

--- components/elements/products/ProductSearchGoogle.jsx
+++ components/elements/products/ProductSearchGoogle.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import Link from 'next/link';
-import useProduct from '~/hooks/useProduct';
 import { formatCurrency } from '~/utilities/product-helper';
 
 
+
 const ProductSearchGoogle = ({ product }) => {
-    const { title } = useProduct();
+
 
     let view;
     if (product.sale_price) {
@@ -49,9 +49,16 @@ const ProductSearchGoogle = ({ product }) => {
 
     return (
         <div className='search_products_head'>
-         <div className="search_products_box_cards">
-            
-         </div>
+            <div className="search_products_box_cards">
+                <Link href={"https://soff.uz"}>
+                    <a><p className='body_span_title'>https://soff.uz</p></a>
+                </Link>
+                <h4 className='title_elh4'>{product?.title}</h4>
+                <p className='descripton_title'>
+
+                    Write a small description of what you have done in the company and what you learned. Try to be not too detailed. Write two to three lines of text. {product?.title}
+                </p>
+            </div>
         </div>
     );
 };
