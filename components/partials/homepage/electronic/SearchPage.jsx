e71e34706b1488e87de3665df6e49fda2ce6f702
doniyor <edoniyorbekdev@gmail.com>
2024-05-23 10:48:39 +0500

    uzbek tilida

--- components/partials/homepage/electronic/SearchPage.jsx
+++ components/partials/homepage/electronic/SearchPage.jsx
@@ -121,7 +121,7 @@ const SearchPage = () => {
                         </h1>
                         <h1>
                             {' '}
-                            Uzbek tilida saralanib borilayotgan sifatli
+                            O'zbek tilida saralanib borilayotgan sifatli
                             ma'lumotlar jamlanmasini, faylar, tasvirlar,
                             videolar, audiolar ko'rinishida qidirib topish
                             imkonini beradi.
