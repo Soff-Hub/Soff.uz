e3149ac902acd27c84f3ec156d3dfd0ddf84e951
Ozodbek Abdisamatov <abdisamatovozodbek003@gmail.com>
2024-05-28 17:55:14 +0500

    get category parnet

--- components/shared/headers/HeaderElectronic.jsx
+++ components/shared/headers/HeaderElectronic.jsx
@@ -3,46 +3,15 @@ import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 import ElectronicHeaderActions from '~/components/shared/headers/modules/ElectronicHeaderActions';
 import { stickyHeader } from '~/utilities/common-helpers';
-import CollectionRepository from '~/repositories/CollectionRepository';
-import ProductRepository from '~/repositories/ProductRepository';
-
-import { useDispatch, useSelector } from 'react-redux';
-import { Category_Lists, TopCategory_Lists } from '~/store/auth/action';
 import NextImageCard from '~/components/nextImagecard';
 
-const HeaderElectronic = ({ kk }) => {
-    const {
-        category_lists: categoryData,
-        top_category_lists: topCategoryData,
-    } = useSelector((state) => state.auth);
-    const dispatch = useDispatch();
+const HeaderElectronic = () => {
+
     const [run, setRun] = useState(false)
-
-    async function getCategoryFunc() {
-        const responseData = await CollectionRepository.getCategoryData(
-            `seller/admin/category-parent/`
-        );
-        if (responseData) {
-            dispatch(Category_Lists(responseData?.data?.results));
-        }
-    }
-
-    async function getTopCategory() {
-        const responsData = await ProductRepository.getTopCategories();
-        if (responsData) {
-            dispatch(TopCategory_Lists(responsData));
-        }
-    }
 
     useEffect(() => {
         if (process.browser) {
             window.addEventListener('scroll', stickyHeader);
-        }
-        if (categoryData?.length === 0) {
-            getCategoryFunc();
-        }
-        if (topCategoryData?.length === 0) {
-            getTopCategory();
         }
     }, []);
 
@@ -84,27 +53,4 @@ const HeaderElectronic = ({ kk }) => {
 
 export default HeaderElectronic;
 
-export async function getServerSideProps() {
-    try {
-        const request = await fetch(baseUrl + 'seller/admin/category-parent/');
-        if (!request.ok) {
-            throw new Error(
-                'Request to the API failed with status ' + request.status
-            );
-        }
-        const categoryResponse = await request.json();
 
-        return {
-            props: {
-                kk: categoryResponse,
-            },
-        };
-    } catch (error) {
-        console.error('Error fetching data:', error);
-        return {
-            props: {
-                kk: null,
-            },
-        };
-    }
-}
