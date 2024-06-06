eb021b71d8b6eba61cf342d8b01ed1e6e69a2dc9
Ozodbek Abdisamatov <abdisamatovozodbek003@gmail.com>
2024-06-06 13:14:18 +0500

    loading orders all and my-ordrs

--- components/partials/account/DealsList.jsx
+++ components/partials/account/DealsList.jsx
@@ -52,7 +52,7 @@ export default function DealsList({ setOpen }) {
                 title: 'Muvaffaqqiyatli!',
                 content: `${ItemsData?.data?.msg
                     ? ItemsData?.data?.msg
-                    : 'Sizning arizangiz yuborildi'
+                    : "Sizning buyurtmangiz muvaffaqqiyatli yuborildi! 24 soat ichida adminlar tomonidan  buyurtmangiz 'Tasdiqlangan' dan so'ng Barcha buyurtmalarda ko'rishingiz mumkin yoki 'Bekor' qilishinishi ham mumkin"
                     }  `,
             });
             modal.update;
