c2edff5dc2533b5bb02b57403c2ec0e2e1e6e78c
Ozodbek Abdisamatov <abdisamatovozodbek003@gmail.com>
2024-06-11 18:59:31 +0500

    websocket reload

--- components/partials/account/modules/DealsSidebar.jsx
+++ components/partials/account/modules/DealsSidebar.jsx
@@ -98,29 +98,6 @@ const DealsSidebar = ({ setType, setLifetime, setLifetime2, setProgressPrice }) 
     ]
 
 
-    useEffect(() => {
-        if (socket) {
-            socket.onmessage = (event) => {
-                setWebData(JSON.parse(event?.data));
-            };
-
-            socket.onopen = () => {
-                console.log('WebSocket connection established');
-            };
-
-            socket.onclose = () => {
-                console.log('WebSocket connection closed');
-            };
-
-            socket.onerror = (error) => {
-                console.error('WebSocket error:', error);
-            };
-            return () => {
-                socket.close();
-            };
-        }
-    }, [socket]);
-
 
 
 
@@ -131,8 +108,42 @@ const DealsSidebar = ({ setType, setLifetime, setLifetime2, setProgressPrice }) 
                 `${process.env.NEXT_PUBLIC_WS_BASE_URL}ws/deals?token=${token}`
             );
             setSocket(ws);
+
+            ws.onopen = () => {
+                console.log('WebSocket connection established');
+            };
+
+            ws.onmessage = (event) => {
+                setWebData(JSON.parse(event?.data));
+            };
+
+            ws.onclose = () => {
+                console.log('WebSocket connection closed');
+            };
+
+            ws.onerror = (error) => {
+                console.error('WebSocket error:', error);
+            };
+
+            // Clean up on unmount
+            return () => {
+                if (ws.readyState === WebSocket.OPEN) {
+                    ws.close();
+                }
+            };
         }
     }, [user?.access]);
+
+
+    useEffect(() => {
+        if (socket) {
+            socket.onmessage = (event) => {
+                setWebData(JSON.parse(event?.data));
+            };
+        }
+    }, [socket]);
+
+
 
 
 
