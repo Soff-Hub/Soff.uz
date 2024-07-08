9a02e1e13c1ff7b14fcdb055b283134ec6a0320f
Ozodbek Abdisamatov <abdisamatovozodbek003@gmail.com>
2024-07-08 12:13:20 +0500

    video compenents

--- components/elements/detail/thumbnail/DefaultVideo.jsx
+++ components/elements/detail/thumbnail/DefaultVideo.jsx
@@ -36,32 +36,26 @@ export default function DefaultVideo({
     //     }
     // };
 
-    const url = product?.document?.file_url 
-    ? product?.document?.file_url
-    : product?.document?.short_content_url
+    const url = product?.document?.file_url
+        ? product?.document?.file_url
+        : product?.document?.short_content_url
 
 
     return (
-        <video
-            // onMouseEnter={() => handleMouseLeave(product?.id)}
-            // onMouseLeave={() => handleMousePaused(product?.id)}
-            // controlsList='nodownload'
-            id={`videoPlayer-${product.id}`}
-            className={class_products ? 'border w-100' : 'video_iframe'}
-            width="100%"
-            height="auto"
-            style={{
-                background: class_products ? 'unset' : '',
-                maxHeight: class_products ? '' : '380px',
-                objectFit: 'contain',
-            }}
-            onPlay={() => setIsPlay?.(product?.id)}
-            controls
-            preload="none"
-            controlsList="nodownload"
-            poster={product?.poster_url ? product?.poster_url : product?.poster}
-            src={url}>
-            Your browser does not support the video tag.
-        </video>
+        <div className="video-container">
+            <video
+
+                id={`videoPlayer-${product.id}`}
+                className={'video_iframe'}
+                onPlay={() => setIsPlay?.(product?.id)}
+                onContextMenu={(e) => e.preventDefault()}
+                controls
+                disablePictureInPicture
+                controlsList="nodownload"
+                poster={product?.poster_url ? product?.poster_url : product?.poster}
+            >
+                <source src={url}  />
+            </video>
+        </div>
     );
 }
