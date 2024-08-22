0d57844fd1879f545d3b7c20c8be37edaa03301b
Ozodbek Abdisamatov <abdisamatovozodbek003@gmail.com>
2024-08-22 11:14:25 +0500

    template

--- components/elements/detail/TemplateDetailFullwidth.jsx
+++ components/elements/detail/TemplateDetailFullwidth.jsx
@@ -5,35 +5,29 @@ import ModuleProductDetailDescription from '~/components/elements/detail/modules
 import ModuleDetailShoppingActions from '~/components/elements/detail/modules/ModuleDetailShoppingActions';
 import ModuleDetailTopInformation from '~/components/elements/detail/modules/ModuleDetailTopInformation';
 import Link from 'next/link';
+import TemplateModuleDetailTopInformation from './modules/TemplateModuleDetailTopInformation';
+import TemplateModuleProductDetailDescription from './modules/TemplateModuleProductDetailDescription';
+import TemplateModuleDetailShoppingActions from './modules/TemplateModuleDetailShoppingActions';
 
-const ProductDetailFullwidth = ({ product, views }) => {
+const TemplateDetailFullwidth = ({ product, views }) => {
 
 
 
     return (
         <>
             <div className="ps-product--detail ps-product--fullwidth">
-                <div className="ps-product__header ">
-                    <ThumbnailDefault product={product} views={views?.view_count} />
-                    <div className="ps-product__info">
-                        <ModuleDetailTopInformation product={product} />
-
-                        <ModuleProductDetailDescription
-                            product={product}
-                            views={views}
-                        />
-                        <ModuleDetailShoppingActions
-                            product={product}
-                        />
-                        <p>Tezkor teglar</p>
-                        <div className=" d-flex justify-content-start align-content-center flex-wrap">
+                <div className="row ">
+                    <div className='col-md-8 '>
+                        <ThumbnailDefault product={product} views={views?.view_count} />
+                        <p className='mt-5 pl-3'>Tezkor teglar</p>
+                        <div className="mb-5 d-flex justify-content-start align-content-center flex-wrap">
                             {product?.tag?.length > 0 &&
                                 product?.tag.slice(0, 15).map((item, i) => (
                                     <div key={i} className="m-2 " style={{
                                         borderRadius: "20px",
                                         padding: "8px 16px",
                                         border: "1px solid #999",
-                                        fontSize:"15px"
+                                        fontSize: "15px"
                                     }}>
                                         <Link href={`/search?keyword=${item?.name?.replace(/^#/, '')}`}>
                                             <a
@@ -46,6 +40,19 @@ const ProductDetailFullwidth = ({ product, views }) => {
                                 ))}
                         </div>
                     </div>
+
+                    <div className="ps-product__info col-md-4 ">
+                        <TemplateModuleDetailTopInformation product={product} /> 
+
+                        <TemplateModuleProductDetailDescription
+                            product={product}
+                            views={views}
+                        />
+                        <TemplateModuleDetailShoppingActions
+                            product={product}
+                        />
+                    </div>
+
                 </div>
 
                 <DefaultDescription product={product} />
@@ -55,4 +62,4 @@ const ProductDetailFullwidth = ({ product, views }) => {
     );
 };
 
-export default ProductDetailFullwidth;
+export default TemplateDetailFullwidth;
