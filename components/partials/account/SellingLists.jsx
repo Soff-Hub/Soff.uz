ac8c1de9889e8d4e2ac8221299e723b5dd6f5a72
Doniyor Eshmamatov <edoniyorbekdev@gmail.com>
2024-08-30 19:40:39 +0500

    ds

--- components/partials/account/SellingLists.jsx
+++ components/partials/account/SellingLists.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import AccountMenuSidebar from './modules/AccountMenuSidebar';
-import { Form, Modal, Pagination, Select, Table, Tabs, Input, Button, Tooltip, InputNumber, Radio, DatePicker } from 'antd';
+import { Form, Modal, Pagination, Select, Table, Tabs, Input, Button, Tooltip, InputNumber, Radio, DatePicker, Checkbox } from 'antd';
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import useDebounce from '~/hooks/useDebounce';
@@ -21,8 +21,10 @@ const { Option } = Select;
 const { TextArea } = Input;
 import dayjs from 'dayjs';
 import customParseFormat from 'dayjs/plugin/customParseFormat';
+import 'dayjs/locale/uz';
 
 dayjs.extend(customParseFormat);
+// dayjs.locale('uz');
 const dateFormat = 'YYYY-MM-DD HH:mm';
 
 export const startDate = dayjs().add(2, 'day').hour(9).minute(0).format('YYYY-MM-DD HH:mm');
@@ -34,6 +36,23 @@ const disabledDate = (current) => {
 
 
 function SellingsLists() {
+    const locale = require('antd/es/date-picker/locale/uz_UZ')
+    const uzLotinLocale = {
+        ...locale,
+        lang: {
+            ...locale.lang,
+            locale: 'uz',
+            months: [
+                'Yanvar', 'Fevral', 'Mart', 'Aprel', 'May', 'Iyun',
+                'Iyul', 'Avgust', 'Sentabr', 'Oktabr', 'Noyabr', 'Dekabr',
+            ],
+            weekdays: ['Dushanba', 'Seshanba', 'Chorshanba', 'Payshanba', 'Juma', 'Shanba', 'Yakshanba'],
+            shortWeekdays: ['Du', 'Se', 'Cho', 'Pa', 'Ju', 'Sha', 'Yak'],
+        },
+    };
+
+
+
     const { accountLinks, user } = useSelector((state) => state.auth);
     const [data, setData] = useState([]);
     const [dataStatic, setDataStatic] = useState(null);
@@ -63,6 +82,8 @@ function SellingsLists() {
     const [price, setPrice] = useState('')
     const [sellMethod, setSellMethod] = useState('simple')
     const [auctionDate, setAuctionDate] = useState(startDate)
+    const [confirm, setConfirm] = useState(false)
+    const [accept, setAccept] = useState(false)
 
 
     // Sotilgan mahsulotlar listi
@@ -864,7 +885,7 @@ function SellingsLists() {
 
             <Modal
                 title={<div className='d-flex align-items-center gap-2'>
-                    <div onClick={() => (setEvaluation(null), setPrice(''))} style={{ cursor: 'pointer' }}>
+                    <div onClick={() => (setEvaluation(null), setPrice(''), setAccept(false), setConfirm(false))} style={{ cursor: 'pointer' }}>
                         <i class="fa-solid fa-arrow-left mr-2"></i>
                     </div>
                     Mahsulotni birjaga sotuvga chiqarish
@@ -876,10 +897,10 @@ function SellingsLists() {
                 width={600}
             >
 
-                {!evaluation && <p className='fs-4 mt-4 text-primary-emphasis'>
+                {/* {!evaluation && <p className='fs-4 mt-4 text-primary-emphasis'>
                     <i className='fa-solid fa-circle-info me-2 mb-3'></i>
                     Buyerda description bo'lishi kerak
-                </p>}
+                </p>} */}
 
                 {!evaluation && <label
                     className={'rounded-3 mt-3  form-label border p-0 d-flex justify-content-between align-items-center'}
@@ -997,13 +1018,19 @@ function SellingsLists() {
                                         <div className=''>
                                             <input value={price} onChange={e => setPrice(e.target.value)} className='d-flex rounded-3 align-items-center justify-content-between border p-3 w-100 my-4' placeholder={`${sellMethod === 'simple' ? 'Sotuv' : 'Auksion'} narxini kiriting`} />
                                         </div>
+
+                                        <div className='d-flex align-items-center gap-2 mb-3'>
+                                            <Checkbox checked={accept} onClick={() => setConfirm(!confirm)} />
+                                            <p className='m-0' onClick={() => setConfirm(true)} style={{ textDecoration: 'underline', cursor: 'pointer' }}>Birja.soff.uz platformasidan foydalanish qonun-qoidalari</p>
+                                        </div>
+
                                         <div className='header_table_content'>
                                             {/* <button style={{ height: "40px" }} className='btn btn-warning px-4 fs-4 w-100 d-flex align-items-center justify-content-center'>
                                                 <i class="fa-solid fa-arrow-left mr-2"></i>
                                                 Boshqa mahsulot tanlash
                                             </button> */}
 
-                                            <button style={{ height: "40px" }}
+                                            <button disabled={!accept} style={{ height: "40px" }}
                                                 onClick={() => (setOpenApplicationID(evaluation), setOpenApplication(true))}
 
                                                 className="btn btn-success fs-4 px-4 w-100">Davom etish</button>
@@ -1109,6 +1136,152 @@ function SellingsLists() {
                         <span className="fs-3">Tasdiqlash</span>
                     </button>
                 </div>
+            </Modal>
+
+            <Modal
+                title={""}
+                open={confirm}
+                onOk={() => (setAccept(true), setConfirm(false))}
+                footer={null}
+                width={800}
+                onCancel={() => (setConfirm(false))}
+                centered
+            >
+                <div className="modal-dating">
+                    <h3>
+                        Birja.soff.uz platformasidan foydalanish qonun-qoidalari
+                    </h3>
+                    <h4>1. Umumiy qoidalar</h4>
+                    <p>
+                        1.1. Siz birja.soff.uz platformasiga tashrif buyurar ekansiz, quyida belgilangan qonun-qoidalar va shartlarga rozilik bildirgan hisoblanasiz.
+                    </p>
+                    <p>
+                        1.2. Birja.soff.uz platformasida (keyingi o‘rinlarda «Platforma» deb yuritiladi) taqdim etilayotgan xizmatlar, biznes rejalar, ma’lumotlar, qo’llanmalar, referatlar, prezentatsiyalar va boshqalar shartli ravishda mahsulot deb nomlanadi va ularning barchasi faqat Soff.uz qidiruv tizimida mavjud bo’lgan mahsulotlar bo’la oladi. Ularga Platmormada faqatgina egalik huquqlarini amlashishi imkoniyatlarini yaratib beriladi.
+                    </p>
+                    <h4>2. Sotuvchi bo’lib ro’yxatdan o’tish</h4>
+                    <p>
+                        2.1. Platformada Soff.uzdagi o’z mahsulotlarini egalik huqularini sotish yoki aksincha boshqa sotuvchilarga tegishli bo’lgan mahsulotlarning egalik huquqlarini sotib olish orqali daromad qilish va boshqalarni amalga oshirish uchun Platformadan «Sotuvchi» bo’lib ro’yxatdan o’tish lozim. Ro’yxatdan o’tish davomida sotuvchi barcha ma’lumotlar haqqoniy va to’g’ri to’ldirilishiga javobgar hisoblanadi.
+                    </p>
+                    <h4>3. Sotuvchi mahsulotlarini birja.soff.uz birjasiga sotuvga chiqarish shartlari</h4>
+                    <p>
+                        3.1. Sotuvchi mahsulotlarining egalik huqularini birja.soff.uz birjasiga sotuvga chiqarar ekan, mahsulot o‘z muallifligi ostida bo‘lishi shart.
+                    </p>
+                    <p>
+                        3.2. Sotuvchi Platformada mahsulotining faqat egalik huquqini sota oladi, mualliflik huquqi esa saqlanib qoladi.
+                    </p>
+                    <p>
+                        3.3. Sotuvchi o’z mahsulotining egalik huquqini Platformada sotib yuborar ekan, Soff.uz’dan aynan o’sha mahsulotdan kelgusida keladigan daromadlarini ham butunlay egalik huquqi bilan birga o'tkazadi. Shuning uchun sotuvchilarga mahsulotlarining egalik huquqini sotishdan oldin diqqat bilan qaror qabul qilish tavsiya etiladi. Yuqorida takidlaganimizdek mahsulotning faqat egalik huquqi va kelgusi barcha daromadlari o’tadi lekin mualliflik doimiy saqlanib qoladi.
+                    </p>
+                    <p>
+                        3.4 Mahsulotning egalik huquqi miqdori muallifning hohishi bilan belgilanadi va mahsulotning ayni paytdagi narxidan bir necha barobar katta bo'lishi ham mumkin.
+                    </p>
+                    <p>
+                        3.5 Muallif o’z mahsulotining egalik huquqini sotganidan so’ng, kelgusida yana shu mahsulotning egalik huquqlari boshqa bir sotuvchiga o’tgan vaqtda, mahsulotning egalik huquqi miqdori sumasidan 5% mahsulot muallifiga o'tadi va bu holat doimiy ravishda takrorlanib boradi.
+                    </p>
+                    <h4>
+                        4. Mahsulotning egalik huquqini sotish shakllari
+                    </h4>
+                    <p>
+                        4.1. Mahsulotlarni egalik huqulari birja.soff.uz birjasida ikki xil shaklda sotish mumkin: «Bittada sotish» yoki «Auksionda sotish».
+                    </p>
+                    <p>
+                        4.2 Bittada Sotish: Sotuvchi mahsulotning egalik huquqi uchun ma’lum bir narx belgilaydi va sotish jarayoni shu narxda amalga oshiriladi. Belgilangan vaqt ichida mahsulotning egalik huquqi sotilmasa, u qayta sotuvchining o’zida qoladi. Sotuvchi o’z hohshiga qarab qayta va qayta birja.soff.uz birjasiga mahsulotining egalik huquqini sotuvga chiqarishi mumkin bo‘ladi.
+                    </p>
+                    <p>
+                        4.3. Auksionda Sotish: Sotuvchi mahsulotning egalik huquqi uchun boshlang‘ich narx belgilaydi va boshqa sotuvchilar ushbu mahsulotning egalik huquqi uchun o‘z takliflarini berishadi. Vaqt tugaganidan so‘ng eng yuqori taklif beruvchiga mahsulotning egalik huquqi o‘tkaziladi.
+                    </p>
+                    <h4>
+                        5. Sotuvchilarning huquq va majburiyatlari
+                    </h4>
+                    <h5>
+                        5.1. Huquqlar:
+                    </h5>
+                    <p>
+                        5.1.1. Platforma tizimida erkin ro’yxatdan o’tish;
+                    </p>
+                    <p>
+                        5.1.2. Platforma tomonidan taqdim etilayotgan xizmatlarga o’z muallifligi ostidagi mahsulotlarini qo’shish;
+                    </p>
+                    <p>
+                        5.1.3. Xarid qilingan mahsulotlarning egalik huquqidan erkin foydalanish (mahsulotlardan tijorat hamda foyda olish maqsadida Soff.uz’dan boshqa saytlardan foydalanish taqiqlanadi);
+                    </p>
+                    <p>
+                        5.1.4. Sotuvchi tomonidan xarid qilingan mahsulotning egalik huquqi, kelgusida Soff.uz qidiruv tizimida bu mahsulotdan kelgan daromadning barchasiga egalik qilishi.
+                    </p>
+                    <p>
+                        5.1.5. Muallif sotib yuborgan mahsulotning egalik huquqidan so’ng, kelgusida bu mahsulotning egalik huquqi har safar almashilgandagi pul miqdorning 5% qismiga sherik bo’lishi(4.5 bandga ko’ra).
+                    </p>
+                    <p>
+                        5.1.6. Sotuvchi o’z hohshiga qarab Platformaga xarid qilgan mahsulotlarining egalik huquqini qayta sotuvga chiqarish mumkin.
+                    </p>
+                    <p>
+                        5.1.7. Xarid qilingan mahsulotlarning egalik huquqi bo’yicha xabarnomalarni ro’yxatdan o’tish davomida sotuvchi tomonidan kiritilgan elektron pochta (e-mail) orqali qabul qilib olish yoki o’zining shaxsiy profilidagi «Mahsulotlar» sahifasiga kirish orqali qabul qilib olish;
+                    </p>
+                    <p>
+                        5.1.8. Sotuvchining sotilgan har bir mahsulotning egalik huquqi bo’yicha xabarnomalarni ro’yxatdan o’tish davomida sotuvchi tomonidan kiritilgan elektron pochta (e-mail) orqali qabul qilib olish yoki o’zining shaxsiy profilidagi «Mahsulotlar» sahifasiga kirish orqali qabul qilib olish;
+                    </p>
+                    <p>
+                        5.1.9. Sotuvchining sotilgan mahsulotlarining egalik huquqi yig’indisidan platforma ulushining (sotilgan mahsulotning egalik huquqi 5% i) qiymatini ayirgan holatda hosil bo’lgan summani o’z kartalariga tashlab olish;
+                    </p>
+                    <p>
+                        5.1.10. Platforma ma’muriyatiga murojaat qilish huquqlariga ega.
+                    </p>
+                    <h5>5.2. Majburiyatlar:</h5>
+                    <p>
+                        5.2.1. Sotuvchi yoki muallif sotib yuborgan mahsulotning egalik huquqidan so’ng, kelgusida Soff.uz qidiruv tizimida bu mahsulotdan kelgan daromadga egalik qila olmasligi.
+                    </p>
+                    <p>
+                        5.2.2. Sotuvchi Platforma tizimida ro’yxatdan o’tish davmida so’ralgan ma’lumotlarni to’g’ri kiritishi shart;
+                    </p>
+                    <p>
+                        5.2.3. Platforma sotuvchilari o’zaro aloqalar davomida beadab, haqoratli va behurmat qiladigan jumlalardan foydalanish hamda O’zbekiston Respublikasining amaldagi qonunchiligida taqiqlangan boshqa ma’lumotlarni o’z ichiga oluvchi xabarlarni jo’natish taqiqlanadi.
+                    </p>
+                    <p>
+                        5.2.4. Ro’yxatdan o’tish jarayonida amalda mavjud bo’lgan, ishlaydigan elektron manzil (e-mail) kiritish shart;
+                    </p>
+                    <p>
+                        5.2.5. Sotuvchining yig’ilgan summasini kartasiga tashlab olish jarayonidan uning miqdori 10 ming so’mdan kam bo’lmasligi shart.
+                    </p>
+                    <p>
+                        5.2.6. Sotuvchi platformada sotilgan har bir mahsulotning egalik huquqi 5% ini platforma ma’muriyati olib qolishi shart.
+                    </p>
+                    <p>
+                        5.2.7. Sotuvchi Platformaning foydalanish qonun-qoidalarida belgilangan talablarni to’liq bajarishi shart.
+                    </p>
+                    <h4>6. Javobgarlik</h4>
+                    <p>
+                        6.1. Platforma ma’muriyati sotuvchilarning noto’g’ri ma’lumot kiritishi va amallarni bajarishi sababli xarid qilish jarayonida yuzaga kelgan zarar, ziyon yoki xarajatlar uchun javobgar emas
+                    </p>
+                    <p>
+                        6.2. Sotuvchi tomonidan xarid qilingan mahsulotning egalik huquqi sotuvchining Platformadagi o’z shaxsiy kabinetiga joylashtiriladi hamda elektron manziliga xabarnoma yuboriladi. Taqdim etilgan mahsulotning egalik huquqi sotuvchi tomonidan yuklab olinish, olinmasligi va boshqa har qanday holatlardan qat’iy nazar, mahsulotning egalik huquqi sotuvchi tomonidan xarid qilingan va Platforma tizimida yetkazib berildi deya belgilanadi hamda amalga oshirilgan summa qaytarilmaydi. Shu sababli, ro’yxatdan o’tish davrida amalda mavjud bo’lgan, ishlaydigan elektron manzil (e-mail) kiritish va to’lovni muvaffaqiyatli amalga oshirganingizdan so’ng e-mailingizni ham tekshirib turish zarur.
+                    </p>
+                    <p>
+                        6.3. Platformada ro’yxatdan o’tish davomida kiritilgan ma’lumotlar haqqoniyligi uchun butun javobgarlik foydalanuvchi va sotuvchining o’z bo’ynida bo’ladi.
+                    </p>
+                    <p>
+                        6.4. Platforma ma’muriyati to’lovni amalga oshirish jarayonida yuzaga kelgan zarar uchun javobgar emas.
+                    </p>
+                    <h4>7. Sotuvchining shaxsiy ma’lumotlarning daxlsizligi</h4>
+                    <p>
+                        7.1. Sotuvchining shaxsiy ma’lumotlarining daxlsizligi biz uchun juda muhim.
+                    </p>
+                    <p>
+                        7.2. Biz quyidagi ma’lumotlarni statistik ko’rsatgichlarni aniqlash maqsadida to’playmiz va saqlaymiz:
+                    </p>
+                    <div>
+                        <ul className='ms-4'>
+                            <li>Sotuvchining Platformaga kirish uchun foydalangan IP-manzili;</li>
+                            <li>Platformaga kirish uchun sotuvchi tomonidan ishlatilgan brauzer va operatsion tizim turi;</li>
+                            <li>Platformaga kirish muddatlari;</li>
+                            <li>Sotuvchi ko’rib chiqqan sahifalar va havolalar.</li>
+                        </ul>
+                    </div>
+                    <p>
+                        7.3. Biz ushbu manzillardan foydalanuvchining shaxsini aniqlash maqsadida foydalanmaymiz, saytga qilingan hujumlar bundan mustasno.
+                    </p>
+                </div>
+
+                <button onClick={() => (setAccept(true), setConfirm(false))} style={{ height: "40px" }} className="btn btn-success fs-4 px-4 w-100 mt-3">Tanishib chiqdim</button>
             </Modal>
 
         </section >
