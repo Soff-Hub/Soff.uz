71b9b231c82d91179f96620dff8e16c3bdf378f2
Doniyor Eshmamatov <edoniyorbekdev@gmail.com>
2024-09-27 17:48:44 +0500

    header

--- components/shared/headers/HeaderElectronic.jsx
+++ components/shared/headers/HeaderElectronic.jsx
@@ -27,7 +27,26 @@ const HeaderElectronic = () => {
         <header
             className="header header--standard header--electronic "
             id={run ? "" : "headerSticky"}>
+
             <div className="header__content">
+                <div className='header-topsection'>
+                    <div className="container">
+                        <div className='sotuvchi-boling sotuvchi-boling-2'>
+                            <a href='tel:+998910086789'>
+                                <i class="fa-solid fa-phone mr-2"></i>
+                                <span>{'+998 (91) 008 67 89'}</span>
+                            </a>
+
+                            <a href='https://t.me/soff_uz' target='_blank' >
+                                <i class="fa-brands fa-telegram me-2"></i>
+                            </a>
+                        </div>
+                        <div className='sotuvchi-boling'>
+                            <a href='https://seller.soff.uz' target='_blank'>Sotuvchi bo'lish</a>
+                            <i class="fa-solid fa-user-check"></i>
+                        </div>
+                    </div>
+                </div>
                 <div className="container">
                     <div className="header__content-left">
                         <Link href="/">
--- components/shared/headers/HeaderMobileElectronic.jsx
+++ components/shared/headers/HeaderMobileElectronic.jsx
@@ -11,6 +11,25 @@ class HeaderMobileElectronic extends Component {
     render() {
         return (
             <header className="header header--mobile electronic">
+                <div className='header-topsection'>
+                    <div className="container">
+                        <div className='sotuvchi-boling sotuvchi-boling-2'>
+                            <a href='tel:+998910086789'>
+                                <i class="fa-solid fa-phone mr-2"></i>
+                                <span>{'+998 (91) 008 67 89'}</span>
+                            </a>
+
+                            <a href='https://t.me/soff_uz' target='_blank' >
+                                <i class="fa-brands fa-telegram me-2"></i>
+                            </a>
+                        </div>
+                        <div className='sotuvchi-boling'>
+                            <a href='https://seller.soff.uz' target='_blank'>Sotuvchi bo'lish</a>
+                            <i class="fa-solid fa-user-check"></i>
+                        </div>
+                    </div>
+                </div>
+
                 <div className="navigation--mobile">
                     <div className="navigation__left">
                         <Link href="/">
