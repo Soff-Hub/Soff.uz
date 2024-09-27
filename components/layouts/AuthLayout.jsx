474e85ceb6026fe01d14f68bd5ca9b9f09bd59f9
Doniyor Eshmamatov <edoniyorbekdev@gmail.com>
2024-09-27 18:19:04 +0500

    scroll bug

--- components/layouts/AuthLayout.jsx
+++ components/layouts/AuthLayout.jsx
@@ -25,7 +25,7 @@ export default function AuthLayout({ children }) {
     }, [user])
 
     return (
-        <div className='row bg-white' style={{ height: '100vh', overflow: 'hidden' }} >
+        <div className='row bg-white' style={{ height: '100vh', overflow: 'hidden', width: '100%' }} >
             <div className='col-md-5 auth-img'>
                 <div className="auth-header p-4">
                     <img src={`/static/img/auth/logo-dark.jpg`} alt="soff.uz logo" style={{ marginLeft: '100px' }} width={140} />
