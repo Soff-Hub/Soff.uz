2c942946173927af6c90c0a82d275fe392fe3589
Doniyor Eshmamatov <edoniyorbekdev@gmail.com>
2024-09-28 15:51:46 +0500

    b

--- pages/auth/code-verify.jsx
+++ pages/auth/code-verify.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
 
 import Meta from '~/components/shared/headers/Meta';
-// import BreadCrumb from '~/components/elements/BreadCrumb';
+import BreadCrumb from '~/components/elements/BreadCrumb';
 
 import PageContainer from '~/components/layouts/PageContainer';
 import FooterDefault from '~/components/shared/footers/FooterDefault';
 import CodeVerifyForm from '~/components/partials/account/auth/CodeVerifyForm';
 
 const CodeVerifyPage = () => {
-    // const breadCrumb = [
-    //     {
-    //         text: 'Asosiy sahifa',
-    //         url: '/',
-    //     },
-    //     {
-    //         text: "Kod tasdiqlash",
-    //     }
-    // ];
+    const breadCrumb = [
+        {
+            text: 'Asosiy sahifa',
+            url: '/',
+        },
+        {
+            text: "Kirish",
+            url: '/auth/login'
+        },
+        {
+            text: "Tasdiqlash",
+        },
+    ];
 
     return (
         <PageContainer footer={<FooterDefault />} title="Register">
@@ -25,7 +29,7 @@ const CodeVerifyPage = () => {
                     title={"Ro'yxatdan o'tish"}
                     description="Soff.uz - Saytidan hoziroq ro'yxatdan o'ting va o'z mahsulatlaringizni soting"
                 />
-                {/* <BreadCrumb breacrumb={breadCrumb} /> */}
+                <BreadCrumb breacrumb={breadCrumb} />
                 <CodeVerifyForm />
             </div>
         </PageContainer>
