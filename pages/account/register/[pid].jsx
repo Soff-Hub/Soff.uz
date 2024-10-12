5c25c9868d4ab70bf7bd2568f08d4dcb1e043eef
Doniyor Eshmamatov <edoniyorbekdev@gmail.com>
2024-10-12 16:31:03 +0500

    dsd

--- pages/account/register/[pid].jsx
+++ pages/account/register/[pid].jsx
@@ -1,45 +1,19 @@
-import React, { useEffect } from 'react';
+import React from 'react'
 
-import BreadCrumb from '~/components/elements/BreadCrumb';
-import Register from '~/components/partials/account/Register';
-import PageContainer from '~/components/layouts/PageContainer';
-import FooterDefault from '~/components/shared/footers/FooterDefault';
-import { useSelector } from 'react-redux';
-import { useRouter } from 'next/router';
-import Mistake from '../mistake';
-import Meta from '~/components/shared/headers/Meta';
-
-const RegisterPage = () => {
-    const Router = useRouter()
-    const { pid } = Router.query
-    const breadCrumb = [
-        {
-            text: 'Asosiy sahifa',
-            url: '/',
-        },
-        {
-            text: "Ro'yxatdan o'tish",
-        },
-    ];
-    const { user } = useSelector(state => state.auth)
-    const sallerEndPoint = `auth/seller-register/${pid}/`;
-
-
+function Search() {
     return (
-        user?.access ?
-            <Mistake />
-            :
-            <PageContainer footer={<FooterDefault />} title="Register">
-                <div className="ps-page--my-account">
-                    <Meta
-                        title={"Ro'yxatdan o'tish"}
-                        description="Soff.uz - O'z hisobingizga kiring va mahsulotlaringizni soting"
-                    />
-                    <BreadCrumb breacrumb={breadCrumb} />
-                    <Register url={sallerEndPoint} />
-                </div>
-            </PageContainer>
-    );
-};
+        <div>Redirecting...</div>
+    )
+}
 
-export default RegisterPage;
+export async function getServerSideProps({ params }) {
+    return {
+        redirect: {
+            destination: `https://seller.soff.uz/account/register/${params?.pid}`,
+            permanent: true,
+        },
+    };
+}
+
+
+export default Search
