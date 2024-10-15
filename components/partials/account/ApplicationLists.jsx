11d1e1c56cf25d2f5f59b84742091c12930ff850
Doniyor Eshmamatov <edoniyorbekdev@gmail.com>
2024-10-15 16:21:01 +0500

    dsd

--- components/partials/account/ApplicationLists.jsx
+++ components/partials/account/ApplicationLists.jsx
@@ -565,7 +565,7 @@ function ApplicationLists() {
                         <div className="ps-page__content ">
                             <div className="ps-section--account-setting ">
 
-                                {true ? <div className="ps-section__content ">
+                                {user?.role === 'seller' ? <div className="ps-section__content ">
                                     <div className='border py-4 rounded '>
                                         {dataBlock?.has_blocked ? <div className=' px-4'>
                                             <span className='text-danger fw-bold '>
