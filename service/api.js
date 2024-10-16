4f9fd4e2cc2f1b055a63fad1b7879a37a1c17147
Doniyor Eshmamatov <edoniyorbekdev@gmail.com>
2024-10-16 11:40:30 +0500

    add interceptor

---
+++ service/api.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+const baseURL =
+    process.env.NODE_ENV === 'development'
+        ? process.env.NEXT_PUBLIC_LOCAL_BASE_URL
+        : process.env.NEXT_PUBLIC_BASE_URL;
+
+const api = axios.create({
+    baseURL,
+});
+
+api.interceptors.request.use((config) => {
+    const storedUser = localStorage.getItem('user');
+
+    if (storedUser) {
+        config.headers['Authorization'] = `Bearer ${
+            JSON.parse(storedUser)?.access
+        }`;
+    }
+
+    return config;
+});
+
+export default api;
