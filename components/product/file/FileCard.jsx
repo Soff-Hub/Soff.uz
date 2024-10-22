a6e7f8d3d1fd7a297691f8c4e981169d32beddf2
Doniyor Eshmamatov <edoniyorbekdev@gmail.com>
2024-10-22 18:28:14 +0500

    product card

--- components/product/file/FileCard.jsx
+++ components/product/file/FileCard.jsx
@@ -1,26 +1,31 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 import { formatCurrencyWithSpace } from '~/utilities/product-helper'
 
 export default function FileCard() {
     return (
         <div className='file-card'>
-            <div className="file-card-inner">
-                <div className="file-card-img">
-                    <Image
-                        src={'https://eu2.contabostorage.com/20ddac7ab90d4d188d1ca104120b91ed:soffuz/media/Images/Kalonka_detaliga_mexanik_ishlov_berish_samaradorlix63.docx_page-1_generate.jpg'}
-                        width={300}
-                        height={400}
-                        alt=''
-                    />
-                </div>
-                <div className="file-card-content">
-                    <h3 className='file-card-title'>
-                        Transportda yuklarni tashishning taqsimlanishi
-                    </h3>
-                    <ins className='file-card-price'>{formatCurrencyWithSpace(10000)} so'm</ins>
-                </div>
-            </div>
+            <Link href="#">
+                <a>
+                    <div className="file-card-inner">
+                        <div className="file-card-img">
+                            <Image
+                                src={'https://eu2.contabostorage.com/20ddac7ab90d4d188d1ca104120b91ed:soffuz/media/Images/Kalonka_detaliga_mexanik_ishlov_berish_samaradorlix63.docx_page-1_generate.jpg'}
+                                width={300}
+                                height={400}
+                                alt=''
+                            />
+                        </div>
+                        <div className="file-card-content">
+                            <h3 className='file-card-title'>
+                                Transportda yuklarni tashishning taqsimlanishi
+                            </h3>
+                            <ins className='file-card-price'>{formatCurrencyWithSpace(10000)} so'm</ins>
+                        </div>
+                    </div>
+                </a>
+            </Link>
         </div>
     )
 }
