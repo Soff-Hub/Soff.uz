8e0ed9dc608b2c0c446e8977e6bd67b339fa708d
Doniyor Eshmamatov <edoniyorbekdev@gmail.com>
2024-10-31 22:52:27 +0500

    dfddd

--- next.config.js
+++ next.config.js
@@ -6,12 +6,13 @@ const nextSettings = {
     },
     // Change your site title here
     env: {
-        title: 'Soff',
+        title: 'Soff uz',
         titleDescription:
             'Soff.uz - Intellektual mulk marketi, Intellektual mahsulotlarini soting va xarid qiling.',
     },
+    // O'zgarish: 'experimental.serverActions' ni olib tashlash
     experimental: {
-        serverActions: true,
+        // serverActions: true, // Buni olib tashlang
     },
     images: {
         remotePatterns: [
@@ -26,10 +27,11 @@ const nextSettings = {
         ],
         domains: ['eu2.contabostorage.com'],
     },
-    presets: [
-        ['@babel/preset-env'],
-        ['@babel/preset-react', { runtime: 'automatic' }],
-    ],
+    // O'zgarish: 'presets' ni olib tashlash
+    // presets: [
+    //     ['@babel/preset-env'],
+    //     ['@babel/preset-react', { runtime: 'automatic' }],
+    // ],
 };
 
 module.exports = nextSettings;
