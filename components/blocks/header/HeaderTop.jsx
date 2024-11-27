55122a2c73b499a883bda1f77b62646be27e40af
Doniyor Eshmamatov <edoniyorbekdev@gmail.com>
2024-11-27 11:56:32 +0500

    sotuvchi bolish

--- components/blocks/header/HeaderTop.jsx
+++ components/blocks/header/HeaderTop.jsx
@@ -6,26 +6,24 @@ export default function HeaderTop() {
     const { pathname } = useRouter()
     const isDark = pathname !== '/'
     return (
-        <div className='header-topsection ' style={{ backgroundColor: isDark ? "" : 'transparent' }}>
+        <div className='header-topsection mb-2' style={{ backgroundColor: '#00a44f' }}>
             <div className="container">
                 <div className='sotuvchi-boling sotuvchi-boling-2'>
-                    <a href='tel:+998910086789'>
-                        <i className={`fa-solid fa-phone mr-2 ${isDark ? 'text-black' : 'text-white'}`}></i>
-                        <span className={isDark ? 'text-black' : 'text-white'}>{'+998 (91) 008 67 89'}</span>
-                    </a>
-
                     <a href='https://t.me/soff_uz' target='_blank' >
-                        <i className={`fa-brands fa-telegram me-1 phone-ic fs-4 ${isDark ? 'text-black' : 'text-white'}`}></i>
+                        <i className={`fa-brands fa-telegram me-1 phone-ic fs-4 text-white`}></i>
                     </a>
 
                     <a href='https://www.instagram.com/soffuz_/' target='_blank' >
-                        <i className={`fa-brands fa-instagram me-1 phone-ic fs-4 ${isDark ? 'text-black' : 'text-white'}`}></i>
+                        <i className={`fa-brands fa-instagram me-1 phone-ic fs-4 text-white`}></i>
                     </a>
                 </div>
                 <div className='sotuvchi-boling'>
-                    {/* <i className={`fa-solid fa-user-check fs-4 ${isDark ? 'text-black' : 'text-white'}`}></i> */}
                     <PremiumBadge />
-                    <a href='https://seller.soff.uz' target='_blank' className={`fs-4`} style={{ color: '#00A44F !important' }}>Sotuvchi bo'lish</a>
+                    <a href='https://seller.soff.uz' target='_blank' className={`fs-4 text-white`}>Sotuvchi bo'lish</a>
+                    <a href='tel:+998910086789' className='ml-3'>
+                        <i className={`fa-solid fa-phone mr-2 text-white`}></i>
+                        <span className={'text-white'}>{'+998 (91) 008 67 89'}</span>
+                    </a>
                 </div>
             </div>
         </div>
