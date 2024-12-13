5098d57eaf5a50a45681d52c2e13dd0a94d6a57a
macbook <macbook@MacBook-Pro-macbook.local>
2024-12-13 23:27:40 +0500

    added yandex and google monitization

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -111,6 +111,9 @@ export default function Document() {
                     `,
                         }}></script>
                 )}
+
+            <script>window.yaContextCb=window.yaContextCb||[]</script>
+            <script src="https://yandex.ru/ads/system/context.js" async></script>
             </Head>
             <body>
                 <Main />
