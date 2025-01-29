5c8c6d53df1962055b7427934c84309dfde624db
Zufarbek <zufarbek.abdurakhmonov@gmail.com>
2025-01-29 13:55:43 +0500

    title is changed

--- components/blocks/header/headerTitle.jsx
+++ components/blocks/header/headerTitle.jsx
@@ -1,22 +1,13 @@
 import React from 'react';
 
-export default function HeaderTitle () {
+export default function HeaderTitle() {
     return (
         <div className='text-center py-4 mt-0 headerTitle'>
             <h2 className='w-50 mx-auto fw-normal'>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Molestias, amet?
+                Mahsulotlarni Filtrlar yordamida Tez va Qulay Qidirish
             </h2>
             <p className='w-50 mx-auto py-3'>
-                O‘zingizga kerakli materiallarni topish uchun filterlardan
-                foydalaning. Har bir materialni o‘rganish, tahlil qilish yoki
-                ilmiy ishlaringizda qo‘llash uchun sotib olishingiz mumkin.
-                Bepul materiallar ham mavjud bo‘lib, ular darhol yuklab olinishi
-                mumkin. Ushbu resurslar mahalliy mutaxassislar tomonidan
-                tayyorlangan va sizga yuqori sifatli, ishonchli materiallarni
-                taqdim etadi. Ilmiy va amaliy ehtiyojlaringizga mos
-                materiallarni topish uchun filterlardan foydalaning va
-                o‘zingizga kerakli resurslarni toping!
+            Har bir materialni o‘rganish, tahlil qilish yoki ilmiy ishlaringizda qo‘llash uchun sotib olishingiz mumkin. Bepul materiallar ham mavjud bo‘lib, ular darhol yuklab olinishi mumkin. Ushbu resurslar mahalliy mutaxassislar tomonidan tayyorlangan va sizga yuqori sifatli, ishonchli materiallarni taqdim etadi. I
             </p>
             <a className='px-5 py-4 bg-success btn btn-success rounded-5 text-white font-weight-bold fs-4 text-xl hover-overlay '>
                 Buyurma bering
