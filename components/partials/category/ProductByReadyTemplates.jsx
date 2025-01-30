19741dc8268eac5e7f44d10b2c29e4bc626770d9
AnvarMirsaatov <anvarmirsaatov6358@gmail.com>
2025-01-30 15:57:25 +0500

    ready templates page is ready

---
+++ components/partials/category/ProductByReadyTemplates.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+import { Pagination } from 'antd';
+import { Skeleton } from 'antd'
+import ReadyTemplatesProduct from '~/components/elements/products/ReadyTemplatesProduct';
+
+export default function ProductByReadyTemplates({ data = [], page, handlePagination, isLoading }) {
+
+    return (
+        <div id='products'>
+            <div className="row">
+                {
+                    data && (data?.results?.map((item, index) => (
+                        <div
+                            className="home-card col-xl-3 col-lg-3 col-md-4 col-sm-6 col-xs-3 col-6"
+                            key={item.id}>
+                            <ReadyTemplatesProduct product={item} />
+                        </div>
+                    )))
+                }
+                {
+                    isLoading && <div className={`product-list p-loading`}>
+                        {Array(15).fill(0).map((d, i) => <Skeleton.Image
+                            key={i}
+                            active
+                            className={`skeletion-card file`}
+                        />)
+                        }
+                    </div>
+                }
+
+            </div>
+            {
+                data?.count >= 48 && (
+                    <div className="text-center my-4">
+                        <Pagination
+                            total={data?.count}
+                            pageSize={48}
+                            responsive={true}
+                            showSizeChanger={false}
+                            current={page}
+                            showTotal={(total, range) =>
+                                `${total} ta dan ${range[0]}-${range[1]} oralig'i `
+                            }
+                            onChange={(e) => handlePagination(e)}
+                        />
+                    </div>
+                )
+            }
+        </div >
+    )
+}
---
+++ pages/ready-templates/[slug].jsx
@@ -0,0 +1,71 @@
+import { useRouter } from 'next/router';
+import React from 'react';
+import HeaderTitle from '~/components/blocks/header/HeaderTitle';
+import CategoriesFilterSecion from '~/components/elements/CategoriesFilterSecion';
+import PageContainer from '~/components/layouts/PageContainer';
+import ProductByReadyTemplates from '~/components/partials/category/ProductByReadyTemplates';
+import FooterDefault from '~/components/shared/footers/FooterDefault';
+import Meta from '~/components/shared/headers/Meta';
+import useApi, { baseUrlUseApi } from '~/repositories/useApi';
+
+export default function ReadyTemplates () {
+    const router = useRouter();
+    const { slug, page, parentCategory, childCategory } = router.query;
+
+    // products API uchun so'rov
+    const { data, error, isLoading } = useApi(
+        ['products', slug, page, parentCategory, childCategory], // queryKey dinamik
+        `${baseUrlUseApi}customer/products/?type=file&category=${
+            childCategory ? childCategory : parentCategory
+        }&page=${page || 1}&page_size=48`,
+        'GET'
+    );
+
+    // Four-child API uchun so'rov
+    const {
+        data: fourChildData,
+        error: fourChildError,
+        isLoading: isFourChildLoading,
+    } = useApi(
+        ['fourChild'], // Query key
+        `${baseUrlUseApi}customer/four-child?type=file`,
+        'GET'
+    );
+
+    // Pagination tugmalari uchun funksiya
+    const handlePageChange = newPage => {
+        router.push({
+            pathname: router.pathname,
+            query: { ...router.query, page: newPage }, // URL'ga yangi page qo'shish
+        });
+    };
+
+    return (
+        <PageContainer
+            footer={<FooterDefault />}
+            title={'Kategoriya'}
+            boxed={true}>
+            <Meta
+                title={`${'asdf'}`}
+                description={`Biz siz qidirayotgan mahsulotlarni Soff.uz saytimizning kategoriyasida topdik`}
+            />
+            <HeaderTitle />
+
+            <div className='ps-page--shop container'>
+                <CategoriesFilterSecion
+                    breacrumb={fourChildData}
+                    count={data?.count}
+                    isLoading={isFourChildLoading}
+                />
+                <ProductByReadyTemplates
+                    data={data}
+                    page={page}
+                    handlePagination={number => {
+                        handlePageChange(number);
+                    }}
+                    isLoading={isLoading}
+                />
+            </div>
+        </PageContainer>
+    );
+}
