755c1d371314ae3bd3faf864b3e7dc0f5e236c04
AnvarMirsaatov <anvarmirsaatov6358@gmail.com>
2025-01-30 14:42:59 +0500

    3d models and interior design page is ready

--- pages/models-and-designs/[slug].jsx
+++ pages/models-and-designs/[slug].jsx
@@ -1,34 +1,40 @@
+import { useRouter } from 'next/router';
 import React from 'react';
+import HeaderTitle from '~/components/blocks/header/HeaderTitle';
+import ModelAndDesignHero from '~/components/blocks/header/ModelAndDesignHero';
+import CategoriesFilterSecion from '~/components/elements/CategoriesFilterSecion';
 import PageContainer from '~/components/layouts/PageContainer';
+import ProductsByModelsAndDesignCategory from '~/components/partials/category/ProductsByModelsAndDesignCategory';
 import FooterDefault from '~/components/shared/footers/FooterDefault';
 import Meta from '~/components/shared/headers/Meta';
-import ProductsByCategory from '~/components/partials/category/ProductsByCategory';
-import { useRouter } from 'next/router';
-import CategoriesFilterSecion from '~/components/elements/CategoriesFilterSecion';
-import HeaderTitle from '../../components/blocks/header/HeaderTitle';
 import useApi, { baseUrlUseApi } from '~/repositories/useApi';
 
-export default function ProductCategoryScreen() {
+export default function ModelsAndInteriorDesign () {
     const router = useRouter();
     const { slug, page, parentCategory, childCategory } = router.query;
 
     // products API uchun so'rov
     const { data, error, isLoading } = useApi(
-        ["products", slug, page, parentCategory, childCategory], // queryKey dinamik
-        `${baseUrlUseApi}customer/products/?type=file&category=${childCategory ? childCategory : parentCategory}&page=${page || 1}&page_size=48`,
-        "GET"
+        ['products', slug, page, parentCategory, childCategory], // queryKey dinamik
+        `${baseUrlUseApi}customer/products/?type=file&category=${
+            childCategory ? childCategory : parentCategory
+        }&page=${page || 1}&page_size=48`,
+        'GET'
     );
 
-
     // Four-child API uchun so'rov
-    const { data: fourChildData, error: fourChildError, isLoading: isFourChildLoading } = useApi(
-        ["fourChild"], // Query key
+    const {
+        data: fourChildData,
+        error: fourChildError,
+        isLoading: isFourChildLoading,
+    } = useApi(
+        ['fourChild'], // Query key
         `${baseUrlUseApi}customer/four-child?type=file`,
-        "GET"
+        'GET'
     );
 
     // Pagination tugmalari uchun funksiya
-    const handlePageChange = (newPage) => {
+    const handlePageChange = newPage => {
         router.push({
             pathname: router.pathname,
             query: { ...router.query, page: newPage }, // URL'ga yangi page qo'shish
@@ -44,15 +50,16 @@ export default function ProductCategoryScreen() {
                 title={`${'asdf'}`}
                 description={`Biz siz qidirayotgan mahsulotlarni Soff.uz saytimizning kategoriyasida topdik`}
             />
-
             <HeaderTitle />
+            <ModelAndDesignHero/>
+
             <div className='ps-page--shop container'>
                 <CategoriesFilterSecion
                     breacrumb={fourChildData}
                     count={data?.count}
                     isLoading={isFourChildLoading}
                 />
-                <ProductsByCategory
+                <ProductsByModelsAndDesignCategory  
                     data={data}
                     page={page}
                     handlePagination={number => {
@@ -61,7 +68,6 @@ export default function ProductCategoryScreen() {
                     isLoading={isLoading}
                 />
             </div>
-
         </PageContainer>
     );
 }
