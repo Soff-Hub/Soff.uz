42cbfa738686f78162f059f83cd021f08d8d94df
Zufarbek <zufarbek.abdurakhmonov@gmail.com>
2025-02-02 16:24:18 +0500

    fixed yandex adds styles

--- components/elements/detail/thumbnail/ThumbnailDefault.jsx
+++ components/elements/detail/thumbnail/ThumbnailDefault.jsx
@@ -32,50 +32,50 @@ const ThumbnailDefault = ({ product, vertical = true, views }) => {
     }, [product]);
 
     return (
-        <div
-            className="ps-product__thumbnail full-detail-image"
-            data-vertical={vertical ? 'true' : 'false'}>
-            <figure style={{ flex: '1', minWidth: '100%' }}>
-                <div className="ps-wrapper d-block p-4" ref={wrapperRef}>
-                    
-                    {/* Yandex reklama kodi */}
-                    <div id="yandex_rtb_R-A-13331140-2"></div>
-                    {/* Yandex scriptni yuklash */}
-                    <Script
-                        src="https://yandex.ru/ads/system/context.js"
-                        strategy="lazyOnload"
-                        onLoad={() => {
-                            if (window.yaContextCb) {
-                                window.yaContextCb.push(() => {
-                                    Ya.Context.AdvManager.render({
-                                        blockId: "R-A-13331140-2",
-                                        renderTo: "yandex_rtb_R-A-13331140-2"
-                                    });
-                                });
-                            }
-                        }}
-                    />
-
-                    {product?.document?.images?.length > 0
-                        ? product?.document?.images?.map((item, i) => (
-                            <img
-                                src={item?.image_url}
-                                className="border mb-3 w-100 mx-auto"
-                                // width="100%"
-                                // height={['.docx', '.doc', '.pdf'].includes(type) ? '800px' : '400px'}
-                                // style={{ objectFit: 'contain' }}
-                                key={i}
-                                detail={true}
-                                style={{ display: 'flex', width: '90%', maxWidth: '60%' }}
-                            />
-                        ))
-                        : ''}
-                </div>
-                <div className="views">
-                    {' '}
-                    <i className="fa-solid fa-eye"></i> <span>{views}</span>
-                </div>
-            </figure>
+        <div>
+            {/* Yandex reklama kodi */}
+            <div id="yandex_rtb_R-A-13331140-2"></div>
+            {/* Yandex scriptni yuklash */}
+            <Script
+                src="https://yandex.ru/ads/system/context.js"
+                strategy="lazyOnload"
+                onLoad={() => {
+                    if (window.yaContextCb) {
+                        window.yaContextCb.push(() => {
+                            Ya.Context.AdvManager.render({
+                                blockId: "R-A-13331140-2",
+                                renderTo: "yandex_rtb_R-A-13331140-2"
+                            });
+                        });
+                    }
+                }}
+            />
+            <div
+                className="ps-product__thumbnail full-detail-image mt-4"
+                data-vertical={vertical ? 'true' : 'false'}>
+                <figure style={{ flex: '1', minWidth: '100%' }}>
+                    <div className="ps-wrapper d-block p-4" ref={wrapperRef}>
+                        {product?.document?.images?.length > 0
+                            ? product?.document?.images?.map((item, i) => (
+                                <img
+                                    src={item?.image_url}
+                                    className="border mb-3 w-100 mx-auto"
+                                    // width="100%"
+                                    // height={['.docx', '.doc', '.pdf'].includes(type) ? '800px' : '400px'}
+                                    // style={{ objectFit: 'contain' }}
+                                    key={i}
+                                    detail={true}
+                                    style={{ display: 'flex', width: '90%', maxWidth: '60%' }}
+                                />
+                            ))
+                            : ''}
+                    </div>
+                    <div className="views">
+                        {' '}
+                        <i className="fa-solid fa-eye"></i> <span>{views}</span>
+                    </div>
+                </figure>
+            </div>
         </div>
     );
 };
