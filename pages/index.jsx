78316dea97e8c91bdb1aa2a0c7e9f4de79de5f90
AnvarMirsaatov <anvarmirsaatov6358@gmail.com>
2025-02-11 20:31:32 +0500

    HeroServise components is ready

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import HeroMain from '~/components/blocks/hero'
+import HeroService from '~/components/blocks/hero/HeroService'
 import HomeCategories from '~/components/blocks/home-categories'
 import SubProjects from '~/components/blocks/home-products/sub-projects'
 import HomeVideo from '~/components/blocks/home-video'
@@ -12,7 +13,8 @@ function NewHomePage({ tab, category }) {
     return (
         <PageLayout>
             <Meta title="Soff - barcha ma'lumotlar bazasi" image="/static/img/soff/soff_green_white.png" />
-            <HeroMain />
+            {/* <HeroMain /> */}
+            <HeroService/>
             {/* <CategoryTabs tab={tab} category={category} /> */}
             <HomeCategories/>
             <HomeVideo/>
