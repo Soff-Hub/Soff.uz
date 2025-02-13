ac98ff916f1b324ee3e729a7ba74b97ab6feeb4f
Ozodbek Abdisamatov <abdisamatovozodbek003@gmail.com>
2025-02-13 09:47:55 +0500

    template crate

--- pages/account/upload/3d-models-and-interior-designs/create.jsx
+++ pages/account/upload/3d-models-and-interior-designs/create.jsx
@@ -19,92 +19,7 @@ import { updateUploadingFile } from '~/rtk-store/upload/slice';
 import { useRouter } from 'next/router';
 import TemplateProductDetail from '~/components/elements/detail/TemplateProductDetail';
 
-export const formItemLayout = {
-    labelCol: {
-        xs: {
-            span: 24,
-        },
-        sm: {
-            span: 6,
-        },
-    },
-    wrapperCol: {
-        xs: {
-            span: 24,
-        },
-        sm: {
-            span: 14,
-        },
-    },
-};
 
-export const technologies = [
-    'Photoshop',
-    'Figma',
-    'Adobe Illustrator',
-    'Adobe XD',
-    'Canva',
-    'Sketch',
-    'CorelDRAW',
-    "Suniy Intellekt",
-    'HTML',
-    'CSS',
-    'JavaScript',
-    'TypeScript',
-    'React',
-    'Angular',
-    'Vue.js',
-    'Svelte',
-    'Node.js',
-    'Express.js',
-    'Next.js',
-    'Nuxt.js',
-    'Python',
-    'Django',
-    'Flask',
-    'Ruby on Rails',
-    'PHP',
-    'Laravel',
-    'MySQL',
-    'PostgreSQL',
-    'MongoDB',
-    'Firebase',
-    'AWS',
-    'Google Cloud',
-    'Azure',
-    'AI',
-    'Machine Learning',
-    'TensorFlow',
-    'Keras',
-    'PyTorch',
-    'Bootstrap',
-    'Tailwind CSS',
-    'Sass',
-    'LESS',
-    'Webpack',
-    'Babel',
-    'Git',
-    'GitHub',
-    'Docker',
-    'Kubernetes',
-    'Microsoft Word',
-    'Microsoft Excel',
-    'Microsoft PowerPoint',
-    'Microsoft Outlook',
-    'Microsoft Access',
-    'Google Docs',
-    'Google Sheets',
-    'Google Slides',
-    'LibreOffice Writer',
-    'LibreOffice Calc',
-    'LibreOffice Impress',
-    'Zoho Docs',
-    'Notion',
-    'Evernote',
-    'Trello',
-    'Slack',
-    'Asana'
-];
 
 export default function ThreeDmodelsAndInteriorDesignsCreate() {
     const [form] = Form.useForm();
@@ -156,7 +71,7 @@ export default function ThreeDmodelsAndInteriorDesignsCreate() {
                 ])
             }
         } else {
-            // await dispatch(updateUploadingFile(null));
+            await dispatch(updateUploadingFile(null));
             Modal.success({
                 title: "Muvaffaqiyatli",
                 content: "Sizning mahsulotingiz muvaffaqqiyatli yuborildi! 10 soat ichida adminlar tomonidan  mahsulotingiz 'Tasdiqlangan' dan so'ng  sotuvda ko'rishingiz mumkin yoki 'Bekor' qilishinishi ham mumkin"
@@ -254,6 +169,8 @@ export default function ThreeDmodelsAndInteriorDesignsCreate() {
         };
     }, []);
 
+
+
     return (
         <PageContainer>
             <div className="bg-white">
@@ -264,8 +181,8 @@ export default function ThreeDmodelsAndInteriorDesignsCreate() {
                             Sotuvdagi holatini ko'rish
                         </Button>
                     </div>
+
                     <Form
-                        {...formItemLayout}
                         form={form}
                         variant={'outlined'}
                         onFinish={handleSubmit}
@@ -273,162 +190,142 @@ export default function ThreeDmodelsAndInteriorDesignsCreate() {
                         initialValues={{
                             variant: 'filled',
                         }}
+                        layout='vertical'
                         autoComplete='off'
-                        className='row m-0'
+                        className='row'
                     >
-                        <div className="pt-5 col-12 col-md-6 col-lg-6 col-xl-6">
-                            <Form.Item
-                                label="Shablon nomi"
-                                name="title"
-                                rules={[
-                                    {
-                                        required: true,
-                                        message: 'Kiritish majburiy',
-                                    },
-                                ]}
-                                className='smmb-1'
-                            >
-                                <Input />
-                            </Form.Item>
+                        <Form.Item
+                            label="Shablon nomi"
+                            name="title"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Kiritish majburiy',
+                                },
+                            ]}
+                            className='smmb-1  col-md-6 col-12'
+                        >
+                            <Input />
+                        </Form.Item>
 
-                            <Form.Item
-                                label="Shablon narxi"
-                                name="price"
-                                rules={[
-                                    {
-                                        required: true,
-                                        message: "To'g'ri narx kiriting",
-                                    },
-                                ]}
-                                className='smmb-1'
-                            >
-                                <Input />
-                            </Form.Item>
+                        <Form.Item
+                            label="Shablon narxi"
+                            name="price"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: "To'g'ri narx kiriting",
+                                },
+                            ]}
+                            className='smmb-1  col-md-6 col-12'
+                        >
+                            <Input />
+                        </Form.Item>
 
-                            <Form.Item
-                                label="Kategoriya"
-                                name="category"
-                                rules={[
-                                    {
-                                        required: true,
-                                        message: 'Kiritish majburiy',
-                                    },
-                                ]}
-                                className='smmb-1'
-                            >
-                                <Select
-                                    onSearch={(e) => debounceFunction(e, (v) => fetchCategories(v))}
-                                    showSearch
-                                    optionFilterProp="label"
-                                    onFocus={() => fetchCategories()}
-                                    size='large'
-                                    options={myCategories?.map(el => ({ label: el?.label, value: el?.value }))}
-                                />
-                            </Form.Item>
-                        </div>
-                        <div className="pt-5 col-12 col-md-6 col-lg-6 col-xl-6" style={{ width: '100%' }}>
+                        <Form.Item
+                            label="Kategoriya"
+                            name="category"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Kiritish majburiy',
+                                },
+                            ]}
+                            className='smmb-1  col-md-6 col-12'
+                        >
+                            <Select
+                                onSearch={(e) => debounceFunction(e, (v) => fetchCategories(v))}
+                                showSearch
+                                optionFilterProp="label"
+                                onFocus={() => fetchCategories()}
+                                size='large'
+                                options={myCategories?.map(el => ({ label: el?.label, value: el?.value }))}
+                            />
+                        </Form.Item>
 
-                            <Form.Item
-                                label="Teglar"
-                                name="tags"
-                                className='smmb-1'
+                        <Form.Item
+                            label="Teglar"
+                            name="tags"
+                            className='smmb-1  col-md-6 col-12'
+                        >
+                            <Select
+                                onSearch={(e) => debounceFunction(e, (v) => fetchTags(v))}
+                                onFocus={() => fetchTags()}
+                                mode="tags"
                             >
-                                <Select
-                                    onSearch={(e) => debounceFunction(e, (v) => fetchTags(v))}
-                                    onFocus={() => fetchTags()}
-                                    mode="tags"
+                                {
+                                    tags?.map(el => (
+                                        <Select.Option key={el?.id} value={el.name} >{el.name}</Select.Option>
+                                    ))
+                                }
+                            </Select>
+                        </Form.Item>
+
+                        <Form.Item
+                            label="Asosiy fayl (.zip)"
+                            name="document"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Yuklash majburiy',
+                                },
+                            ]}
+                            className='smmb-1  col-md-6 col-12'
+
+                        >
+                            <Upload
+                                defaultFileList={uploading ? [uploading] : null}
+                                accept='.zip'
+                                maxCount={1} {...props}
+                                lassName='main-file-upload'
+                                style={{ width: '100%' }}
+
+                            >
+                                <Button size='large' className='w-100' icon={<i class="fa-solid fa-plus"></i>}>Yuklash uchun bosing</Button>
+                            </Upload>
+                        </Form.Item>
+
+                        <Form.Item
+                            label=""
+                            name={'images'}
+                            valuePropName="images"
+                            getValueFromEvent={normFile}
+                            required
+                            className='smmb-1  col-md-6 col-12'
+                            rules={[
+                                {
+                                    required: true,
+                                    message: "Kamida 1ta rasm yuklashingiz kerak"
+                                }
+                            ]}
+                        >
+                            <Upload
+                                listType='picture-card'
+                                className='upload-btn'
+                                accept='image/*'
+                                {...posterProps}
+                                previewFile={getPreviewFile}
+                            >
+                                <button
+                                    style={{
+                                        border: 0,
+                                        background: 'none',
+                                    }}
+                                    type="button"
+                                    className='upload-btn-btn'
                                 >
-                                    {
-                                        tags?.map(el => (
-                                            <Select.Option key={el?.id} value={el.name} >{el.name}</Select.Option>
-                                        ))
-                                    }
-                                </Select>
-                            </Form.Item>
-
-                            <Form.Item
-                                label="Asosiy fayl (.zip)"
-                                name="document"
-                                rules={[
-                                    {
-                                        required: true,
-                                        message: 'Yuklash majburiy',
-                                    },
-                                ]}
-                                className='smmb-1'
-                            >
-                                <Upload
-                                    defaultFileList={uploading ? [uploading] : null}
-                                    accept='.zip'
-                                    maxCount={1} {...props}
-                                    lassName='main-file-upload'
-                                >
-                                    <Button size='large' className='w-100' icon={<i class="fa-solid fa-plus"></i>}>Yuklash uchun bosing</Button>
-                                </Upload>
-                            </Form.Item>
-                            {/* 
-                            <Form.Item
-                                label="Qo'shimcha fayllar"
-                                name="addintional_files"
-                                className='smmb-1'
-                            >
-                                <Upload accept='.zip' {...props} className='main-file-upload'>
-                                    <Button size='large' className='w-100' icon={<i class="fa-solid fa-plus"></i>}>Yuklash uchun bosing</Button>
-                                </Upload>
-                            </Form.Item> */}
-                        </div>
-                        
-                        <div className="col-12 col-md-9 mb-3 mt-3">
-                            <div className="d-flex p-0">
-                                <p className='ant-form-item-required'>Qo'shimcha rasmlar: </p>{' '}
-                                <Tooltip title="Mijozlarga mahsulotingiz haqida ko'proq ma'lumot yetkazish uchun shablonning rasmlarini yuklang">
-                                    <i
-                                        style={{ cursor: 'pointer' }}
-                                        className="fa-regular fa-circle-question px-4 mt-2"></i>
-                                </Tooltip>
-                            </div>
-                            <Form.Item
-                                label=""
-                                name={'images'}
-                                valuePropName="images"
-                                getValueFromEvent={normFile}
-                                required
-                                className='smmb-1'
-                                rules={[
-                                    {
-                                        required: true,
-                                        message: "Kamida 1ta rasm yuklashingiz kerak"
-                                    }
-                                ]}
-                            >
-                                <Upload
-                                    listType='picture-card'
-                                    className='upload-btn'
-                                    accept='image/*'
-                                    {...posterProps}
-                                    previewFile={getPreviewFile}
-                                >
-                                    <button
+                                    <i class="fa-solid fa-plus"></i>
+                                    <div
                                         style={{
-                                            border: 0,
-                                            background: 'none',
+                                            marginTop: 8,
+                                            height: '200px !important'
                                         }}
-                                        type="button"
-                                        className='upload-btn-btn'
                                     >
-                                        <i class="fa-solid fa-plus"></i>
-                                        <div
-                                            style={{
-                                                marginTop: 8,
-                                                height: '200px !important'
-                                            }}
-                                        >
-                                            Yuklash
-                                        </div>
-                                    </button>
-                                </Upload>
-                            </Form.Item>
-                        </div>
+                                        Yuklash
+                                    </div>
+                                </button>
+                            </Upload>
+                        </Form.Item>
 
                         <div className='col-12 mx-auto'>
                             <div className="d-flex p-0">
@@ -451,16 +348,16 @@ export default function ThreeDmodelsAndInteriorDesignsCreate() {
                             </div>
                         </div>
 
-                        <div className='col-12'>
-                            <Form.Item
-                                className='d-flex justify-content-center'
-                            >
-                                <Button loading={isLoading} type="primary" htmlType="submit" style={{ flex: 1 }}>
-                                    Yuborish
-                                </Button>
-                            </Form.Item>
-                        </div>
+                        <Form.Item
+                            className='col-12 d-flex justify-content-end'
+                        >
+                            <Button loading={isLoading} type="primary" htmlType="submit" style={{ flex: 1 }}>
+                                Yuborish
+                            </Button>
+                        </Form.Item>
                     </Form>
+
+
                     <Modal
                         okText="Yuklashda davom etish"
                         cancelText="Yopish"
