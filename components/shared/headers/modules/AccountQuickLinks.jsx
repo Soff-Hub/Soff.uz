41b46519ee9dd91e997c86a7d2ce006266c39d27
Ozodbek Abdisamatov <abdisamatovozodbek003@gmail.com>
2025-02-17 18:06:51 +0500

    seller detail updates

--- components/shared/headers/modules/AccountQuickLinks.jsx
+++ components/shared/headers/modules/AccountQuickLinks.jsx
@@ -25,11 +25,6 @@ const AccountQuickLinks = (props) => {
         const res = logOutAuth(data);
 
         if (res) {
-            const modal = Modal.info({
-                centered: true,
-                title: 'Muvaffaqqiyatli!',
-                content: `Siz muvaffaqqiyatli chiqdingiz`,
-            });
             if (asPath == '/account/dashbord') {
                 Router.push('/auth/login')
             } else if ('/account/myproducts') {
