339ff65d5fe43abf19135802750d1368b1a266e1
AnvarMirsaatov <anvarmirsaatov6358@gmail.com>
2025-02-25 13:06:52 +0500

    aaa

--- components/partials/category/WebsitesProductsByCategory.jsx
+++ components/partials/category/WebsitesProductsByCategory.jsx
@@ -1,63 +1,74 @@
-import React from 'react'
+import React from 'react';
 import { Pagination } from 'antd';
-import { Skeleton } from 'antd'
+import { Skeleton } from 'antd';
 import WebsitesProduct from '~/components/elements/products/WebsitesProduct';
 
-export default function WebsitesProductsByCategory({ data = [], page, handlePagination, isLoading }) {
-
+export default function WebsitesProductsByCategory ({
+    data = [],
+    page,
+    handlePagination,
+    isLoading,
+}) {
     return (
         <div id='products' className='overflow-hidden container'>
-            <div className="row ">
-                {
-                    isLoading && (
-                        <div className={`product-list p-loading`}>
-                            {
-                                Array(15).fill(0).map((d, i) => <Skeleton.Image
+            <div className='row '>
+                {isLoading && (
+                    <div className={`product-list p-loading`}>
+                        {Array(15)
+                            .fill(0)
+                            .map((d, i) => (
+                                <Skeleton.Image
                                     key={i}
                                     active
                                     className={`skeletion-card file`}
-                                />)
-                            }
-                        </div>
-                    )
-
-                }
-                {
-                    (data?.results?.length > 0) && (data?.results?.map((item, index) => (
+                                />
+                            ))}
+                    </div>
+                )}
+                {data?.results?.length > 0 &&
+                    data?.results?.map((item, index) => (
                         <div
-                            className="home-card col-md-3 col-sm-4 col-xs-3 col-6"
+                            className='home-card col-md-3 col-sm-4 col-xs-3 col-6'
                             key={index}>
                             <WebsitesProduct product={item} />
                         </div>
-                    )))
-                }
+                    ))}
             </div>
             <div className='row justify-content-center'>
-                {
-                    (data?.results?.length == 0) && (
-                        <div
-                            className="text-center col-md-6 com-12" style={{ padding: '150px 0px'}}>
-                            <p className='fs-1'>😕 Bu yerda hozircha hech narsa yo‘q...</p>
-                            <p className='fs-4'>Birinchilardan bo‘lib ushbu kategoriyaga mahsulot joylashtirib boshlang va o'z auditoriyangizni yig'ib daromad qilishni boshlang! 👉 <a target='_blank' className='text-primary' href="https://seller.soff.uz">seller.soff.uz</a> </p>
-                        </div>
-                    )
-                }
+                {data?.results?.length == 0 && (
+                    <div
+                        className='text-center col-md-6 com-12'
+                        style={{ padding: '150px 0px' }}>
+                        <p className='fs-1'>
+                            😕 Bu yerda hozircha hech narsa yo‘q...
+                        </p>
+                        <p className='fs-4'>
+                            Birinchilardan bo‘lib ushbu kategoriyaga mahsulot
+                            joylashtirib boshlang va o'z auditoriyangizni yig'ib
+                            daromad qilishni boshlang! 👉{' '}
+                            <a
+                                target='_blank'
+                                className='text-primary'
+                                href='https://seller.soff.uz'>
+                                seller.soff.uz
+                            </a>{' '}
+                        </p>
+                    </div>
+                )}
             </div>
             {data?.count >= 48 && (
-                <div className="text-center my-4">
+                <div className='d-flex justify-content-center mt-5'>
                     <Pagination
+                        className='text-success'
                         total={data?.count}
                         pageSize={48}
                         responsive={true}
                         showSizeChanger={false}
                         current={page}
-                        showTotal={(total, range) =>
-                            `${total} ta dan ${range[0]}-${range[1]} oralig'i `
-                        }
-                        onChange={(e) => handlePagination(e)}
+                        onChange={e => handlePagination(e)}
                     />
                 </div>
             )}
         </div>
-    )
+    );
 }
