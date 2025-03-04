8fe726adb67fa57d2be8aab5d57ffab5080d6998
AnvarMirsaatov <anvarmirsaatov6358@gmail.com>
2025-03-04 18:32:12 +0500

    asdf

--- components/partials/account/Checkout.jsx
+++ components/partials/account/Checkout.jsx
@@ -21,7 +21,7 @@ const Checkout = () => {
     return (
         <div className='container p-xl-0 mt-5'>
             <div className='row'>
-                <div className='container col-xl-7 col-lg-12 col-md-12 col-12'>
+                <div className='container col-xl-7 col-lg-12 col-md-10 col-sm-11 col-12'>
                     <RedesignModulePaymentOrderSummary ecomerce={ecomerce} />
                 </div>
                 <div className=' container col-xl-5 col-lg-10 col-md-10 col-sm-10 col-12'>
