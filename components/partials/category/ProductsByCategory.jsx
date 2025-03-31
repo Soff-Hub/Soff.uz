cc26cdb43f278630a09bc2d2be804994c812a1cb
Zufarbek <zufarbek.abdurakhmonov@gmail.com>
2025-03-31 16:48:06 +0500

    fixed non products view in category page

--- components/partials/category/ProductsByCategory.jsx
+++ components/partials/category/ProductsByCategory.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Pagination } from 'antd';
 import { Skeleton } from 'antd';
 import RedesignProduct from '~/components/elements/products/Redesign/Redesign-Product';
+import Link from 'next/link';
 
 export default function ProductsByCategory ({
     data = [],
@@ -32,30 +33,27 @@ export default function ProductsByCategory ({
                         <RedesignProduct product={item} />
                     </div>
                 ))}
-                <div className='row justify-content-center'>
-                    {data?.results?.length == 0 && (
-                        <div
-                            className='text-center col-md-6 com-12'
-                            style={{ padding: '150px 0px' }}>
-                            <p className='fs-1'>
-                                😕 Bu yerda hozircha hech narsa yo‘q...
+            </div>
+
+            {data?.results?.length == 0 && (
+                <div className="ps-page-status">
+                    <div className="container">
+                        <div className="ps-section__content">
+                        <img src="/static/img/noinfo.svg" alt="Ma'lumot topilmadi" />
+                            <h3>😕 Bu yerda hozircha hech narsa yo‘q...</h3>
+                            <p>
+                                Ammo bu siz uchun ajoyib imkoniyat! Birinchilardan bo‘lib ushbu kategoriyaga mahsulot joylashtiring, o‘z auditoriyangizni yarating va daromad olishni boshlang. Imkoniyatni qo‘ldan boy bermang!
                             </p>
-                            <p className='fs-4'>
-                                Birinchilardan bo‘lib ushbu kategoriyaga
-                                mahsulot joylashtirib boshlang va o'z
-                                auditoriyangizni yig'ib daromad qilishni
-                                boshlang! 👉{' '}
-                                <a
-                                    target='_blank'
-                                    className='text-primary'
-                                    href='https://seller.soff.uz'>
-                                    seller.soff.uz
-                                </a>{' '}
+                            <p>
+                                <Link href="https://seller.soff.uz">
+                                    <a target='_blank'>Sotuvchi bo'lish</a>
+                                </Link>
                             </p>
                         </div>
-                    )}
+                    </div>
                 </div>
-            </div>
+            )}
+
             {data?.count >= 48 && (
                 <div className='d-flex justify-content-center mt-5'>
                     <Pagination
