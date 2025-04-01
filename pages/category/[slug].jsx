b61d52fb671eb7ee870db55da7cd2c0cc929671e
Zufarbek <zufarbek.abdurakhmonov@gmail.com>
2025-04-02 02:57:25 +0500

    fixed category page SEO

--- pages/category/[slug].jsx
+++ pages/category/[slug].jsx
@@ -1,67 +1,23 @@
-import React, { useEffect, useState } from 'react';
+// pages/category/[slug].js
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
-import PageContainer from '~/components/layouts/PageContainer';
-import FooterDefault from '~/components/shared/footers/FooterDefault';
+const CategoryPage = ({ slug }) => {
+  const router = useRouter();
 
-// import ShopItems from '~/components/partials/shop/ShopItems';
-import { baseUrl } from '~/repositories/Repository';
-import Meta from '~/components/shared/headers/Meta';
+  useEffect(() => {
+    // Redirect to the new URL
+    router.push(`/scientific-resources/${slug}?parentCategory=${slug}`);
+  }, [slug, router]);
 
-export default function ProductCategoryScreen() {
-
-    return (
-        <PageContainer
-            footer={<FooterDefault />}
-            title={"category2" ? "nom" : 'Kategoriya'}
-            boxed={true}>
-            <Meta
-                title={`nom`}
-                description={`Biz siz qidirayotgan mahsulotlarni Soff.uz saytimizning  kategoriyasida topdik`}
-            />
-            <div className="ps-page--shop">
-                {/* <BreadCrumb breacrumb={breadCrumb} /> */}
-                <div className="container">
-                    <div className="ps-layout--shop ps-shop--category">
-                        <div className="ps-layout__left">
-                            <WidgetShopCategories data={category2} />
-                            <WidgetShopFilterByPriceRange
-                                setFilteredData={setFilteredData}
-                                chaildId={chaildId}
-                                parentId={parentId}
-                                categoryData={category2 || []}
-                                setCount={setCount}
-                                defVal={defVal}
-                                min={min}
-                                max={max}
-                                setDefVal={setDefVal}
-                                setMax={setMax}
-                                setMin={setMin}
-                            />
-                        </div>
-                        <div className="ps-layout__right">
-                            {/* <ShopItems
-                                data={filteredData}
-                                columns={4}
-                                pageSize={36}
-                                dataCount={count}
-                                setDataCount={setCount}
-                                categoryData={category2 || []}
-                            /> */}
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </PageContainer>
-    );
+  return null; // This page will redirect immediately
 };
-export async function getServerSideProps(context) {
-    const { slug } = context.params
-    const res = await fetch(`${baseUrl}customer/parent-category-list/?category=${slug}`);
-    const responseData = await res.json();
 
-    return {
-        props: {
-            category2: responseData,
-        },
-    };
+export async function getServerSideProps(context) {
+  const { slug } = context.params;
+
+  // Pass the slug to the component as a prop
+  return { props: { slug } };
 }
+
+export default CategoryPage;
