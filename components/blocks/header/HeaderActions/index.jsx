80b5721ed7004d88fde451390fe3d16026a540e2
Zufarbek <zufarbek.abdurakhmonov@gmail.com>
2025-04-02 04:11:48 +0500

    visable phone and added search icon

--- components/blocks/header/HeaderActions/index.jsx
+++ components/blocks/header/HeaderActions/index.jsx
@@ -6,6 +6,7 @@ import useWishlist from '~/hooks/useWishlist';
 import MiniCart from '~/components/shared/headers/modules/MiniCart';
 import HeaderNotifications from './HeaderNotifications';
 import HeaderUserDropdown from './HeaderUserDropdown';
+import HeaderSearchIcon from './HeaderSearchIcon';
 
 const HeaderActions = ({ auth, isDark }) => {
     const { wishlist } = useWishlist();
@@ -18,7 +19,8 @@ const HeaderActions = ({ auth, isDark }) => {
                     <i className="fa-regular fa-circle-question"></i>
                 </a>
             </Link> */}
-
+            
+            <HeaderSearchIcon />
             <HeaderNotifications color={isDark ? 'text-black' : 'text-white'} />
 
             {wishlist?.length > 0 ? (
