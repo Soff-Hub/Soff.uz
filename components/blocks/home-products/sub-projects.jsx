41aa16607bfb65ec520815219148452f4d192835
Zufarbek <zufarbek.abdurakhmonov@gmail.com>
2025-04-19 02:04:11 +0500

    fixed alt values

--- components/blocks/home-products/sub-projects.jsx
+++ components/blocks/home-products/sub-projects.jsx
@@ -4,11 +4,13 @@ export default function SubProjects() {
     const projecs = [
         {
             logoImg: '/static/img/soff.svg',
+            imgAlt: "Soff uz logo",
             link: 'https://soff.uz',
             linkMask: 'SOFF.UZ - raqamli xizmatlar bozori!',
         },
         {
             logoImg: '',
+            imgAlt: "Soff Jamoasi",
             link: '#',
             title: 'Soff Jamoasi',
             linkMask: 'Biz rivojalishda davom etamiz!',
@@ -18,21 +20,25 @@ export default function SubProjects() {
         },
         {
             logoImg: '/static/img/soffcrm.svg',
+            imgAlt: "Soff CRM logo",
             link: 'https://soffcrm.uz',
             linkMask: "SOFFCRM - O'quv markazlar uchun crm tizim",
         },
         {
             logoImg: '/static/img/_Soff_.svg',
+            imgAlt: "Soff Hub logo",
             link: 'https://soffhub.uz',
             linkMask: 'SOFFHUB - Biznes uchun raqamli yechimlar',
         },
         {
             logoImg: '/static/img/soffBIrja.png',
+            imgAlt: "Soff Birja logo",
             link: 'https://birja.soff.uz',
             linkMask: 'SOFF BIRJA - Intellektual mulk birjasi',
         },
         {
             logoImg: '/static/img/soff-study.png',
+            imgAlt: "Soff Study logo",
             link: 'https://soffstudy.uz',
             linkMask: "SOFF STUDY - Zamonaviy kasblarni o'qitish markazi",
         },
@@ -55,7 +61,7 @@ export default function SubProjects() {
                                 {el?.last ? (
                                     <div className='d-flex gap-3'>
                                         {el?.logoImg && (
-                                            <img src={el.logoImg} alt='' />
+                                            <img src={el.logoImg} alt={el.imgAlt}/>
                                         )}
                                         <h2
                                             className='sdsdsd'
@@ -72,7 +78,7 @@ export default function SubProjects() {
                                         {el?.logoImg && (
                                             <img
                                                 src={el.logoImg}
-                                                alt={el.title}
+                                                alt={el.imgAlt}
                                             />
                                         )}
                                         <h2>{el?.title}</h2>
