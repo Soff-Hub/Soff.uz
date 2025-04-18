691b508e6c811df5a741c031963097a6f2792aef
Zufarbek <zufarbek.abdurakhmonov@gmail.com>
2025-04-19 04:02:09 +0500

    removed metrica script teg for yandex

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -87,7 +87,7 @@ export default function Document() {
                         }}></script>
                 )}
 
-            <noscript><div><img src="https://mc.yandex.ru/watch/96098801" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+            {/* <noscript><div><img src="https://mc.yandex.ru/watch/96098801" style="position:absolute; left:-9999px;" alt="" /></div></noscript> */}
 
             <script>window.yaContextCb=window.yaContextCb||[]</script>
             </Head>
