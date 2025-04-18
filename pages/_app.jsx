e81b59de21bb322386d598c0ffe665ccc83b16f4
Zufarbek <zufarbek.abdurakhmonov@gmail.com>
2025-04-19 00:42:29 +0500

    title and description added in home page and added script to improve seo for search-page

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -68,6 +68,28 @@ function App({ Component, pageProps }) {
                 {/* <meta name="apple-mobile-web-app-capable" content="yes" /> */}
                 <meta name="mobile-web-app-capable" content="yes"></meta>
                 <meta name="google-adsense-account" content="ca-pub-2651864926558603" />
+                
+                <link rel="alternate" href="https://soff.uz/" hrefLang="uz" />
+                <script
+                    defer
+                    type="application/ld+json"
+                    dangerouslySetInnerHTML={{
+                        __html: JSON.stringify({
+                        "@context": "https://schema.org",
+                        "@type": "WebSite",
+                        "name": "Soff.uz",
+                        "url": "https://soff.uz",
+                        "potentialAction": {
+                            "@type": "SearchAction",
+                            "target": "https://soff.uz/search-page?keyword={search_term_string}",
+                            "query-input": "required name=search_term_string"
+                        }
+                        })
+                    }}
+                    ></script>
+
+
+
             </Head>
 
             <NextProgress
