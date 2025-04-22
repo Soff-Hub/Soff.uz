7b3efc3ab5acff598fc32d8e157a87eb90943332
Zufarbek <zufarbek.abdurakhmonov@gmail.com>
2025-04-22 12:42:21 +0500

    changed

--- components/details-components/file-products-detail/details-page.jsx
+++ components/details-components/file-products-detail/details-page.jsx
@@ -16,15 +16,15 @@ function FileProductsDetails({ product }) {
         <div className="ps-product--detail_seller my-5">
           <div>
             <h1 className='titleh3' style={{ fontWeight: 600, fontSize: "25px", lineHeight: "37.5px", color: "#312F30", margin: 0 }}>
-              {product?.title}
+              {product?.title || <span className="invisible-text">Mahsulot nomi</span>}
             </h1>
-            {/* <Link href={`/scientific-resources/${product?.category?.slug}?childCategory=${product?.category?.slug}`}>
+            <Link href={`/scientific-resources/${product?.category?.slug}?childCategory=${product?.category?.slug}`}>
               <a>
                 <p className='m-0 fs-3'>
                   {product?.category?.name || <span className="invisible-text">Kategoriya nomi</span>}
                 </p>
               </a>
-            </Link> */}
+            </Link>
           </div>
           <div className="ps-product__header_seller ">
             <FileImagesScroll
