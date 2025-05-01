058fcfb03554acb3096627d1dedd6c72dfff148c
AnvarMirsaatov <anvarmirsaatov6358@gmail.com>
2025-05-01 10:37:33 +0500

    asdf

--- pages/websites/[slug].jsx
+++ pages/websites/[slug].jsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import PageContainer from '~/components/layouts/PageContainer';
 import FooterDefault from '~/components/shared/footers/FooterDefault';
@@ -16,7 +15,7 @@ export default function Websites () {
     // products API uchun so'rov
     const { data, error, isLoading } = useApi(
         ['products', page, parentCategory, childCategory], // queryKey dinamik
-        `${baseUrlUseApi}customer/products/?direction=scientific_work&category=${
+        `${baseUrlUseApi}customer/products/?direction=website&category=${
             childCategory ? childCategory : parentCategory
         }&page=${page || 1}&page_size=48`,
         'GET'
@@ -29,7 +28,7 @@ export default function Websites () {
         isLoading: isFourChildLoading,
     } = useApi(
         ['fourChild'], // Query key
-        `${baseUrlUseApi}customer/four-child?direction=scientific_work`,
+        `${baseUrlUseApi}customer/four-child?direction=website`,
         'GET'
     );
 
@@ -40,7 +39,7 @@ export default function Websites () {
         isLoading: isChildCategory,
     } = useApi(
         ['fourChild', parentCategory], // Query key
-        `${baseUrlUseApi}customer/four-child?direction=scientific_work&parent__slug=${parentCategory}`,
+        `${baseUrlUseApi}customer/four-child?direction=website&parent__slug=${parentCategory}`,
         'GET'
     );
 
