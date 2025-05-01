2fe0c212f405cdab3394b89374e9b86e93fa7b49
AnvarMirsaatov <anvarmirsaatov6358@gmail.com>
2025-05-01 15:44:43 +0500

    ADF

---
+++ pages/3d-models-and-interior-designs/[slug].jsx
@@ -0,0 +1,83 @@
+import { useRouter } from 'next/router';
+import React from 'react';
+import FooterComponents from '~/components/blocks/footer/FooterComponents';
+import ThreeDCategoriesFilterSecion from '~/components/elements/ThreeDCategoriesFilterSecion';
+import PageContainer from '~/components/layouts/PageContainer';
+import ProductsByModelsAndDesignCategory from '~/components/partials/category/ProductsByModelsAndDesignCategory';
+import FooterDefault from '~/components/shared/footers/FooterDefault';
+import Meta from '~/components/shared/headers/Meta';
+import useApi, { baseUrlUseApi } from '~/repositories/useApi';
+
+export default function ModelsAndInteriorDesign () {
+    const router = useRouter();
+    const { slug, page, parentCategory, childCategory } = router.query;
+
+    // products API uchun so'rov
+    const { data, error, isLoading } = useApi(
+        ['products', page, parentCategory, childCategory], // queryKey dinamik
+        `${baseUrlUseApi}customer/products/?direction=scientific_work&category=${
+            childCategory ? childCategory : parentCategory
+        }&page=${page || 1}&page_size=48`,
+        'GET'
+    );
+
+    // Ota kategoriya API uchun so'rov
+    const {
+        data: fourChildData,
+        error: fourChildError,
+        isLoading: isFourChildLoading,
+    } = useApi(
+        ['fourChild'], // Query key
+        `${baseUrlUseApi}customer/four-child?direction=scientific_work`,
+        'GET'
+    );
+    console.log('fourChildData =>', fourChildData);
+
+    // Farzand kategoriya API uchun so'rov
+    const {
+        data: childCategoryData,
+        error: childCategoryEror,
+        isLoading: isChildCategory,
+    } = useApi(
+        ['fourChild', parentCategory], // Query key
+        `${baseUrlUseApi}customer/four-child?direction=scientific_work&parent__slug=${parentCategory}`,
+        'GET'
+    );
+
+    // Pagination tugmalari uchun funksiya
+    const handlePageChange = newPage => {
+        router.push({
+            pathname: router.pathname,
+            query: { ...router.query, page: newPage }, // URL'ga yangi page qo'shish
+        });
+    };
+
+    return (
+        <PageContainer
+            footer={<FooterDefault />}
+            title={'Kategoriya'}
+            boxed={true}>
+            <Meta
+                title={`${'3D moddellar va Interier dizaynlar'}`}
+                description={`3D moddellar va Interier dizaynlar kategoriyasi: Taqdimotlar Tayyor shablonlar Kurs ishlari Diplom ishlari Referatlar Mustaqil ishlar Labaratoriya Ishlari Dissertatsiya ishlari Testlar O'quv qo'llanmalar Dars ishlanmalar Tarqatma materiallar Amaliy ishlar Blankalar Ijodiy Ishlar Loyihalar Plakatlar Maqola Ixtiro patenti Namunaviy hujjatlar Statistika Elektron kitoblar Dasturlash tillari `}
+            />
+            <div className='ps-page--shop container p-xl-0 p-l-0'>
+                <ThreeDCategoriesFilterSecion
+                    breacrumb={fourChildData}
+                    count={data?.count}
+                    isLoading={isFourChildLoading}
+                    childCategoryData={childCategoryData}
+                />
+                <ProductsByModelsAndDesignCategory
+                    data={data}
+                    page={page}
+                    handlePagination={number => {
+                        handlePageChange(number);
+                    }}
+                    isLoading={isLoading}
+                />
+            </div>
+            <FooterComponents />
+        </PageContainer>
+    );
+}
