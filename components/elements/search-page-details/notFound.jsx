70208d9532882037cf57ff6e2d47ab91eea2be3a
Abdumomin <abduganiabdurasulov5150@gmail.com>
2025-07-02 19:06:09 +0500

    searchpage return home btn fixed

--- components/elements/search-page-details/notFound.jsx
+++ components/elements/search-page-details/notFound.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 
 export default function Search_Results_NotFound () {
+    const router = useRouter();
+
+    const handleRedirect = () => {
+        router.push('/'); // bu yerda '/' asosiy sahifani bildiradi
+    };
+
     return (
         <div>
             <div className='Search_Results_not_found'>
@@ -12,7 +19,13 @@ export default function Search_Results_NotFound () {
                 <p className='Search_Results_not_found_title'>
                     Kechirasiz, natijalar topilmadi
                 </p>
-                <p className='Search_Results_not_found_btn'>Asosiy sahifaga</p>
+                <p
+                    className='Search_Results_not_found_btn'
+                    onClick={handleRedirect}
+                    style={{ cursor: 'pointer' }}
+                >
+                    Asosiy sahifaga
+                </p>
             </div>
         </div>
     );
