4b458364e28ce6517e898745a40d3075f0d2d22c
AnvarMirsaatov <anvarmirsaatov6358@gmail.com>
2025-07-05 16:13:42 +0500

    fix/homePage=> carousel

---
+++ components/blocks/sellerStatistics/BestSellerStatics.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function BestSellerStatics () {
+    return <div></div>;
+}
---
+++ components/blocks/sellerStatistics/index.jsx
@@ -0,0 +1,13 @@
+import CategoryHighlights from './categoryHighlights';
+
+export default function BestSellerStatics () {
+    return (
+        <div className='products mt-1'>
+            <div className='container  p-0'>
+                <div className={`product-list`}>
+                    <BestSellerStatics />
+                </div>
+            </div>
+        </div>
+    );
+}
