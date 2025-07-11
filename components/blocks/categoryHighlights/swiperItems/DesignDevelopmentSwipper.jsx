00f3e7a95298410d16c921d73de67662b54adfe0
AnvarMirsaatov <anvarmirsaatov6358@gmail.com>
2025-07-11 15:07:45 +0500

    test 2

--- components/blocks/categoryHighlights/swiperItems/DesignDevelopmentSwipper.jsx
+++ components/blocks/categoryHighlights/swiperItems/DesignDevelopmentSwipper.jsx
@@ -1,8 +1,4 @@
-import { Spin } from 'antd';
 import React, { useEffect, useState } from 'react';
-import DesignDevelopmentProducts from '~/components/elements/products/DesignDevelopmentProducts';
-import { baseURL } from '~/repositories/api';
-import SwiperPages from '~/components/details-components/swiper/swiper-page';
 import Swiper_Pages from '../swipper/swiper';
 import RedesignProduct from '~/components/elements/products/Redesign/Redesign-Product';
 
@@ -39,59 +35,6 @@ export default function DesignDevelopmentSwipper () {
                                     <RedesignProduct
                                         product={item}
                                         key={index}
-                                    />
-                                </div>
-                            ))}
-                        </Swiper_Pages>
-                    ) : null}
-                </div>
-            )}
-        </div>
-    );
-}
-import React, { useEffect, useState } from 'react';
-import Swiper_Pages from '../swipper/swiper';
-import RedesignProduct from '~/components/elements/products/Redesign/Redesign-Product';
-import DesignDevelopmentProducts from '~/components/elements/products/DesignDevelopmentProducts';
-
-export default function DesignDevelopmentSwipper () {
-    const [product, setProduct] = useState(null);
-    const [isLoading, setIsLoading] = useState(false);
-
-    useEffect(() => {
-        setIsLoading(true);
-
-        fetch(`https://api.soff.uz/api/v1/customer/products?direction=design`)
-            .then(res => res.json())
-            .then(data => {
-                setProduct(data.results);
-            })
-            .catch(error => {
-                console.error('Error fetching products:', error);
-            })
-            .finally(() => {
-                setIsLoading(false);
-            });
-    }, []);
-
-    return (
-        <div className=' categoryHighlightsSwippercontainer'>
-            {Array.isArray(product) && product.length > 0 && (
-                <div className='categoryHighlightsSwipper'>
-                    <div className='SwipperTitlewrap'>
-                        <a
-                            className='SwipperTitle'
-                            href='/design-developments/all'>
-                            Dizayn shablonlar
-                        </a>
-                    </div>
-                    {product ? (
-                        <Swiper_Pages categoryName type='design'>
-                            {product?.map((item, index) => (
-                                <div>
-                                    <DesignDevelopmentProducts
-                                        key={index}
-                                        product={item}
                                     />
                                 </div>
                             ))}
--- components/blocks/categoryHighlights/swiperItems/scientificResourcesSwipper.jsx
+++ components/blocks/categoryHighlights/swiperItems/scientificResourcesSwipper.jsx
@@ -1,6 +1,4 @@
-import { Spin } from 'antd';
 import React, { useEffect, useState } from 'react';
-import { baseURL } from '~/repositories/api';
 import Swiper_Pages from '../swipper/swiper';
 import RedesignProduct from '~/components/elements/products/Redesign/Redesign-Product';
 
@@ -34,61 +32,6 @@ export default function ScientificResourcesSwipper () {
                     {products ? (
                         <Swiper_Pages categoryName type='template'>
                             {products?.map((item, index) => (
-                                <div>
-                                    <RedesignProduct
-                                        product={item}
-                                        key={index}
-                                    />
-                                </div>
-                            ))}
-                        </Swiper_Pages>
-                    ) : null}
-                </div>
-            )}
-        </div>
-    );
-}
-import { Spin } from 'antd';
-import React, { useEffect, useState } from 'react';
-import { baseURL } from '~/repositories/api';
-import Swiper_Pages from '../swipper/swiper';
-import RedesignProduct from '~/components/elements/products/Redesign/Redesign-Product';
-import DesignDevelopmentProducts from '~/components/elements/products/DesignDevelopmentProducts';
-
-export default function ScientificResourcesSwipper () {
-    const [product, setProduct] = useState();
-    const [isLoading, setIsLoading] = useState(false);
-
-    useEffect(() => {
-        setIsLoading(true);
-
-        fetch(`${baseURL}customer/products?direction=file`)
-            .then(res => res.json())
-            .then(data => {
-                setProduct(data?.results);
-            })
-            .catch(error => {
-                console.error('Error fetching products:', error);
-            })
-            .finally(() => {
-                setIsLoading(false);
-            });
-    }, []);
-
-    return (
-        <div className=' categoryHighlightsSwippercontainer'>
-            {Array.isArray(product) && product.length > 0 && (
-                <div className='categoryHighlightsSwipper'>
-                    <div className='SwipperTitlewrap'>
-                        <a
-                            className='SwipperTitle'
-                            href='/scientific-resources/all'>
-                            Ilmiy ishlar{' '}
-                        </a>
-                    </div>
-                    {product ? (
-                        <Swiper_Pages categoryName type='file'>
-                            {product?.map((item, index) => (
                                 <div>
                                     <RedesignProduct
                                         product={item}
