13ea00715bd89802c9bc0d98aaf856b9129cad87
AnvarMirsaatov <anvarmirsaatov6358@gmail.com>
2025-07-14 19:07:52 +0500

    saf

--- components/shared/seller-profile/sellerPortfolio.jsx
+++ components/shared/seller-profile/sellerPortfolio.jsx
@@ -85,15 +85,16 @@ export default function SellerPortfolio ({ pid }) {
 
     return (
         <div>
-            {true ? (
-                <div className='SellerPortfolioWrap'>
+            {isLoading ? (
+                <div className='sellerProductSkeletonWrap'>
                     {Array(16)
                         .fill(0)
                         .map((_, i) => (
                             <Skeleton.Image
                                 key={i}
                                 active
-                                className='SellerPortfolioSkeleton shadow'
+                                className='sellerProductSkeleton shadow'
+                                style={{ width: '100%' }}
                             />
                         ))}
                 </div>
@@ -153,10 +154,7 @@ export default function SellerPortfolio ({ pid }) {
                                                 height='10'
                                                 viewBox='0 0 18 10'
                                                 fill='none'>
-                                                <path
-                                                    d='M0.798557 4.09724C0.599203 4.35429 0.5 4.67471 0.5 5C0.5 5.32529 0.599204 5.64571 0.798558 5.90276C5.03539 11.3657 12.9646 11.3657 17.2014 5.90276C17.4008 5.64571 17.5 5.32529 17.5 5C17.5 4.67471 17.4008 4.35429 17.2014 4.09724C12.9646 -1.36575 5.03539 -1.36575 0.798557 4.09724ZM9 9C6.65254 9 4.75 7.20937 4.75 5C4.75 2.79063 6.65254 1 9 1C11.3475 1 13.25 2.79063 13.25 5C13.25 7.20937 11.3475 9 9 9ZM9 2.75C9 2.54031 9.03689 2.34082 9.09664 2.14825C9.11783 2.07996 9.0715 2 9 2C7.24023 2 5.8125 3.34375 5.8125 5C5.8125 6.65625 7.24023 8 9 8C10.7598 8 12.1875 6.65625 12.1875 5C12.1875 4.93343 12.1091 4.88717 12.045 4.9052C11.8372 4.96369 11.6184 5 11.3906 5C10.0691 5 9 3.99375 9 2.75Z'
-                                                    fill='white'
-                                                />
+                                                <path d='...' fill='white' />
                                             </svg>
                                             <p className='text-white p-0 m-0'>
                                                 {item?.view_count}
