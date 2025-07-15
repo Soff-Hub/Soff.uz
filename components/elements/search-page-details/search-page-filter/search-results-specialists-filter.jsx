840ee0c6a3f07a67bcde8e1851db769816cb0ed8
Abdumomin <abduganiabdurasulov5150@gmail.com>
2025-07-15 16:09:32 +0500

    searchpage mutahasislar added

--- components/elements/search-page-details/search-page-filter/search-results-specialists-filter.jsx
+++ components/elements/search-page-details/search-page-filter/search-results-specialists-filter.jsx
@@ -1,32 +1,85 @@
-import React from 'react';
+import { Select } from 'antd';
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
+import Axios from 'axios';
 
-export default function SearchResultsSpecialists_Filter (data) {
-    
+export default function SearchResultsSpecialists_Filter(data) {
+    const [positions, setPositions] = useState([])
+    const router = useRouter();
+
+    useEffect(()=>{
+        const fetchpositions = async () => {
+            try {
+                const res = await Axios.get('http://176.96.241.219:8005/api/v1/categories/positions')
+                setPositions(res.data)
+            } catch (error) {
+                console.error(error)
+            }
+        }
+        fetchpositions()
+    }, [])
+
+    const handleChange = (key, value) => {
+        router.push({
+            pathname: router.pathname,
+            query: {...router.query, [key]: value  },
+        });
+    };
+
+   
+
     return (
         <div className='Search_Results_Specialists_form_box'>
-            <p className='countSpecialist'>{data?.count?.length} ta mutaxassis</p>
+            <p className='countSpecialist'>{data?.count?.count > 0 ? `${data?.count?.count} ta mutaxassis` : ''} </p>
             <form action='' className='Search_Results_Specialists_form'>
-                <div className='Search_Results_Specialists_form_inputBox'>
-                    <input type='text' placeholder='Mutaxassisni izlash' />
-                    <svg
-                        xmlns='http://www.w3.org/2000/svg'
-                        width='12'
-                        height='13'
-                        viewBox='0 0 12 13'
-                        fill='none'>
-                        <path
-                            d='M9.47006 9.13465L12 11.6646L11.1646 12.5L8.63465 9.97006C7.72497 10.6978 6.57133 11.1332 5.31661 11.1332C2.38184 11.1332 0 8.75138 0 5.81661C0 2.88184 2.38184 0.5 5.31661 0.5C8.25138 0.5 10.6332 2.88184 10.6332 5.81661C10.6332 7.07133 10.1978 8.22497 9.47006 9.13465ZM8.28487 8.69632C9.00722 7.95188 9.45175 6.93641 9.45175 5.81661C9.45175 3.53194 7.60127 1.68147 5.31661 1.68147C3.03194 1.68147 1.18147 3.53194 1.18147 5.81661C1.18147 8.10127 3.03194 9.95175 5.31661 9.95175C6.43641 9.95175 7.45188 9.50722 8.19632 8.78487L8.28487 8.69632Z'
-                            fill='#7B7B7B'
-                        />
-                    </svg>{' '}
-                </div>
-                <select name='' id=''>
-                    <option value=''>Kasb</option>
-                </select>
-                <select name='' id=''>
-                    <option value=''>Reytingi yuqori</option>
-                </select>
+
+                <Select
+                    className="select_specalist"
+                    value={router.position}
+                    placeholder="Kasbni tanlang"
+                    onChange={(value) => handleChange('position', value)}
+                    allowClear
+                >
+                    <Select.Option value="">Kasb</Select.Option>
+                    {positions?.map((item) => (
+                        <Select.Option key={item} value={item}>
+                            {item}
+                        </Select.Option>
+                    ))}
+                </Select>
+
+                <Select
+                    className="select_specalist"
+                    value={router.position}
+                    placeholder="Saralash"
+                    onChange={(value) => handleChange('order', value)}
+                >
+                    <Select.Option value="">Reyting</Select.Option>
+                    {positions.map((item) => (
+                        <Select.Option key={item.value} value={item.value}>
+                            {item}
+                        </Select.Option>
+                    ))}
+                </Select>
             </form>
         </div>
     );
 }
+
+
+
+{/* <div className='Search_Results_Specialists_form_inputBox'>
+    <input type='text' placeholder='Mutaxassisni izlash' />
+    <svg
+        xmlns='http://www.w3.org/2000/svg'/
+        width='12'
+        height='13'
+        viewBox='0 0 12 13'
+        fill='none'>
+        <path
+            d='M9.47006 9.13465L12 11.6646L11.1646 12.5L8.63465 9.97006C7.72497 10.6978 6.57133 11.1332 5.31661 11.1332C2.38184 11.1332 0 8.75138 0 5.81661C0 2.88184 2.38184 0.5 5.31661 0.5C8.25138 0.5 10.6332 2.88184 10.6332 5.81661C10.6332 7.07133 10.1978 8.22497 9.47006 9.13465ZM8.28487 8.69632C9.00722 7.95188 9.45175 6.93641 9.45175 5.81661C9.45175 3.53194 7.60127 1.68147 5.31661 1.68147C3.03194 1.68147 1.18147 3.53194 1.18147 5.81661C1.18147 8.10127 3.03194 9.95175 5.31661 9.95175C6.43641 9.95175 7.45188 9.50722 8.19632 8.78487L8.28487 8.69632Z'
+            fill='#7B7B7B'
+        />
+    </svg>{' '}
+</div> */}
--- components/elements/search-page-details/services.jsx
+++ components/elements/search-page-details/services.jsx
@@ -3,7 +3,7 @@ import Search_Results_Services_filter from './search-page-filter/search-results-
 import SearchResultsServices_Card from './search-page-card/searchResultsServices_Card';
 import { Skeleton } from 'antd';
 
-export default function Search_Results_Services ({ data }) {
+export default function Search_Results_Services({ data }) {
     return (
         <div className='Search_Results_Services'>
             <Search_Results_Services_filter count={data} />
@@ -22,7 +22,7 @@ export default function Search_Results_Services ({ data }) {
                                             className='Search_Results_Wrap_skeleton'
                                         />
                                     ))}
-                            </>
+                            </> 
                         ) : (
                             data?.map((item, index) => (
                                 <div key={index}>
@@ -33,25 +33,27 @@ export default function Search_Results_Services ({ data }) {
                             ))
                         )}
                     </div>
-                    {/* <Link href='#services'> */}
-                    <button className='forMoreBox'>
-                        Ko‘proq ko‘rish
-                        <svg
-                            xmlns='http://www.w3.org/2000/svg'
-                            width='18'
-                            height='18'
-                            viewBox='0 0 18 18'
-                            fill='none'>
-                            <path
-                                d='M11.6243 7.97984L7.86833 4.22389L8.85857 3.23364L14.305 8.68005L8.85857 14.1264L7.86833 13.1361L11.6243 9.38027H3.10156V7.97984H11.6243Z'
-                                fill='#312F30'
-                            />
-                        </svg>
-                    </button>
-                    {/* </Link> */}
                 </div>
                 <div className='forAdds'></div>
             </div>
         </div>
     );
 }
+
+
+// featured items
+
+{/* <button className='forMoreBox'>
+    Ko‘proq ko‘rish
+    <svg
+        xmlns='http://www.w3.org/2000/svg'
+        width='18'
+        height='18'
+        viewBox='0 0 18 18'
+        fill='none'>
+        <path
+            d='M11.6243 7.97984L7.86833 4.22389L8.85857 3.23364L14.305 8.68005L8.85857 14.1264L7.86833 13.1361L11.6243 9.38027H3.10156V7.97984H11.6243Z'
+            fill='#312F30'
+        />
+    </svg>
+</button> */}
