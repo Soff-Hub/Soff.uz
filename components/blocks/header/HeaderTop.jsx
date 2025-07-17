8a045afa428ea8a98f418d3c26991e3065efacc6
abdum0min <abduganiabdurasulov5150@gmail.com>
2025-07-17 17:32:09 +0500

    Update HeaderTop.jsx

--- components/blocks/header/HeaderTop.jsx
+++ components/blocks/header/HeaderTop.jsx
@@ -8,14 +8,14 @@ export default function HeaderTop() {
         <div className="container header__top__items py-2">
             <div className='d-flex justify-content-between align-items-center'>
 
-                {/* <Link href="https://t.me/soff_uz">
+                <Link href="https://t.me/soff_uz">
                     <a target='_blank' className="rounded-circle" 
                     style={{ display: 'flex', padding: 4, color: '#0088cc', border: "1px solid #0088cc"}} 
                     >
                         <i className='fa-regular fa-paper-plane'></i>
                     </a>
-                </Link> */}
-                <HeaderAIIcon />
+                </Link> 
+               {/*  <HeaderAIIcon />*/}
                 <MenuCategoriesDropdown />
             </div>
         </div>
