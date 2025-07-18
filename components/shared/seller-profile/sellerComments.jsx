101fdd44e6a7a331b4c2fd3727e4fe60a9791377
Abdumomin <abduganiabdurasulov5150@gmail.com>
2025-07-18 18:43:43 +0500

    some changes

--- components/shared/seller-profile/sellerComments.jsx
+++ components/shared/seller-profile/sellerComments.jsx
@@ -2,64 +2,121 @@ import { Rate, Skeleton } from 'antd';
 import React from 'react';
 import { useGet } from '~/repositories/https';
 import { getTimeAgo } from '~/utilities/calculateTime';
-export default function SellerComments ({ pid }) {
-    const {data: comments, isLoading} = useGet("comments", `http://176.96.241.219:8005/api/v1/comments/service-comments?service_id=9`)
-
-
+export default function SellerComments({ pid }) {
+    const { data: serviceComments, isLoading } = useGet("serviceComments", `http://176.96.241.219:8005/api/v1/comments/service-comments?seller_id=8`)
+    const { data: productComments, isLoading: isLoadingProduct } = useGet("productComments", `http://176.96.241.219:8006/api/v1/customer/reviews/${pid}`)
     return (
-        <div className='SellerComments'>
-            {isLoading && (
-                <>
-                    {Array(15)
-                        .fill(0)
-                        .map((d, i) => (
-                            <Skeleton
-                                key={i}
-                                active
-                                className='SellerCommentsSkeleton shadow'
-                            />
-                        ))}
-                </>
-            )}
-            {comments?.comments?.map((item, index) => (
-                <div key={index} className='SellerCommentsCard'>
-                    <div className='SellerCommentsCardAboutSeller'>
-                        <div className='SellerCommentsCardAboutSellerinfo mb-4'>
-                            <div className='SellerCommentsCardSellerAvatar'>
-                                <img
-                                    src={
-                                        item.user_image
-                                            ? item.user_image
-                                            : '/static/img/user_without_img.png'
-                                    }
-                                    alt={item.isName}
+        <div className='SellerComments '>
+            <div className='col-12 col-md-6 '>
+                <p style={{fontWeight: '600'}} className='fs-4'>Xizmatlar uchun commentlar</p>
+                {isLoading && (
+                    <>
+                        {Array(15)
+                            .fill(0)
+                            .map((d, i) => (
+                                <Skeleton
+                                    key={i}
+                                    active
+                                    className='SellerCommentsSkeleton shadow'
                                 />
-                                <p>{item.user.full_name}</p>
-                                <p className='SellerCommentsCardSellerActivety'>
-                                    {getTimeAgo(item.created_at)}
-                                </p>
+                            ))}
+                    </>
+                )}
+                {serviceComments?.comments?.map((item, index) => (
+                    <div key={index} className='SellerCommentsCard mb-4'>
+                        <div className='SellerCommentsCardAboutSeller'>
+                            <div className='SellerCommentsCardAboutSellerinfo mb-4'>
+                                <div className='SellerCommentsCardSellerAvatar'>
+                                    <img
+                                        src={
+                                            item.user_image
+                                                ? item.user_image
+                                                : '/static/img/user_without_img.png'
+                                        }
+                                        alt={item.isName}
+                                    />
+                                    <p>{item.buyer_user.full_name}</p>
+                                    <p className='SellerCommentsCardSellerActivety'>
+                                        {getTimeAgo(item.created_at)}
+                                    </p>
+                                </div>
+                                <Rate style={{ fontSize: '16px', color: 'orange' }} value={item.rating} disabled />
                             </div>
-                            <Rate style={{fontSize: '16px', color: 'orange'}} value={item.rating}  disabled/>
+                            <img
+                                src={item.rating}
+                                className='SellerCommentsCardSellerRating'
+                                alt=''
+                            />
                         </div>
-                        <img
-                            src={item.rating}
-                            className='SellerCommentsCardSellerRating'
-                            alt=''
-                        />
+                        <div className='SellerCommentsCardSellerCommentWrap'>
+                            <p className='SellerCommentsCardComment'>{item.content}</p>
+                        </div>
+                        <div className='SellerCommentsCardBtnWrap'>
+                            <a
+                                href={`/product/${item.slug}`}
+                                className='SellerCommentsCardBtn'>
+                                Xizmatni  ko'rish
+                            </a>
+                        </div>
                     </div>
-                    <div className='SellerCommentsCardSellerCommentWrap'>
-                        <p className='SellerCommentsCardComment'>{item.content}</p>
+                ))}
+                <p className='text-center mt-3 fs-5'>Ko'proq ko'rish . . .</p>
+            </div>
+            <div className='col-12 col-md-6 '>
+                <p style={{fontWeight: '600'}} className='fs-4'>Mahsulotlar uchun commentlar</p>
+                {isLoading && (
+                    <>
+                        {Array(15)
+                            .fill(0)
+                            .map((d, i) => (
+                                <Skeleton
+                                    key={i}
+                                    active
+                                    className='SellerCommentsSkeleton shadow'
+                                />
+                            ))}
+                    </>
+                )}
+                {productComments?.results?.map((item, index) => (
+                    <div key={index} className='SellerCommentsCard mb-4'>
+                        <div className='SellerCommentsCardAboutSeller'>
+                            <div className='SellerCommentsCardAboutSellerinfo mb-4'>
+                                <div className='SellerCommentsCardSellerAvatar'>
+                                    <img
+                                        src={
+                                            item.user_image
+                                                ? item.user_image
+                                                : '/static/img/user_without_img.png'
+                                        }
+                                        alt={item.isName}
+                                    />
+                                    <p>{item.user_full_name}</p>
+                                    <p className='SellerCommentsCardSellerActivety'>
+                                        {getTimeAgo(item.created_at)}
+                                    </p>
+                                </div>
+                                <Rate style={{ fontSize: '16px', color: 'orange' }} value={item.rating} disabled />
+                            </div>
+                            <img
+                                src={item.rating}
+                                className='SellerCommentsCardSellerRating'
+                                alt=''
+                            />
+                        </div>
+                        <div className='SellerCommentsCardSellerCommentWrap'>
+                            <p className='SellerCommentsCardComment'>{item.text}</p>
+                        </div>
+                        <div className='SellerCommentsCardBtnWrap'>
+                            <a
+                                href={`/product/${item.slug}`}
+                                className='SellerCommentsCardBtn'>
+                                Mahsulotni ko'rish
+                            </a>
+                        </div>
                     </div>
-                    <div className='SellerCommentsCardBtnWrap'>
-                        <a
-                            href={`/product/${item.slug}`}
-                            className='SellerCommentsCardBtn'>
-                            Mahsulotni ko'rish
-                        </a>
-                    </div>
-                </div>
-            ))}
-            <p className='text-center fs-5'>Ko'proq ko'rish . . .</p>
+                ))}
+                <p className='text-center fs-5'>Ko'proq ko'rish . . .</p>
+            </div>
         </div>
     );
 }
