9a0615bbfec8252c892a28ff4052b4b8d22134d7
Abdumomin <abduganiabdurasulov5150@gmail.com>
2025-08-12 14:13:51 +0500

    service list added

--- components/services/ServicesCardSection.jsx
+++ components/services/ServicesCardSection.jsx
@@ -1,19 +1,59 @@
-import React from 'react'
+import React from 'react';
 import ServiceCard from './ServiceCard';
+import { Empty } from 'antd';
+import { SmileOutlined } from '@ant-design/icons';
 
+const ServicesCardSection = ({ services }) => {
+    const hasProducts = services?.items?.length > 0;
+    const isFewProducts = services?.items?.length < 4; // Kam bo‘lsa ham 100vh qilamiz
 
-const ServicesCardSection = ({services}) => {
     return (
-        <div className='container'>
-            <div className='row'>
-                {services?.results?.map(service => (
-                    <div className='col-6 col-md-4 col-lg-3 custom-col-5 mb-4'>
-                        <ServiceCard product={service} />
+        <div className="container">
+            <div
+                className="row"
+                style={{
+                    minHeight: isFewProducts ? '50vh' : 'auto',
+                    alignItems: isFewProducts ? 'center' : 'flex-start',
+                    display: 'flex'
+                }}
+            >
+                {hasProducts ? (
+                    services.items.map((service, index) => (
+                        <div key={index} className="col-6 col-md-4 col-lg-3 custom-col-5 mb-4">
+                            <ServiceCard product={service} />
+                        </div>
+                    ))
+                ) : (
+                    <div className="col-12">
+                        <div
+                            style={{
+                                padding: '50px 0',
+                                display: 'flex',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                flexDirection: 'column',
+                                backgroundColor: '#fafafa',
+                                borderRadius: '8px',
+                                height: '50vh'
+                            }}
+                        >
+                            <Empty
+                                image={Empty.PRESENTED_IMAGE_SIMPLE}
+                                description={
+                                    <span style={{ fontSize: '16px', color: '#666' }}>
+                                        Hozircha hech qanday xizmat topilmadi
+                                    </span>
+                                }
+                            />
+                            <p style={{ color: '#999', marginTop: '10px' }}>
+                                Qidiruv yoki filtrlarni o‘zgartirib ko‘ring <SmileOutlined />
+                            </p>
+                        </div>
                     </div>
-                ))}
+                )}
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default ServicesCardSection
+export default ServicesCardSection;
--- pages/orders/index.jsx
+++ pages/orders/index.jsx
@@ -8,58 +8,55 @@ import ServicesCardSection from '~/components/services/ServicesCardSection';
 
 
 export default function SoffFreelancerPage({ servicesData }) {
-  console.log(servicesData)
-  return (
-    <PageContainer>
-      <Meta
-        title={'Raqamli mahsulot buyurtma berish - Soff.uz'}
-        description={'Soff.uz orqali raqamli mahsulotlarga buyurtma bering. Ishonchli sotuvchilar va sifatli kontent bilan tez va oson xizmatlardan foydalaning.'}
-        image="/static/img/video-darsliklar-2.png"
-        keywords={[{ name: "Biznes rejalar buyurtma berish" }, { name: "Taqdimotlar buyurtma berish" }, { name: "Kurs ishlari buyurtma berish" }, { name: "Diplom ishlari buyurtma berish" }, { name: "Referatlar buyurtma berish" }, { name: "Mustaqil ishlar buyurtma berish" }, { name: "Labaratoriya Ishlari buyurtma berish" }, { name: "Dissertatsiya ishlari buyurtma berish" }, { name: "Testlar buyurtma berish" }, { name: "O'quv qo'llanmalar buyurtma berish" }, { name: "MustDars ishlanmalaraqil buyurtma berish" }, { name: "Tarqatma materiallar buyurtma berish" }, { name: "Amaliy ishlar buyurtma berish" }, { name: "Blankalar buyurtma berish" }, { name: "Ijodiy Ishlar buyurtma berish" }, { name: "Loyihalar buyurtma berish" }, { name: "Plakatlar buyurtma berish" }, { name: "Elektron kitoblar buyurtma berish" }, { name: "Dasturlash tillari" }]}
-        author="Soff.uz"
-      />
+    console.log(servicesData)
+    return (
+        <PageContainer>
+            <Meta
+                title={'Raqamli mahsulot buyurtma berish - Soff.uz'}
+                description={'Soff.uz orqali raqamli mahsulotlarga buyurtma bering. Ishonchli sotuvchilar va sifatli kontent bilan tez va oson xizmatlardan foydalaning.'}
+                image="/static/img/video-darsliklar-2.png"
+                keywords={[{ name: "Biznes rejalar buyurtma berish" }, { name: "Taqdimotlar buyurtma berish" }, { name: "Kurs ishlari buyurtma berish" }, { name: "Diplom ishlari buyurtma berish" }, { name: "Referatlar buyurtma berish" }, { name: "Mustaqil ishlar buyurtma berish" }, { name: "Labaratoriya Ishlari buyurtma berish" }, { name: "Dissertatsiya ishlari buyurtma berish" }, { name: "Testlar buyurtma berish" }, { name: "O'quv qo'llanmalar buyurtma berish" }, { name: "MustDars ishlanmalaraqil buyurtma berish" }, { name: "Tarqatma materiallar buyurtma berish" }, { name: "Amaliy ishlar buyurtma berish" }, { name: "Blankalar buyurtma berish" }, { name: "Ijodiy Ishlar buyurtma berish" }, { name: "Loyihalar buyurtma berish" }, { name: "Plakatlar buyurtma berish" }, { name: "Elektron kitoblar buyurtma berish" }, { name: "Dasturlash tillari" }]}
+                author="Soff.uz"
+            />
 
-      <div className='ps-page--shop my-5 container p-xl-0 p-l-0'>
-        <ServicesFilterSection count={servicesData?.count} />
-
-
-        <ServicesCardSection services={servicesData} />
-      </div>
+            <div className='ps-page--shop my-5 container p-xl-0 p-l-0'>
+                <ServicesFilterSection count={servicesData?.count} />
 
 
+                <ServicesCardSection services={servicesData} />
+            </div>
 
-    </PageContainer>
-  );
+
+
+        </PageContainer>
+    );
 }
 
 export async function getServerSideProps(context) {
-  const { query } = context;
-  const {
-    direction = '',
-    search = '',
-    category,
-    budget = '',
-    delivary = '',
-    rating = ''
-  } = query;
+    const { query } = context;
+    const {
+        category_id = '',
+        search = '',
+        direction = '',
+        limit = 20,
+        offset = 0
+    } = query;
 
-  const queryParams = new URLSearchParams({
-    direction,
-    search,
-    category,
-    budget,
-    delivary,
-    rating
-  });
+    const queryParams = new URLSearchParams({
+        ...(category_id && { category_id }),
+        ...(search && { search }),
+        ...(direction && { direction }),
+        limit,
+        offset
+    });
 
-  const url = `http://176.96.241.219:8005/api/v1/users/sellers/service?${queryParams.toString()}`;
+    const url = `http://176.96.241.219:8005/api/v1/customer?${queryParams.toString()}`;
+    const res = await fetch(url);
+    const servicesData = res.ok ? await res.json() : null;
 
-  const res = await fetch(url);
-  const servicesData = res.ok ? await res.json() : null;
-
-  return {
-    props: {
-      servicesData,
-    },
-  };
+    return {
+        props: {
+            servicesData,
+        },
+    };
 }
