e406883ee45f81a3947d1dc028f3fa271a3e1e24
Abdumomin <abduganiabdurasulov5150@gmail.com>
2025-08-14 15:47:24 +0500

    token bug fixed

--- components/shared/headers/fast-dowload/FastDowloadApi.js
+++ components/shared/headers/fast-dowload/FastDowloadApi.js
@@ -2,27 +2,46 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 import { baseURL } from '~/repositories/api';
 
-const token = Cookies.get("token")
+const getToken = () => Cookies.get("token");
 
 export const fetchFastDownloadProduct = async () => {
+    const token = getToken();
 
     if (!token) {
         throw new Error('Token mavjud emas. Foydalanuvchi tizimga kirmagan.');
     }
 
-    const res = await axios.get(`${baseURL}customer/downloadable-docs/`, {
-        headers: {
-            Authorization: `Bearer ${token}`,
+    try {
+        const res = await axios.get(`${baseURL}customer/downloadable-docs/`, {
+            headers: { Authorization: `Bearer ${token}` }
+        });
+        return res.data;
+    } catch (error) {
+        if (error.response && error.response.status === 403) {
+            Cookies.remove('token'); // tokenni o'chirish
+            throw new Error('Token invalid yoki muddati o‘tgan. Iltimos, qaytadan tizimga kiring.');
         }
-    });
-    return res.data;
+        throw error;
+    }
 };
 
 export const fetchProductDowload = async (id) => {
-    const res = await axios.get(`${baseURL}customer/download/${id}/`, {
-        headers: {
-            Authorization: `Bearer ${token}`,
+    const token = getToken();
+
+    if (!token) {
+        throw new Error('Token mavjud emas. Foydalanuvchi tizimga kirmagan.');
+    }
+
+    try {
+        const res = await axios.get(`${baseURL}customer/download/${id}/`, {
+            headers: { Authorization: `Bearer ${token}` }
+        });
+        return res.data;
+    } catch (error) {
+        if (error.response && error.response.status === 401) {
+            Cookies.remove('token');
+            throw new Error('Token invalid yoki muddati o‘tgan.');
         }
-    })
-    return res.data
-}
+        throw error;
+    }
+};
