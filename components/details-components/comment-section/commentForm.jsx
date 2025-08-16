90c032a5c9e55acea4a27165203376d1ba0250a7
Abdumomin <abduganiabdurasulov5150@gmail.com>
2025-08-16 11:27:27 +0500

    simple star deleted

--- components/details-components/comment-section/commentForm.jsx
+++ components/details-components/comment-section/commentForm.jsx
@@ -1,19 +1,18 @@
 import { useState } from 'react';
-import { Rating } from 'react-simple-star-rating';
 import Cookies from 'js-cookie';
 import axios from 'axios';
 import { baseURL } from '~/repositories/api';
 import toast from 'react-hot-toast';
+import { Rate } from 'antd';
 
 export default function CommentForm({ documentId, fComment }) {
   const [text, setText] = useState('');
   const [rating, setRating] = useState(0);
-  const [ratingKey, setRatingKey] = useState(0);
   const [loading, setLoading] = useState(false);
 
   const handleSubmit = async (e) => {
-    // e.preventDefault();
-    if (!text.trim() ) return;
+    e.preventDefault();
+    if (!text.trim()) return;
 
     const token = Cookies.get('token');
     if (!token) {
@@ -23,7 +22,7 @@ export default function CommentForm({ documentId, fComment }) {
 
     try {
       setLoading(true);
-      const res = await axios.post(
+      await axios.post(
         `${baseURL}seller/document-review/${documentId}`,
         {
           text,
@@ -38,18 +37,13 @@ export default function CommentForm({ documentId, fComment }) {
       );
 
       setText('');
-      setRating(0);
-      setRatingKey(prev => prev + 1); // reset star rating
-      toast.success("Izoh muvaffaqiyatli yuborildi!")
+      setRating(0); // Rate reset
+      toast.success("Izoh muvaffaqiyatli yuborildi!");
     } catch (err) {
-      toast.error("Izoh yuborilmadi.")
+      toast.error("Izoh yuborilmadi.");
     } finally {
       setLoading(false);
     }
-  };
-
-  const handleRating = (rate) => {
-    setRating(rate);
   };
 
   return (
@@ -65,11 +59,11 @@ export default function CommentForm({ documentId, fComment }) {
         />
         <div className="d-flex justify-content-between align-items-center mt-3">
           {!fComment ? (
-            <Rating
-              key={ratingKey}
-              onClick={handleRating}
-              size={25}
-              initialValue={rating}
+            <Rate
+              value={rating}
+              onChange={(value) => setRating(value)}
+              style={{ fontSize: 25 }}
+              allowHalf
             />
           ) : (
             <div></div>
--- components/details-components/seller-detail/seller-service-rating-component.jsx
+++ components/details-components/seller-detail/seller-service-rating-component.jsx
@@ -1,46 +1,43 @@
 import React from 'react';
-import { Rating } from 'react-simple-star-rating';
+import { Rate } from 'antd';
 
-export default function SellerRating ({ data }) {
+export default function SellerRating({ data }) {
     return (
-        <div className='mt-2'>
-            <p className='fs-3'>Mezonlar bo‘yicha reytinglar</p>
+        <div className="mt-2">
+            <p className="fs-3">Mezonlar bo‘yicha reytinglar</p>
+
             {data?.reviews?.ratings?.speed !== 0 && (
-                <div className='d-flex gap-2 align-items-center'>
-                    <p className='p-0 m-0 fs-4'>Tezlik:</p>
-                    <Rating
-                        readonly
-                        allowFraction
-                        initialValue={data?.reviews?.ratings?.speed}
-                        size={20}
-                        fillColor='orange'
-                        emptyColor='gray'
+                <div className="d-flex gap-2 align-items-center">
+                    <p className="p-0 m-0 fs-4">Tezlik:</p>
+                    <Rate
+                        disabled
+                        allowHalf
+                        value={data?.reviews?.ratings?.speed}
+                        style={{ fontSize: 20 }}
                     />
                 </div>
             )}
+
             {data?.reviews?.ratings?.quality !== 0 && (
-                <div className='d-flex gap-2 align-items-center'>
-                    <p className='p-0 m-0 fs-4'>Sifat:</p>
-                    <Rating
-                        readonly
-                        allowFraction
-                        initialValue={data?.reviews?.ratings?.quality}
-                        size={20}
-                        fillColor='orange'
-                        emptyColor='gray'
+                <div className="d-flex gap-2 align-items-center">
+                    <p className="p-0 m-0 fs-4">Sifat:</p>
+                    <Rate
+                        disabled
+                        allowHalf
+                        value={data?.reviews?.ratings?.quality}
+                        style={{ fontSize: 20 }}
                     />
                 </div>
             )}
+
             {data?.reviews?.ratings?.communication !== 0 && (
-                <div className='d-flex gap-2 align-items-center'>
-                    <p className='p-0 m-0 fs-4'> Muloqot (aloqa sifati):</p>
-                    <Rating
-                        readonly
-                        allowFraction
-                        initialValue={data?.reviews?.ratings?.communication}
-                        size={20}
-                        fillColor='orange'
-                        emptyColor='gray'
+                <div className="d-flex gap-2 align-items-center">
+                    <p className="p-0 m-0 fs-4">Muloqot (aloqa sifati):</p>
+                    <Rate
+                        disabled
+                        allowHalf
+                        value={data?.reviews?.ratings?.communication}
+                        style={{ fontSize: 20 }}
                     />
                 </div>
             )}
