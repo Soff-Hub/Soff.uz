aaa535b541a997d36c564f67064a4cf3f7eb615d
asilbek_tursun <148640148+AsilbekTursunov@users.noreply.github.com>
2025-08-22 15:47:55 +0500

    congrat modal ant headline

--- components/freeleance/services/ServicesCardSection.jsx
+++ components/freeleance/services/ServicesCardSection.jsx
@@ -2,68 +2,70 @@ import React from 'react';
 import ServiceCard from './ServiceCard';
 import { Empty } from 'antd';
 import { SmileOutlined } from '@ant-design/icons';
-
+import styles from './ServiceFilterSection.module.scss'
 const ServicesCardSection = ({ services }) => {
-          const hasProducts = services?.items?.length > 0;
-          const isFewProducts = services?.items?.length < 4; // Kam bo‘lsa ham 100vh qilamiz
+    const hasProducts = services?.items?.length > 0;
+    const isFewProducts = services?.items?.length < 4; // Kam bo‘lsa ham 100vh qilamiz
 
-          return (
-              <div className="container">
-                  <div
-                      className="row"
-                      style={{
-                          minHeight: isFewProducts ? '50vh' : 'auto',
-                          alignItems: isFewProducts ? 'center' : 'stretch',
-                          display: 'flex',
-                      }}>
-                      {hasProducts ? (
-                          services.items.map((service, index) => (
-                              <div
-                                  key={index}
-                                  className="col-6 col-md-4 col-lg-3 px-1 custom-col-5 mb-4">
-                                  <ServiceCard product={service} />
-                              </div>
-                          ))
-                      ) : (
-                          <div className="col-12">
-                              <div
-                                  style={{
-                                      padding: '50px 0',
-                                      display: 'flex',
-                                      justifyContent: 'center',
-                                      alignItems: 'center',
-                                      flexDirection: 'column',
-                                      backgroundColor: '#fafafa',
-                                      borderRadius: '8px',
-                                      height: '50vh',
-                                  }}>
-                                  <Empty
-                                      image={Empty.PRESENTED_IMAGE_SIMPLE}
-                                      description={
-                                          <span
-                                              style={{
-                                                  fontSize: '16px',
-                                                  color: '#666',
-                                              }}>
-                                              Hozircha hech qanday xizmat
-                                              topilmadi
-                                          </span>
-                                      }
-                                  />
-                                  <p
-                                      style={{
-                                          color: '#999',
-                                          marginTop: '10px',
-                                      }}>
-                                      Qidiruv yoki filtrlarni o‘zgartirib
-                                      ko‘ring <SmileOutlined />
-                                  </p>
-                              </div>
-                          </div>
-                      )}
-                  </div>
-              </div>
-          );
-      };;
+    return (
+        <div className={styles.servicesSection}>
+            <div className="container z-3 position-relative">
+                <div
+                    className="row"
+                    style={{
+                        minHeight: isFewProducts ? '50vh' : 'auto',
+                        alignItems: isFewProducts ? 'center' : 'stretch',
+                        display: 'flex',
+                    }}>
+                    {hasProducts ? (
+                        services.items.map((service, index) => (
+                            <div
+                                key={index}
+                                className="col-6 col-md-4 col-lg-3 px-1 custom-col-5 mb-4">
+                                <ServiceCard product={service} />
+                            </div>
+                        ))
+                    ) : (
+                        <div className="col-12">
+                            <div
+                                style={{
+                                    padding: '50px 0',
+                                    display: 'flex',
+                                    justifyContent: 'center',
+                                    alignItems: 'center',
+                                    flexDirection: 'column',
+                                    backgroundColor: '#fafafa',
+                                    borderRadius: '8px',
+                                    height: '50vh',
+                                }}>
+                                <Empty
+                                    image={Empty.PRESENTED_IMAGE_SIMPLE}
+                                    description={
+                                        <span
+                                            style={{
+                                                fontSize: '16px',
+                                                color: '#666',
+                                            }}>
+                                            Hozircha hech qanday xizmat
+                                            topilmadi
+                                        </span>
+                                    }
+                                />
+                                <p
+                                    style={{
+                                        color: '#999',
+                                        marginTop: '10px',
+                                    }}>
+                                    Qidiruv yoki filtrlarni o‘zgartirib ko‘ring{' '}
+                                    <SmileOutlined />
+                                </p>
+                            </div>
+                        </div>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+};
 
 export default ServicesCardSection;
--- components/freeleance/services/service-deatail/ServiceDetail.jsx
+++ components/freeleance/services/service-deatail/ServiceDetail.jsx
@@ -1,74 +1,116 @@
-import React from 'react'
-import ImageCarousel from './ui/ImageCarousel'
-import styles from "./styles/detail.module.scss"
-import PriceBox from './ui/PriceBox'
-import MoneyBack from './ui/MoneyBack'
-import UserBox from './ui/UserBox'
-import ServiceDescription from './ui/ServiceDescription'
-import FaqSection from './ui/FaqSection'
-import PortfolioSection from './ui/PortfolioSection'
-import SwiperPages from '~/components/details-components/swiper/swiper-page'
-import ServiceCard from '../ServiceCard'
-import { useRouter } from 'next/router'
-import CommentSection from './ui/CommentSection'
+import React, { useMemo } from 'react';
+import ImageCarousel from './ui/ImageCarousel';
+import styles from './styles/detail.module.scss';
+import PriceBox from './ui/PriceBox';
+import MoneyBack from './ui/MoneyBack';
+import UserBox from './ui/UserBox';
+import ServiceDescription from './ui/ServiceDescription';
+import FaqSection from './ui/FaqSection';
+import PortfolioSection from './ui/PortfolioSection';
+import SwiperPages from '~/components/details-components/swiper/swiper-page';
+import ServiceCard from '../ServiceCard';
+import { useRouter } from 'next/router';
+import CommentSection from './ui/CommentSection';
+import Link from 'next/link';
+import { Breadcrumb, Button } from 'antd';
 
 const ServiceDetail = ({ data }) => {
-    const { push } = useRouter()
-    const { service, seller_portfolio, similar_services, faqs, order_requirements, user, service_items } = data 
-    
-    const pushUser = () => push(`/_seller/${data?.user[0]?.soff_seller_id}#about_author`)
+    const { push, back } = useRouter();
+    const {
+        service,
+        seller_portfolio,
+        similar_services,
+        faqs,
+        order_requirements,
+        user,
+        service_items,
+    } = data;
+
+    const pushUser = () =>
+        push(`/_seller/${data?.user[0]?.soff_seller_id}#about_author`);
     const priceBox = {
         days: service?.delivery_days,
         price: service?.price,
         revisions: service?.right_to_change,
         id: service?.id,
         title: service?.title,
-        user: user
-    }
-    const { full_name, photo_url } = user[0]
+        user: user,
+    };
+    const { full_name, photo_url } = user[0];
     const description = {
         description: service?.description,
         requirements: order_requirements[0]?.order_requirement_description,
         file: order_requirements[0]?.order_requirement_file,
-        serviceItems: service_items
-    } 
- 
+        serviceItems: service_items,
+    };
+
+    const breadcrumbItems = useMemo(
+        () => [
+            {
+                title: 'Buyurtmalar',
+                href: '/orders?direction=scientific_work',
+            },
+            {
+                title: <span>{service?.title}</span>,
+            },
+        ],
+        [data]
+    );
 
     return (
-        <div style={{ maxWidth: "1400px" }} className='container my-5'>
-            <div className='row'>
-                <div style={{ background: "white", padding: "20px", borderRadius: "12px" }} className='col-12 col-md-8'>
-                    <h1 className={styles.title}>{service?.title}</h1>
-                    <div className={styles.userBox}>
-                        <img
-                            className={styles.avatar}
-                            src={photo_url || "/static/img/ozodbek.png"}
-                            alt={service?.user?.full_name || "User"}
-                            style={{cursor: "pointer"}}
-                            onClick={pushUser}
+        <div style={{ maxWidth: '1400px' }} className="container my-5">
+            <div className="row">
+                <div className="col-12 col-md-8">
+                    <Breadcrumb
+                        className="mb-3 d-flex align-items-center"
+                        items={breadcrumbItems}
+                    />
+                    <div
+                        style={{
+                            background: 'white',
+                            padding: '20px',
+                            borderRadius: '12px',
+                        }}
+                        className="w-100">
+                        <h1 className={styles.title}>{service?.title}</h1>
+                        <div className={styles.userBox}>
+                            <img
+                                className={styles.avatar}
+                                src={photo_url || '/static/img/ozodbek.png'}
+                                alt={service?.user?.full_name || 'User'}
+                                style={{ cursor: 'pointer' }}
+                                onClick={pushUser}
+                            />
+                            <span
+                                style={{ cursor: 'pointer' }}
+                                onClick={pushUser}
+                                className={styles.username}>
+                                {full_name || 'No Name'}
+                            </span>
+                        </div>
+                        <ImageCarousel images={service?.poster} />
+                        <ServiceDescription
+                            priceBox={priceBox}
+                            description={description}
                         />
-                        <span style={{cursor: "pointer"}} onClick={pushUser} className={styles.username}>{full_name || "No Name"}</span>
                     </div>
-                    <ImageCarousel images={service?.poster} />
-                    <ServiceDescription priceBox={priceBox} description={description} />
                 </div>
-                <div className='col-12 col-md-4'>
+                <div className="col-12 col-md-4">
                     <PriceBox priceBox={priceBox} />
                     <MoneyBack />
                     <UserBox priceBox={priceBox} pushUser={pushUser} />
                 </div>
             </div>
-            <div className='row'>
-                <div className='col-12 col-md-8 p-0'>
+            <div className="row">
+                <div className="col-12 col-md-8 p-0">
                     <FaqSection faqs={faqs} />
                     <PortfolioSection portfolios={seller_portfolio} />
                 </div>
             </div>
-       
-            <CommentSection type={"service_id"} id={service?.id} />
-            
-            {
-                similar_services.length > 0 &&
+
+            <CommentSection type={'service_id'} id={service?.id} />
+
+            {similar_services.length > 0 && (
                 <div>
                     <h3>O'xshash xizmatlar</h3>
                     <SwiperPages type={'file'}>
@@ -77,9 +119,9 @@ const ServiceDetail = ({ data }) => {
                         ))}
                     </SwiperPages>
                 </div>
-            }
+            )}
         </div>
-    )
-}
+    );
+};
 
-export default ServiceDetail
+export default ServiceDetail;
