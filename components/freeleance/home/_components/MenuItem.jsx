e5f7403ef323e3541aa6804a1b69103aa46c7d20
asilbek_tursun <148640148+AsilbekTursunov@users.noreply.github.com>
2025-08-26 23:34:41 +0500

    sidebar section

--- components/freeleance/home/_components/MenuItem.jsx
+++ components/freeleance/home/_components/MenuItem.jsx
@@ -1,25 +1,45 @@
 import React, { useState } from 'react';
 import styles from '../styles/menuItem.module.scss';
-export const MenuItem = ({ items, label }) => {
+
+const option = {
+    scientific_work: 'Ilmiy va Akademik Xizmatlar',
+    three_d: '3D Dizayn va Vizualizatsiya',
+    web: 'Dasturlash xizmatlari',
+    dizyn: 'Dizayn',
+    document: 'Shablonlar',
+};
+
+const link = {
+    templates:
+        'scientific-resources/?slug=all&search=&parentCategory=dars-1&childCategory=',
+    orders:
+        'orders?direction=scientific_work&search=&parent_category_id=&category_id=',
+};
+
+export const MenuItem = ({ products, templates, label }) => {
     return (
         <div className={styles.menuItem}>
             <button type="button" className={styles.label}>
-                {label}
+                {option[label]}
             </button>
             <div className={styles.dropDown}>
                 <div className={styles.orders}>
                     <h3 className={styles.sectionLabel}>Buyurtma berish</h3>
                     <ul className={styles.details}>
-                        {items.orders.map(item => (
-                            <li className={styles.detail}>{item.label}</li>
+                        {products.map(item => (
+                            <li key={item.id} className={styles.detail}>
+                                {item.title}
+                            </li>
                         ))}
                     </ul>
                 </div>
                 <div className={styles.templates}>
                     <h3 className={styles.sectionLabel}>Tayyor mahsulotlar</h3>
                     <ul className={styles.details}>
-                        {items.templates.map(item => (
-                            <li className={styles.detail}>{item.label}</li>
+                        {templates.map(item => (
+                            <li key={item.id} className={styles.detail}>
+                                {item.title}
+                            </li>
                         ))}
                     </ul>
                 </div>
@@ -27,3 +47,18 @@ export const MenuItem = ({ items, label }) => {
         </div>
     );
 };
+
+{
+    /* {items.map(item => (
+                        <div key={item.title}>
+                            <p className={styles.detailsTitle}>{item.title}</p>
+                            <ul className={styles.details}>
+                                {item.children.map(child => (
+                                    <li key={child} className={styles.detail}>
+                                        {child}
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    ))} */
+}
