891947abb0265883350a37293ecb145797dc46bd
asilbek_tursun <148640148+AsilbekTursunov@users.noreply.github.com>
2025-08-30 11:11:49 +0500

    new changes delete not used codes

--- components/partials/category/ProductsByCategory.jsx
+++ components/partials/category/ProductsByCategory.jsx
@@ -3,26 +3,26 @@ import { Pagination } from 'antd';
 import Link from 'next/link';
 import ProductCard from '~/components/freeleance/home/ui/ProductCard';
 
-export default function ProductsByCategory ({
+export default function ProductsByCategory({
     data = [],
     page,
     handlePagination,
-}){
+}) {
     return (
-        <section className='container'>
+        <section className="">
             <div className="row px-1 row-cols-2 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 row-gap-2 row-gap-md-5 row-gap-lg-3">
-                {data?.results?.map((item) => (
-                        <div key={item.id} className="col px-1 px-md-3 px-lg-2">
-                            <ProductCard product={item}/>
-                        </div>
-                    ))}
+                {data?.results?.map(item => (
+                    <div key={item.id} className="col px-1 px-md-3 px-lg-2">
+                        <ProductCard product={item} />
+                    </div>
+                ))}
             </div>
             {(data?.count == 0 || !data) && (
-                <div className='ps-page-status'>
-                    <div className='container'>
-                        <div className='ps-section__content'>
+                <div className="ps-page-status">
+                    <div className="container">
+                        <div className="ps-section__content">
                             <img
-                                src='/static/img/noinfo.svg'
+                                src="/static/img/noinfo.svg"
                                 alt="Ma'lumot topilmadi"
                             />
                             <h3>😕 Bu yerda hozircha hech narsa yo‘q...</h3>
@@ -34,8 +34,8 @@ export default function ProductsByCategory ({
                                 Imkoniyatni qo‘ldan boy bermang!
                             </p>
                             <p>
-                                <Link href='https://seller.soff.uz'>
-                                    <a target='_blank'>Sotuvchi bo'lish</a>
+                                <Link href="https://seller.soff.uz">
+                                    <a target="_blank">Sotuvchi bo'lish</a>
                                 </Link>
                             </p>
                         </div>
@@ -44,9 +44,9 @@ export default function ProductsByCategory ({
             )}
 
             {data?.count >= 48 && (
-                <div className='d-flex justify-content-center mt-5'>
+                <div className="d-flex justify-content-center mt-5">
                     <Pagination
-                        className='text-success'
+                        className="text-success"
                         total={data?.count}
                         pageSize={48}
                         responsive={true}
