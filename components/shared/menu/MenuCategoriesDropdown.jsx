358b110622845fedb94a4522a858df578e30a814
Zufarbek <zufarbek.abdurakhmonov@gmail.com>
2025-09-12 06:04:58 +0500

    removed buyrtmalarim

--- components/shared/menu/MenuCategoriesDropdown.jsx
+++ components/shared/menu/MenuCategoriesDropdown.jsx
@@ -7,11 +7,11 @@ const MenuCategoriesDropdown = () => {
     const { isMobile } = useResponsive()
     return (
         <div className="menu--product-categories">
-            {!isMobile && 
+            {/* {!isMobile && 
                 <Link href={'/order/my-orders'} target="_blank">
                     <p className={`${styles.navLink} mx-0 my-0`}>Buyurtmalarim</p>
                 </Link>
-            }
+            } */}
             <Link href={'https://seller.soff.uz/'} target="_blank">
                 <p className={`${styles.navLink} my-0`}>Frilanser bo'lish</p>
             </Link>
