8d6ef26c825d05427605baa6033895dab412683d
Abdumomin <abduganiabdurasulov5150@gmail.com>
2025-09-20 19:49:34 +0500

    tg notlification added

---
+++ components/freeleance/components/telegram-notlification/index.jsx
@@ -0,0 +1,79 @@
+import { useState, useEffect } from "react";
+import { message, Switch, Tooltip, ConfigProvider } from "antd";
+import { InfoCircleOutlined } from "@ant-design/icons";
+import useGetProfile from "../../api/useGetProfile";
+import { useGet, usePatch } from "~/repositories/https";
+import useResponsive from "~/utilities/useResponsive";
+
+export default function TelegramNotification() {
+    const { isMobile } = useResponsive();
+    const { data: tg_link } = useGet(
+        "tg_link",
+        `${process.env.NEXT_PUBLIC_BASE_URL}/auth/get-telegram-link/`
+    );
+    const { data: newProfile } = useGetProfile();
+    const { mutate, isLoading } = usePatch("nimadir");
+
+    const handleOff = () => {
+        mutate(
+            {
+                url: `${process.env.NEXT_PUBLIC_BASE_URL}/auth/profile/`,
+                payload: { telegram_chat_id: null },
+            },
+            {
+                onSuccess: () => {
+                    message.success("Bildirishnomalar o‘chirildi!");
+                },
+            }
+        );
+    };
+
+    const [checked, setChecked] = useState(false);
+
+    useEffect(() => {
+        if (newProfile?.telegram_chat_id) {
+            setChecked(true);
+        } else {
+            setChecked(false);
+        }
+    }, [newProfile]);
+
+    const handleSwitchChange = (value) => {
+        if (value) {
+            if (!newProfile?.telegram_chat_id && tg_link?.link_code) {
+                window.open(tg_link.link_code, "_blank");
+            }
+        } else {
+            handleOff();
+        }
+        setChecked(value);
+    };
+
+    return (
+        <div
+            className={`alert alert-warning d-flex ${
+                isMobile && "flex-column gap-3"
+            } align-items-center justify-content-between`}
+        >
+            <div className="d-flex align-items-center gap-2">
+                <span>Telegram bildirishnomalarini yoqish</span>
+                <Tooltip title="Agar yoqsangiz, yangi buyurtma va xabarnomalar Telegram orqali yuboriladi">
+                    <InfoCircleOutlined style={{ color: "#faad14" }} />
+                </Tooltip>
+            </div>
+            <ConfigProvider
+                theme={{
+                    token: {
+                        colorPrimary: "#00a651", // switch rangini yashil qilib qo‘yamiz
+                    },
+                }}
+            >
+                <Switch
+                    loading={isLoading}
+                    checked={checked}
+                    onChange={handleSwitchChange}
+                />
+            </ConfigProvider>
+        </div>
+    );
+}
