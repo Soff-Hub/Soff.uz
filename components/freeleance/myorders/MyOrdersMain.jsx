d2328f16aedd96be62ca1766f1f81293a7795b9f
Abdumomin <abduganiabdurasulov5150@gmail.com>
2025-09-20 19:49:34 +0500

    tg notlification added

--- components/freeleance/myorders/MyOrdersMain.jsx
+++ components/freeleance/myorders/MyOrdersMain.jsx
@@ -4,6 +4,7 @@ import Loader from '~/components/shared/loader';
 import { useQuery } from '@tanstack/react-query';
 import axiosInstance from '../api/freeleanceApi';
 import useGetCustomBalance from './myorder/api/useGetCustomBalance';
+import TelegramNotification from '../components/telegram-notlification';
 
 // MyOrderTabs faqat client-side'da yuklanadi
 const MyOrderTabs = dynamic(() => import('./myorder/MyOrderTabs'), {
@@ -16,6 +17,7 @@ const MyOrdersMain = () => {
 
     return (
         <div className='navTabsPadding' style={{ margin: '40px 0px', maxWidth: '100%' }}>
+            <TelegramNotification/>
             <div className="d-flex flex-column flex-sm-row align-items-start mb-4 justify-content-between">
                 <h1 className="fs-1 m-0">Mening buyurtmalarim</h1>
                 <span className="fs-3">
