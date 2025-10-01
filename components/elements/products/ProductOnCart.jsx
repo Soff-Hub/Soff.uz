3dafbd0dff8cdd60e8e66500921d776119f459fe
Mirsaid Mirakhmedov <mir21.07.2004@gmail.com>
2025-10-01 17:05:37 +0500

    update: shopping cart view

--- components/elements/products/ProductOnCart.jsx
+++ components/elements/products/ProductOnCart.jsx
@@ -6,10 +6,8 @@ import { addPeriodToThousands } from '~/components/partials/account/price-format
 const ProductOnCart = ({ product, children }) => {
     const { thumbnailImage, title } = useProduct();
 
-   
-
     return (
-        <div className="ps-product--cart-mobile">
+        <div className="ps-card">
             <div className="ps-product__thumbnail">
                 <Link href="/product/[pid]" as={`/product/${product?.slug}`}>
                     <a>{thumbnailImage(product)}</a>
@@ -19,15 +17,22 @@ const ProductOnCart = ({ product, children }) => {
                 {title(product)}
                 <p>
                     <small>
-                        {+product?.discount_price === 0 ? <p className='free-product-text'>Bepul</p> : product?.discount === 0 ? (
-                            <p>{addPeriodToThousands(product.discount_price)} so'm</p>
+                        {+product?.discount_price === 0 ? (
+                            <p className="free-product-text">Bepul</p>
+                        ) : product?.discount === 0 ? (
+                            <p>
+                                {addPeriodToThousands(product.discount_price)}{' '}
+                                so'm
+                            </p>
                         ) : (
                             <>
                                 <del>
                                     {addPeriodToThousands(product?.price)} so'm
                                 </del>
                                 <p>
-                                    {addPeriodToThousands(product?.discount_price)}
+                                    {addPeriodToThousands(
+                                        product?.discount_price
+                                    )}
                                     so'm
                                 </p>
                             </>
