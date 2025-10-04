2e32c227cccd2d9423d4556b0fe07c62207a99c7
Mirsaid Mirakhmedov <mir21.07.2004@gmail.com>
2025-10-04 17:23:42 +0500

    update: navbar menu items and style

---
+++ widgets/navbar-menu/swiperController.jsx
@@ -0,0 +1,33 @@
+import { useSwiper } from 'swiper/react';
+import { useEffect } from 'react';
+
+function SwiperController({ getMethods, setEnding, setBeginning }) {
+    const swiper = useSwiper();
+
+    swiper.isBeginning;
+
+    useEffect(() => {
+        getMethods(swiper);
+        setEnding(swiper.isEnd);
+        setBeginning(swiper.isBeginning);
+    }, [swiper, getMethods, setEnding, setBeginning]);
+
+    return (
+        <>
+            <button
+                aria-label="previous"
+                style={{ display: 'none' }}
+                onClick={() => swiper.slidePrev()}>
+                prev
+            </button>
+            <button
+                aria-label="next"
+                style={{ display: 'none' }}
+                onClick={() => swiper.slideNext()}>
+                next
+            </button>
+        </>
+    );
+}
+
+export default SwiperController;
