517fa45dfd90d3feee6e10a25920c2802818f494
Mirsaid <mir21.07.2004@gmail.com>
2025-10-04 06:59:17 +0500

    update: git basket

--- components/elements/products/ProductOnCart.jsx
+++ components/elements/products/ProductOnCart.jsx
@@ -2,25 +2,32 @@ import React from 'react';
 import Link from 'next/link';
 import useProduct from '~/shared/hooks/useProduct';
 import { addPeriodToThousands } from '~/components/partials/account/price-formatter';
+import useCart from '~/shared/hooks/useCart';
+import { Button } from 'antd';
 
-const ProductOnCart = ({ product, children }) => {
+const ProductOnCart = ({ product }) => {
     const { thumbnailImage, title } = useProduct();
+    const { removeCartOneItem } = useCart();
 
+    function handleRemoveItem(e, item) {
+        e.preventDefault();
+        removeCartOneItem(item.id);
+    }
     return (
-        <div className="ps-card">
-            <div className="ps-product__thumbnail">
+        <div className="ps-basket__content__items__item">
+            <div className="ps-basket__content__items__item__thumbnail">
                 <Link href="/product/[pid]" as={`/product/${product?.slug}`}>
                     <a>{thumbnailImage(product)}</a>
                 </Link>
             </div>
-            <div className="ps-product__content">
-                {title(product)}
+            <div className="ps-basket__content__items__item__content">
+                <h4>{title(product)}</h4>
                 <p>
                     <small>
                         {+product?.discount_price === 0 ? (
                             <p className="free-product-text">Bepul</p>
                         ) : product?.discount === 0 ? (
-                            <p>
+                            <p className="price">
                                 {addPeriodToThousands(product.discount_price)}{' '}
                                 so'm
                             </p>
@@ -38,9 +45,14 @@ const ProductOnCart = ({ product, children }) => {
                             </>
                         )}
                     </small>
-                </p>{' '}
-                {children}
+                </p>
             </div>
+            <Button
+                className="ps-basket__content__items__item__remove"
+                style={{ cursor: 'pointer' }}
+                onClick={e => handleRemoveItem(e, item)}>
+                <i className="icon-cross"></i>
+            </Button>
         </div>
     );
 };
