1975708a1844973923bc9bcc0add8414003e6bfd
Mirsaid Mirakhmedov <mir21.07.2004@gmail.com>
2025-10-07 15:45:42 +0500

    update: open modal on grey card

--- widgets/gray-card/index.jsx
+++ widgets/gray-card/index.jsx
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './style.module.scss';
+import CreateOrderModal from '../../shared/components/modals/CreateOrderModal';
+import AuthModal from '~/components/AuthModal';
+import { useSelector } from 'react-redux';
+
 const GrayCard = ({
     title = '',
     btn = '',
-    goProducts = () => { },
+    goProducts = () => {},
     link = '',
 }) => {
+    const [open, setOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
+    const { isLoggedIn } = useSelector(state => state.auth);
+
+    const handleOrder = e => {
+        e.preventDefault();
+        if (isLoggedIn) {
+            setOpen(true);
+        } else {
+            setIsOpen(true);
+        }
+    };
     return (
         <section className={styles.readyProducts}>
             <div className={styles.block}>
                 <h1 className={styles.title}>{title}</h1>
                 <a
-                    onClick={goProducts}
+                    onClick={handleOrder}
                     href={link}
-                    target='_blank'
+                    target="_blank"
                     className={styles.catalogSeeAll}>
                     {btn}{' '}
                     <i
@@ -21,6 +37,14 @@ const GrayCard = ({
                         className="fa-solid fa-arrow-right"></i>
                 </a>
             </div>
+            <AuthModal
+                open={isOpen}
+                onClose={() => setIsOpen(false)}
+                onSuccess={() => {
+                    setOpen(true);
+                }}
+            />
+            <CreateOrderModal open={open} onClose={() => setOpen(false)} />
         </section>
     );
 };
