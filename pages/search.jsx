4f3b8fe05717faf212e34b57c063ec082fc1aea3
Mirsaid Mirakhmedov <mir21.07.2004@gmail.com>
2025-10-09 11:21:38 +0500

    fix: search page wrong navigation

--- pages/search.jsx
+++ pages/search.jsx
@@ -1,38 +1,18 @@
-import React from 'react'
-import { GetProducts } from '~/features/search/products/api/get-products'
-import { GetServices } from '~/features/search/services/api/get-services'
-import { GetSpecialists } from '~/features/search/specialists/api/get-specialists'
-
 const SearchPage = () => {
-    return (
-        <div>SearchPage</div>
-    )
-}
+    return null;
+};
 
-export default SearchPage
-
+export default SearchPage;
 
 export async function getServerSideProps(ctx) {
-    const { tab = "products", keyword = '', page = 1, limit = 20, offset = 0 } = ctx.query
-
-    let initialdata = null
-    if (tab === "products") {
-        initialdata = await GetProducts({ keyword, page })
-    } else if (tab === 'services') {
-        initialdata = await GetServices({ keyword, limit, offset })
-    } else if (tab === 'specialists') {
-        initialdata = await GetSpecialists({ keyword, limit, offset })
-    }
-    console.log("initialdata", initialdata)
-
     return {
-        props: {
-            tab,
-            keyword,
-            page,
-            limit,
-            offset,
-            initialdata
-        }
-    }
+        redirect: {
+            destination: `/search-page${
+                ctx.resolvedUrl.includes('?')
+                    ? ctx.resolvedUrl.substring(ctx.resolvedUrl.indexOf('?'))
+                    : ''
+            }`,
+            permanent: false, // Use 302 redirect (temporary)
+        },
+    };
 }
