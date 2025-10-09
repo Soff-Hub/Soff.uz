6baf24b2145d7b8d429e03cbec54381714f6a030
Mirsaid Mirakhmedov <mir21.07.2004@gmail.com>
2025-10-09 11:00:59 +0500

    fix: internal server error on products page

--- components/details-components/details-actions/tags.jsx
+++ components/details-components/details-actions/tags.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
+import React from 'react';
 import TagsComponents from './tagsComponents';
 
 function Tags({ tag }) {
     return (
         <div className="d-flex align-items-center gap-3 mb-3 flex-wrap mt-4">
-            {
-                tag.map((item, i) => (
+            {tag
+                ?.filter(item => item?.name && item.name.trim() !== '')
+                .map((item, i) => (
                     <TagsComponents key={i} name={item.name || item} />
-                ))
-            }
+                ))}
         </div>
-    )
+    );
 }
 
-export default Tags
+export default Tags;
