6ae0e1016467a1082f1b1530bc7c425c6a3e3fe4
Mirsaid Mirakhmedov <mir21.07.2004@gmail.com>
2025-10-09 17:10:18 +0500

    update: header create order to page navigation

--- widgets/header/HeaderCategories/index.jsx
+++ widgets/header/HeaderCategories/index.jsx
@@ -183,16 +183,16 @@ const templateIcons = {
 const HeaderCatergories = () => {
     const { isMobile } = useResponsive();
     const [open, setOpen] = useState(false);
-    const { isLoggedIn } = useSelector(state => state.auth);
-    const { directions } = useSelector(state => state.profile);
+    const { isLoggedIn } = useSelector((state) => state.auth);
+    const { directions } = useSelector((state) => state.profile);
     [...directions, { label: 'Boshqa', value: 'other' }];
     const { push, query, replace, pathname } = useRouter();
     const [openAuth, setOpenAuth] = useState(false);
 
     const handleOrder = () => {
         if (isLoggedIn) {
-            // push('/order/create');
-            setOpen(true);
+            push('/order/create');
+            // setOpen(true);
         } else {
             setOpenAuth(true);
         }
@@ -210,7 +210,7 @@ const HeaderCatergories = () => {
                 label: (
                     <a
                         href="/order/create"
-                        onClick={e => e.preventDefault()}
+                        onClick={(e) => e.preventDefault()}
                         className={` ${styles.dropLabel}  `}>
                         Maxsus buyurtma berish
                     </a>
@@ -221,7 +221,7 @@ const HeaderCatergories = () => {
                     borderRadius: '0px',
                 },
             },
-            ...directions.map(dir => ({
+            ...directions.map((dir) => ({
                 key: dir.value,
                 icon: templateIcons[dir.value]
                     ? templateIcons[dir.value]
@@ -260,7 +260,7 @@ const HeaderCatergories = () => {
             )}
             <div className={styles.dropBox}>
                 <Dropdown menu={{ items: products }}>
-                    <a onClick={e => e.preventDefault()}>
+                    <a onClick={(e) => e.preventDefault()}>
                         <Space className={styles.dropLabel}>
                             Mahsulotlar
                             <DownOutlined />
@@ -270,7 +270,7 @@ const HeaderCatergories = () => {
             </div>
             <div className={`${styles.orderBox} ${styles.dropBox}`}>
                 <Dropdown menu={{ items: templates }}>
-                    <a onClick={e => e.preventDefault()}>
+                    <a onClick={(e) => e.preventDefault()}>
                         <Space className={styles.dropLabel}>
                             Buyurtma berish
                             <DownOutlined />
