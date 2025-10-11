6983bc296ec2df825ac8e68bd745f44c8b6e368d
Mirsaid Mirakhmedov <mir21.07.2004@gmail.com>
2025-10-11 18:15:52 +0500

    update: telegram auth modal and fastDownload

--- components/AuthModal.jsx
+++ components/AuthModal.jsx
@@ -2,26 +2,38 @@ import React, { useState } from 'react';
 import { Modal } from 'antd';
 import LoginForm from '~/components/partials/account/auth/LoginForm.jsx';
 import CodeVerifyModal from '~/components/CodeVerifyModal';
+import TelegramCodeVerifyModal from '~/components/TelegramCodeVerifyModal.jsx';
 
 const AuthModal = ({ open, onClose, slug, onSuccess }) => {
     const [codeModalOpen, setCodeModalOpen] = useState(false);
+    const [telegramCodeModalOpen, setTelegramCodeModalOpen] = useState(false);
     const [authCode, setCode] = useState();
 
     const closeAllModals = () => {
         setCodeModalOpen(false);
+        setTelegramCodeModalOpen(false);
         onClose();
+    };
+
+    const handleSuccessOnTelegram = () => {
+        setTelegramCodeModalOpen(false);
+        onClose();
+        onSuccess();
     };
 
     return (
         <>
             <Modal
                 className="custom-auth-modal"
-                open={open && !codeModalOpen}
+                open={open && !codeModalOpen && !telegramCodeModalOpen}
                 onCancel={onClose}
                 footer={null}
                 centered>
                 <LoginForm
                     setCode={setCode}
+                    openTelegram={() => {
+                        setTelegramCodeModalOpen(true);
+                    }}
                     onSuccess={() => setCodeModalOpen(true)}
                     isModal={true}
                 />
@@ -33,6 +45,11 @@ const AuthModal = ({ open, onClose, slug, onSuccess }) => {
                 open={codeModalOpen}
                 onClose={closeAllModals}
                 slug={slug}
+            />
+            <TelegramCodeVerifyModal
+                onSuccess={handleSuccessOnTelegram}
+                open={telegramCodeModalOpen}
+                onClose={closeAllModals}
             />
         </>
     );
