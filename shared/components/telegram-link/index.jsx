81deda0a213e34c016dd870e1496f4c340151b84
Mirsaid Mirakhmedov <mir21.07.2004@gmail.com>
2025-10-15 14:22:01 +0500

    update: tg link button and my orders tab on topnavbar

--- shared/components/telegram-link/index.jsx
+++ shared/components/telegram-link/index.jsx
@@ -1,33 +1,20 @@
 import Link from 'next/link';
-import { useEffect, useState } from 'react';
-import styles from './style.module.scss'
+import styles from './style.module.scss';
+import useResponsive from '~/shared/utilities/useResponsive';
 
 export function TelegramLink() {
-    const [isMobile, setIsMobile] = useState(false);
-
-    useEffect(() => {
-        const handleResize = () => {
-            setIsMobile(window.innerWidth <= 576);
-        };
-
-        handleResize();
-        window.addEventListener('resize', handleResize);
-        return () => window.removeEventListener('resize', handleResize);
-    }, []);
-
-    if (isMobile) return null;
-
+    const { isMobile, isTablet } = useResponsive();
     return (
-        <div className={styles.telegramWrapper}>
+        <div
+            className={styles.telegramWrapper}
+            style={{
+                bottom: isTablet ? '60px' : isMobile ? '70px' : '20px',
+            }}>
             <Link href="https://t.me/+y5GpvEz48_hkMzli" passHref>
-                <a
-                    target="_blank"
-                    className={styles.telegramBtn}
-                >
+                <a target="_blank" className={styles.telegramBtn}>
                     <i className="fa-regular fa-paper-plane" />
                 </a>
             </Link>
         </div>
     );
 }
-    
