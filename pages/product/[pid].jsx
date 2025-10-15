df8457320c83199caad0be53b1eeaf2f71fc1ea2
Mirsaid Mirakhmedov <mir21.07.2004@gmail.com>
2025-10-15 12:32:47 +0500

    fix: product detail not opening problem

--- pages/product/[pid].jsx
+++ pages/product/[pid].jsx
@@ -43,10 +43,12 @@ export default function ProductDefaultPage({ defaultProducts }) {
     const { query } = router;
     const [isPlay, setIsPlay] = useState(null);
     const { isMobile } = useResponsive();
+    console.log({ defaultProducts });
 
     const similarRef = useRef();
 
     const contentType = defaultProducts?.document?.content_type;
+    console.log({ contentType });
 
     const { data: lastAdded, isLoading: lastLoading } = useQuery({
         queryKey: ['last-products', contentType],
@@ -372,12 +374,28 @@ export async function getServerSideProps({ query, req }) {
     const token = cookies.token;
 
     const headers = token ? { Authorization: `Bearer ${token}` } : {};
+    let defaultProducts = null;
+    try {
+        const resquest = await fetch(
+            `${baseUrl}customer/documents/${query.pid}/`,
+            {
+                headers,
+            }
+        );
 
-    const resquest = await fetch(`${baseUrl}customer/documents/${query.pid}/`, {
-        headers,
-    });
+        if (resquest.status === 403 || resquest.status === 401) {
+            throw new Error(
+                'Token invalid yoki muddati o‘tgan. Iltimos, qaytadan tizimga kiring.'
+            );
+        }
 
-    const defaultProducts = await resquest.json();
+        defaultProducts = await resquest.json();
+    } catch (error) {
+        const request = await fetch(
+            `${baseUrl}customer/documents/${query.pid}/`
+        );
+        defaultProducts = await request.json();
+    }
 
     return {
         props: {
