c6d5ed95ba626f6a76d1e865d59dd20d2fee1e8a
Mirsaid Mirakhmedov <mir21.07.2004@gmail.com>
2025-10-17 18:50:44 +0500

    fix: create category calendar problem

--- app/providers/AntdProvider.jsx
+++ app/providers/AntdProvider.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import { ConfigProvider } from 'antd';
 import uz from 'antd/locale/uz_UZ';
+import dayjs from 'dayjs';
+import 'dayjs/locale/uz-latn';
+import localeData from 'dayjs/plugin/localeData';
+
+// Configure dayjs
+dayjs.extend(localeData);
+dayjs.locale('uz-latn');
 
 const AntdProvider = ({ children }) => {
     return (
