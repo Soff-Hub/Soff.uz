73658ee239ee31a69a281ecd6abcf44a7a1d845c
Mirsaid Mirakhmedov <mir21.07.2004@gmail.com>
2025-11-05 17:00:04 +0500

    update: get back the Freelance component in index page

--- widgets/home/index.jsx
+++ widgets/home/index.jsx
@@ -25,7 +25,7 @@ const Home = () => {
             <Catalog />
             <div className="bg-white ">
                 <div className="container">
-                    {/* <Freelance /> */}
+                    <Freelance />
                     <LastServices />
                     <Info />
                 </div>
