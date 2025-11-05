2548d7bfd3a97c9289ee5d5f7e5f83aa072c833e
Mirsaid Mirakhmedov <mir21.07.2004@gmail.com>
2025-11-05 19:06:27 +0500

    update: ui changes on product detail

--- components/details-components/details-actions/tags.jsx
+++ components/details-components/details-actions/tags.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 import TagsComponents from './tagsComponents';
 
 function Tags({ tag }) {
+    const filteredTags = tag?.filter(
+        (item) => item?.name && item.name.trim() !== ''
+    );
+    if (!filteredTags || filteredTags.length === 0) {
+        return null;
+    }
     return (
         <div className="d-flex align-items-center gap-3 mb-3 flex-wrap mt-4">
             {tag
-                ?.filter(item => item?.name && item.name.trim() !== '')
+                ?.filter((item) => item?.name && item.name.trim() !== '')
                 .map((item, i) => (
                     <TagsComponents key={i} name={item.name || item} />
                 ))}
