d357e7f445c13f8e2377fd1ea11e42f451c28c6d
Mirsaid Mirakhmedov <mir21.07.2004@gmail.com>
2025-11-06 19:13:43 +0500

    update: footer ui

--- widgets/footer/index.jsx
+++ widgets/footer/index.jsx
@@ -1,119 +1,308 @@
-import React from "react";
-import styles from "./style.module.scss";
+import React, { useMemo } from 'react';
+import styles from './style.module.scss';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useFGet } from '~/shared/hooks/useFApi';
 
 const footerMenu = {
     soff: {
-        logoImg: "/static/img/soff.svg",
-        title: "Raqamli xizmatlar bozori!",
-        path: "#",
+        logoImg: '/static/img/soff.svg',
+        title: 'Raqamli xizmatlar bozori!',
+        path: '#',
     },
     services: {
-        title: "Aloqa",
+        title: 'Aloqa',
         links: [
-            { name: "Sotib olish va moderatsiya bo‘yicha", link: "" },
-            { name: "+998 (91) 008 67 89", link: "tel:+998910086789" },
-            { name: "@soff_moderator", link: "https://t.me/soff_moderator" },
-            { name: "Texnik muammolar uchun", link: "tel:+998910086789" },
-            { name: "@hr_soffhub", link: "https://t.me/hr_soffhub" },
+            { name: 'Sotib olish va moderatsiya bo‘yicha', link: '' },
+            { name: '+998 (91) 008 67 89', link: 'tel:+998910086789' },
+            { name: '@soff_moderator', link: 'https://t.me/soff_moderator' },
+            { name: 'Texnik muammolar uchun', link: 'tel:+998910086789' },
+            { name: '@hr_soffhub', link: 'https://t.me/hr_soffhub' },
         ],
     },
     social: {
-        title: "Ijtimoiy tarmoqlarimiz",
+        title: 'Ijtimoiy tarmoqlarimiz',
         items: [
             {
-                name: "Telegram",
-                icon: "/static/svg/telegram_.svg",
-                url: "https://t.me/+y5GpvEz48_hkMzli",
+                name: 'Telegram',
+                icon: '/static/svg/telegram_.svg',
+                url: 'https://t.me/+y5GpvEz48_hkMzli',
             },
             {
-                name: "FaceBook",
-                icon: "/static/img/faceb.png",
-                url: "https://www.facebook.com/profile.php?id=61579052952962#",
+                name: 'FaceBook',
+                icon: '/static/img/faceb.png',
+                url: 'https://www.facebook.com/profile.php?id=61579052952962#',
             },
             {
-                name: "Instagram",
-                icon: "/static/img/insta.png",
-                url: "https://www.instagram.com/soffuz_?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==",
+                name: 'Instagram',
+                icon: '/static/img/insta.png',
+                url: 'https://www.instagram.com/soffuz_?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==',
             },
-        ],
-    },
-    contactModeration: {
-        title: "Sotib olish va moderatsiya bo‘yicha:",
-        items: [
-            { contact: "Barcha xizmatlar", url: "/orders/?direction=scientific_work" },
-            { contact: "Buyurtma berish", url: "/orders" },
-            { contact: "Mahsulotlar", url: "/scientific-resources/all?slug=all" },
-            { contact: "Frilanserlar uchun", url: "https://seller.soff.uz/" },
         ],
     },
 };
 
+const products = [
+    {
+        key: '1',
+
+        icon: (
+            <Image
+                src={'/static/svg/book-saved.svg'}
+                alt=""
+                width={20}
+                height={20}
+            />
+        ),
+        link: '/scientific-resources/all?slug=all',
+        label: 'Ilmiy ishlar',
+    },
+    {
+        key: '2',
+        icon: (
+            <Image
+                src={'/static/svg/3dcube.svg'}
+                alt=""
+                width={20}
+                height={20}
+            />
+        ),
+        link: '/3d-models-and-interior-designs/all?slug=all',
+        label: '3D Dizayn va Vizualizatsiya',
+    },
+    {
+        key: '3',
+        link: '/design-developments/all?slug=all',
+        label: 'Dizayn shablonlari',
+        icon: (
+            <Image
+                src={'/static/svg/image.svg'}
+                alt=""
+                width={20}
+                height={20}
+            />
+        ),
+    },
+    {
+        key: '4',
+        icon: (
+            <Image
+                src={'/static/svg/chart.svg'}
+                alt=""
+                width={20}
+                height={20}
+            />
+        ),
+        link: '/templates/all?slug=all',
+        label: 'Turli sohalar uchun shablonlar',
+    },
+    {
+        key: '5',
+        icon: (
+            <Image
+                src={'/static/svg/video-square.svg'}
+                alt=""
+                width={20}
+                height={20}
+            />
+        ),
+        link: '/video-lessons/all?slug=all',
+        label: 'Video darsliklar',
+    },
+    {
+        key: '6',
+        icon: (
+            <Image
+                src={'/static/svg/monitor.svg'}
+                alt=""
+                width={20}
+                height={20}
+            />
+        ),
+        link: '/websites/all?slug=all',
+        label: 'Dasturlash xizmatlari',
+    },
+];
+
+const mainPages = [
+    { key: '1', link: '/', label: 'Bosh sahifa' },
+    { key: '2', link: '/search-page', label: 'Qidiruv' },
+    { key: '3', link: '/orders', label: 'Barcha xizmatlar' },
+    { key: '4', link: '/order/create', label: 'Yangi buyurtma yaratish' },
+    {
+        key: '5',
+        link: '/scientific-resources/all?slug=all',
+        label: 'Mahsulotlar',
+    },
+    { key: '6', link: 'https://seller.soff.uz', label: 'Frilanserlar uchun' },
+    { key: '7', link: '/affiliate_program', label: 'Hamkorlikda ishlash' },
+    // { key: '4', link: '/freelance', label: 'Frilanserlar' },
+    { key: '8', link: '/soffia', label: 'Soffia Bot' },
+    { key: '9', link: '/page/oferta', label: 'Foydalanish shartlari' },
+    { key: '10', link: '/page/video-list', lable: "Video qo'llanmalar" },
+];
+
+const aboutUsPages = [
+    { key: '1', link: '/page/about-us', label: 'Biz haqimizda' },
+    { key: '3', link: '/page/faq', label: 'Savollar (FAQ)' },
+    { key: '4', link: '/page/form', label: 'Talab va takliflar uchun' },
+];
+
 export default function Footer() {
+    const currentYear = new Date().getFullYear();
+    const { data: directionsData } = useFGet(
+        'directions',
+        'categories/all-directions'
+    );
+
+    const directions = useMemo(() => {
+        return (
+            directionsData?.map((elem) => ({
+                label: elem.title,
+                value: elem.value,
+            })) || []
+        );
+    }, [directionsData]);
+
     return (
-        <div className={styles.mainblock}>
-            <footer className="bg-transparent container p-lg-0 d-flex justify-content-between align-items-start flex-wrap">
-                {/* Asosiy shior */}
-                <a href={footerMenu.soff.path} className={styles.footerMainTitle}>
-                    Tayyor mahsulotlar va xizmatlar bir joyda
-                </a>
+        <footer className={`${styles.mainblock}`}>
+            <div className={styles.footerContent}>
+                <div className={styles.footerLogoSection}>
+                    <img
+                        src={`/static/img/soff_green.png`}
+                        alt="soff logo"
+                        style={{ width: '200px', height: 'auto' }}
+                    />
+                    <a
+                        href={footerMenu.soff.path}
+                        className={styles.footerMainTitle}
+                        style={{
+                            fontSize: '20px',
+                            marginTop: '24px',
+                            marginBottom: 0,
+                        }}>
+                        Tayyor mahsulotlar va xizmatlar bir joyda
+                    </a>
 
-                {/* Moderatsiya bo‘limi */}
-                <div className="col-xl-3 col-lg-3 col-md-6 col-sm-12 col-12 p-0 mb-5">
-                    {footerMenu.contactModeration.items.map((item) => (
-                        <a
-                            key={item.url}
-                            href={item.url}
-                            className="d-flex text-white align-items-center gap-3 footer-link fs-4 mb-3"
-                        >
-                            {item.contact}
-                        </a>
-                    ))}
+                    <div
+                        style={{
+                            display: 'flex',
+                            gap: '16px',
+                            alignItems: 'center',
+                            marginTop: '16px',
+                        }}>
+                        {footerMenu.social.items.map((item) => (
+                            <div key={item.url} className={styles.socialIcon}>
+                                <a
+                                    href={item.url}
+                                    target="_blank"
+                                    rel="noreferrer">
+                                    <img src={item.icon} alt={item.name} />
+                                </a>
+                            </div>
+                        ))}
+                    </div>
+                    {/* Xizmatlar (Aloqa) */}
+                    <ul className="mt-5">
+                        <h5 className="fw-semibold fs-2 text-white">
+                            {footerMenu.services.title}
+                        </h5>
+                        {footerMenu.services.links.map((link, i) =>
+                            link.link ? (
+                                <li key={link.link + i}>
+                                    <Link href={link.link}>
+                                        <a
+                                            target="_blank"
+                                            className="fs-4"
+                                            rel="noopener noreferrer">
+                                            {link.name}
+                                        </a>
+                                    </Link>
+                                </li>
+                            ) : (
+                                <li key={i} className="fs-4">
+                                    {link.name}
+                                </li>
+                            )
+                        )}
+                    </ul>
                 </div>
-
-                {/* Xizmatlar (Aloqa) */}
-                <div className="col-xl-3 text-white col-lg-3 col-md-6 p-0 col-sm-12 col-12">
-                    <h5 className="fw-semibold fs-2 text-white">
-                        {footerMenu.services.title}
-                    </h5>
-                    {footerMenu.services.links.map((link, i) =>
-                        link.link ? (
-                            <a
-                                key={link.link + i}
-                                href={link.link}
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                className="footer-link text-white d-block fs-4 mb-3"
-                            >
-                                {link.name}
-                            </a>
-                        ) : (
-                            <p key={i} className="text-white d-block fs-4 mb-3">
-                                {link.name}
-                            </p>
-                        )
-                    )}
-                </div>
-            </footer>
-
-            {/* Pastki qism */}
-            <div className="d-flex gap-4 justify-content-between mt-5 border-top pt-5 flex-wrap container">
-                <img
-                    src="/static/img/soff_green.png"
-                    alt="logo"
-                    className={styles.footerLogo}
-                />
-                <div className="d-flex gap-4 align-items-center">
-                    {footerMenu.social.items.map((item) => (
-                        <a key={item.url} href={item.url} target="_blank" rel="noreferrer">
-                            <img
-                                src={item.icon}
-                                alt={item.name}
-                                style={{ width: "40px", height: "40px" }}
-                            />
-                        </a>
-                    ))}
+                <div className={styles.footerLinksSection}>
+                    <ul>
+                        <h5 className="fw-semibold fs-2 text-white">
+                            Tayyor mahsulotlar
+                        </h5>
+                        {products.map((link) => (
+                            <li key={link.key}>
+                                <Link href={link.link}>
+                                    <a
+                                        className="fs-4"
+                                        rel="noopener noreferrer">
+                                        {link.label}
+                                    </a>
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                    <ul>
+                        <h5 className="fw-semibold fs-2 text-white">
+                            Xizmat turlari
+                        </h5>
+                        {directions.map((link) => (
+                            <li key={link.value}>
+                                <Link href={`/orders?direction=${link.value}`}>
+                                    <a
+                                        className="fs-4"
+                                        rel="noopener noreferrer">
+                                        {link.label}
+                                    </a>
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                    <ul>
+                        <h5 className="fw-semibold fs-2 text-white">
+                            Asosiy sahifalar
+                        </h5>
+                        {mainPages.map((link) => (
+                            <li key={link.key}>
+                                <Link href={link.link}>
+                                    <a
+                                        className="fs-4"
+                                        rel="noopener noreferrer">
+                                        {link.label}
+                                    </a>
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                    <ul>
+                        <h5 className="fw-semibold fs-2 text-white">
+                            Biz haqimizda
+                        </h5>
+                        {aboutUsPages.map((link) => (
+                            <li key={link.value}>
+                                <Link href={`/orders?direction=${link.value}`}>
+                                    <a
+                                        className="fs-4"
+                                        rel="noopener noreferrer">
+                                        {link.label}
+                                    </a>
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
                 </div>
             </div>
-        </div>
+            <div className="d-flex gap-4 justify-content-between mt-5 border-top pt-4 flex-wrap">
+                <span>
+                    © {currentYear} Soff.uz — Barcha huquqlar himoyalangan.
+                </span>
+
+                <Link href="/page/oferta">
+                    <a>® Terms | Privacy</a>
+                </Link>
+            </div>
+        </footer>
     );
 }
