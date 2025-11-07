68cddde40547601cc9bf4b0cf8ddea970ab82fa8
Mirsaid Mirakhmedov <mir21.07.2004@gmail.com>
2025-11-07 12:11:56 +0500

    update: auth customer links made static

--- pages/tgLoader.jsx
+++ pages/tgLoader.jsx
@@ -3,114 +3,7 @@ import React from 'react';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { PacmanLoader } from 'react-spinners';
-import { begin, login, setAccountLinks } from '~/store/auth/slice';
-
-export let accountAdminLinks = [
-    {
-        text: 'Boshqaruv paneli',
-        url: '/account/dashbord',
-        icon: 'fa-solid fa-house-user',
-    },
-    {
-        text: 'Sotuvchilar',
-        url: '/account/shops',
-        icon: 'fa-solid fa-shop',
-    },
-    {
-        text: 'Mahsulotlar',
-        url: '/account/products',
-        icon: 'fa-solid fa-cube',
-    },
-    {
-        text: 'Kategoriyalar',
-        url: '/account/category',
-        icon: 'fa-solid fa-layer-group',
-    },
-    {
-        text: 'Buyurtmalar',
-        url: '/account/orders',
-        icon: 'fa-solid fa-truck',
-    },
-    {
-        text: 'Xaridorlar',
-        url: '/account/users',
-        icon: 'fa-solid fa-users',
-    },
-    {
-        text: 'Taglar',
-        url: '/account/tegs',
-        icon: 'fa-solid fa-tags',
-    },
-    {
-        text: 'Ariza va Takliflar',
-        url: '/account/application',
-        icon: 'fa-solid fa-file-signature',
-    },
-    {
-        text: 'Context',
-        url: '/account/context',
-        icon: 'fa-solid fa-sliders',
-    },
-    {
-        text: 'Sozlamalar',
-        url: '/account/settings',
-        icon: 'fa-solid fa-gear',
-    },
-];
-export let accountSellerLink = [
-    {
-        text: 'Boshqaruv paneli',
-        url: '/account/dashbord',
-        icon: 'fa-solid fa-house-user',
-    },
-    {
-        text: 'Mening mahsulotlarim',
-        url: '/account/myproducts',
-        icon: 'fa-solid fa-shop-lock',
-    },
-    {
-        text: 'Sotib olinganlar',
-        url: '/account/sellerproducts',
-        icon: 'fa-solid fa-bag-shopping',
-    },
-    {
-        text: 'Yangi mahsulot',
-        url: '/account/myproducts/posts',
-        icon: 'fa-solid fa-circle-plus',
-    },
-    {
-        text: 'Olingan buyurtmalar',
-        url: '#',
-        icon: 'fa-solid fa-folder-open',
-    },
-    {
-        text: 'Buyurtmalar',
-        url: '/account/orders',
-        icon: 'fa-solid fa-truck',
-    },
-    {
-        text: 'Ariza va Takliflar',
-        url: '/account/application',
-        icon: 'fa-solid fa-file-signature',
-    },
-    {
-        text: 'Profil',
-        url: '/account/settings',
-        icon: 'fa-solid fa-gear',
-    },
-];
-export let cutomerAccountLink = [
-    {
-        text: 'Mening mahsulotlarim',
-        url: '/account/myproducts',
-        icon: 'fa-solid fa-shop-lock',
-    },
-    {
-        text: 'Profil',
-        url: '/account/settings',
-        icon: 'fa-solid fa-gear',
-    },
-];
+import { begin, login } from '~/store/auth/slice';
 
 const TgLoader = () => {
     const { user } = useSelector((state) => state.auth);
@@ -176,8 +69,6 @@ const TgLoader = () => {
         if (user?.role === 'admin') {
             localStorage.setItem('is_seller', '1');
         }
-
-        dispatch(setAccountLinks(cutomerAccountLink));
 
         // Only redirect if we're not already processing a token
         if (!asPath.includes('token=') && user?.role) {
--- store/auth/slice.js
+++ store/auth/slice.js
@@ -6,7 +6,18 @@ import Cookies from 'js-cookie';
 const initialState = {
     isLoggedIn: false,
     user: null,
-    accountLinks: [],
+    accountLinks: [
+        {
+            text: 'Sotib olinganlar',
+            url: '/account/sellerproducts',
+            icon: 'fa-solid fa-bag-shopping',
+        },
+        {
+            text: 'Buyurtmalarim',
+            url: '/order/my-orders',
+            icon: 'fas fa-truck',
+        },
+    ],
     data: {},
     products: {},
     shop: [],
