43e87f05f23e5ebf3c1e9b0b7bf9b973f446a2b5
Mirsaid Mirakhmedov <mir21.07.2004@gmail.com>
2025-11-08 11:15:49 +0500

    update: making freelance rankings on main page

--- widgets/header/HeaderCategories/index.jsx
+++ widgets/header/HeaderCategories/index.jsx
@@ -25,6 +25,8 @@ import useDebounce from '~/shared/hooks/useDebounce';
 import axiosInstance from '~/shared/api/freeleanceApi';
 import searchStyle from '../navbar-search/style.module.scss';
 import { FiExternalLink } from 'react-icons/fi';
+import { GrBook } from 'react-icons/gr';
+import { directionsImg } from '~/shared/constants/directions-img';
 
 const products = [
     {
@@ -260,12 +262,12 @@ const HeaderCatergories = () => {
             },
             ...directions.map((dir) => ({
                 key: dir.value,
-                icon: templateIcons[dir.value]
-                    ? templateIcons[dir.value]
-                    : templateIcons['not_found'],
+                icon: directionsImg[dir.value]
+                    ? directionsImg[dir.value]
+                    : directionsImg['not_found'],
                 label: (
                     <a
-                        className={`ms-3 ${styles.dropLabel}`}
+                        className={`${styles.dropLabel}`}
                         href={`/orders?direction=${dir.value}`}>
                         {dir.label}
                     </a>
--- widgets/home/freelance/index.jsx
+++ widgets/home/freelance/index.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import styles from './style.module.scss';
 import cardStyle from './service.module.scss';
 import Link from 'next/link';
+import { IoIosArrowBack } from 'react-icons/io';
+import { Button, Skeleton } from 'antd';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperController from '~/widgets/navbar-menu/swiperController';
+import 'swiper/css';
+import { useGetDirectionsQuery } from '~/store/profile/slice';
+import { directionsImg } from '~/shared/constants/directions-img';
+import { useFGet } from '~/shared/hooks/useFApi';
+import SearchSellerCard from '~/entities/seller/search-seller-card';
 
 const items = [
     {
@@ -59,6 +68,7 @@ const link = {
 const Freelance = () => {
     return (
         <>
+            <TopFreelanceRankings />
             <div className={styles.freelance_section}>
                 <div className={styles.freelance_text}>
                     <div className="d-flex  gap-2 flex-fill">
@@ -107,6 +117,213 @@ const Freelance = () => {
 };
 
 export default Freelance;
+
+const TopFreelanceRankings = () => {
+    const [isEnd, setIsEnd] = useState(false);
+    const [isBeginning, setIsBeginning] = useState(true); // Start with true as initial state
+    const [swiperController, setSwiperController] = useState(null);
+    const [rankingCategory, setRankingCategory] = useState(null);
+    const swiperRef = useRef(null);
+
+    const { data: directions } = useGetDirectionsQuery();
+    const {
+        data: sellersData,
+        isSuccess,
+        isLoading,
+    } = useFGet(
+        ['customer/sellers', rankingCategory],
+        `users/sellers?limit=3&offset=0${
+            rankingCategory ? `&direction=${rankingCategory}` : ''
+        }`
+    );
+
+    console.log({ sellersData });
+
+    const getMethods = (swiperClass) => {
+        setSwiperController(swiperClass);
+    };
+
+    const setEnding = (status) => {
+        setIsEnd(status);
+    };
+    const setBeginning = (status) => {
+        setIsBeginning(status);
+    };
+
+    // Function to update swiper state
+    const updateSwiperState = (swiper) => {
+        if (swiper) {
+            setIsEnd(swiper.isEnd);
+            setIsBeginning(swiper.isBeginning);
+        }
+    };
+
+    // Handle swiper initialization
+    const handleSwiperInit = (swiper) => {
+        // Set initial state
+        updateSwiperState(swiper);
+    };
+
+    // Handle slide change
+    const handleSlideChange = (swiper) => {
+        updateSwiperState(swiper);
+    };
+
+    const showResults =
+        Array.isArray(sellersData?.results) && sellersData?.results?.length;
+
+    let resultsContent = null;
+    if (isLoading) {
+        resultsContent = (
+            <div className="d-flex gap-3" style={{ marginTop: '30px' }}>
+                {Array(4)
+                    .fill(0)
+                    .map((_, i) => (
+                        <Skeleton
+                            key={i}
+                            active
+                            className="Search_Results_Wrap_skeleton"
+                        />
+                    ))}
+            </div>
+        );
+    } else if (showResults) {
+        resultsContent = (
+            <>
+                <div className="Search_Results_Specialists_Wrap">
+                    {sellersData.results.map((item) => (
+                        <SearchSellerCard
+                            seller={item}
+                            key={item?.soff_seller_id}
+                        />
+                    ))}
+                </div>
+            </>
+        );
+    } else {
+        resultsContent = <Search_Results_NotFound ref={notFoundRef} />;
+    }
+
+    useEffect(() => {
+        if (swiperRef.current?.swiper) {
+            const swiper = swiperRef.current.swiper;
+
+            // Set initial state
+            updateSwiperState(swiper);
+
+            // Add event listeners
+            swiper.on('slideChange', () => updateSwiperState(swiper));
+            swiper.on('reachEnd', () => setIsEnd(true));
+            swiper.on('reachBeginning', () => setIsBeginning(true));
+            swiper.on('fromEdge', () => {
+                updateSwiperState(swiper);
+            });
+
+            // Cleanup function
+            return () => {
+                swiper.off('slideChange');
+                swiper.off('reachEnd');
+                swiper.off('reachBeginning');
+                swiper.off('fromEdge');
+            };
+        }
+    }, []);
+
+    useEffect(() => {
+        if (isSuccess && !rankingCategory) {
+            setRankingCategory(directions?.[0]?.value || null);
+        }
+    }, [isSuccess]);
+
+    return (
+        <div className={styles.topFreelanceRankings}>
+            <div
+                className={styles.freelance_section}
+                style={{
+                    paddingBottom: '20px',
+                }}>
+                <div className={styles.freelance_text}>
+                    <div className="d-flex gap-2 flex-fill">
+                        <div className="d-none d-md-flex">
+                            <img
+                                src={'/static/img/star.svg'}
+                                width={30}
+                                height={30}
+                                alt="starts"
+                            />
+                        </div>
+                        <div className={styles.titleWrapper}>
+                            <h2 className={styles.labelWrapperH1}>
+                                Top frilanser va sotuvchilar reytingi
+                            </h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.swiperContainer}>
+                <Button
+                    aria-label="previous"
+                    size="large"
+                    onClick={() => swiperController?.slidePrev()}
+                    style={{
+                        display: isBeginning ? 'none' : 'flex',
+                    }}
+                    className={styles.swipe_btn}
+                    disabled={isBeginning}>
+                    <IoIosArrowBack />
+                </Button>
+                <Swiper
+                    ref={swiperRef}
+                    spaceBetween={20}
+                    navigation={false}
+                    freeMode={true}
+                    slidesPerView={'auto'}
+                    onSwiper={handleSwiperInit}
+                    onSlideChange={handleSlideChange}
+                    className={` ${!isEnd && styles.categorySwiperEnding} ${
+                        !isBeginning && styles.categorySwiperBeginning
+                    }`}>
+                    {directions?.map((item) => (
+                        <SwiperSlide key={item.value}>
+                            <Button
+                                icon={directionsImg[item.value]}
+                                type="text"
+                                size="large"
+                                className={
+                                    item.value === rankingCategory
+                                        ? styles['categoryButton--selected']
+                                        : styles['categoryButton--ordinary']
+                                }
+                                onClick={() => setRankingCategory(item.value)}>
+                                <span className={styles.rankingCardText}>
+                                    {item.label}
+                                </span>
+                            </Button>
+                        </SwiperSlide>
+                    ))}
+
+                    <SwiperController
+                        getMethods={getMethods}
+                        setEnding={setEnding}
+                        setBeginning={setBeginning}
+                    />
+                </Swiper>
+                <Button
+                    aria-label="next"
+                    size="large"
+                    style={{
+                        display: isEnd ? 'none' : 'flex',
+                    }}
+                    className={styles.swipeNext}
+                    onClick={() => swiperController?.slideNext()}
+                    disabled={isEnd}>
+                    <IoIosArrowBack />
+                </Button>
+            </div>
+            {resultsContent}
+        </div>
+    );
+};
 
 const ServiceCard = ({ content_type = 'file', items = {} }) => {
     return (
