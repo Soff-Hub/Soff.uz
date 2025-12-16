bb9b08303ce3471611dd0d16bb9dbcc032e8a5d3
Mirsaid Mirakhmedov <mir21.07.2004@gmail.com>
2025-12-16 23:03:28 +0500

    update: uzbek lang support in antd

--- app/providers/AntdProvider.jsx
+++ app/providers/AntdProvider.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ConfigProvider, message } from 'antd';
+import uzUZ from '~/app/locales/uz_UZ';
+import dayjs from 'dayjs';
+import 'dayjs/locale/uz-latn';
 
 message.config({ duration: 6 });
 
 const AntdProvider = ({ children }) => {
+    useEffect(() => {
+        // Ensure dayjs locale is set for DatePicker components
+        dayjs.locale('uz-latn');
+    }, []);
+
     return (
         <ConfigProvider
+            locale={uzUZ}
             theme={{
                 token: {
                     colorPrimary: '#00a44f',
