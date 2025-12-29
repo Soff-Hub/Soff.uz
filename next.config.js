8202f580127ce940b665337de0fb9964bab2b74c
Mirsaid Mirakhmedov <mir21.07.2004@gmail.com>
2025-12-29 17:24:19 +0500

    update: latest changes

--- next.config.js
+++ next.config.js
@@ -4,16 +4,11 @@ const nextSettings = {
     eslint: {
         ignoreDuringBuilds: true,
     },
-    // Enable detailed hydration error logging
     experimental: {
         logging: {
             level: 'verbose',
         },
     },
-    // Note: optimizePackageImports is Next.js 13+ only
-    // For Next.js 12, tree shaking works automatically with named imports
-    // Your imports are already correct: import { Button } from 'antd'
-    // Show more detailed hydration mismatches
     reactStrictMode: true,
     env: {
         title: 'Soff uz',
@@ -116,36 +111,36 @@ module.exports = nextSettings;
 
 // Injected content via Sentry wizard below
 
-const { withSentryConfig } = require('@sentry/nextjs');
+// const { withSentryConfig } = require('@sentry/nextjs');
 
-module.exports = withSentryConfig(module.exports, {
-    // For all available options, see:
-    // https://www.npmjs.com/package/@sentry/webpack-plugin#options
+// module.exports = withSentryConfig(module.exports, {
+//     // For all available options, see:
+//     // https://www.npmjs.com/package/@sentry/webpack-plugin#options
 
-    org: 'for-personal-use-jw',
-    project: 'javascript-nextjs',
+//     org: 'for-personal-use-jw',
+//     project: 'javascript-nextjs',
 
-    // Only print logs for uploading source maps in CI
-    silent: !process.env.CI,
+//     // Only print logs for uploading source maps in CI
+//     silent: !process.env.CI,
 
-    // For all available options, see:
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+//     // For all available options, see:
+//     // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
-    // Upload a larger set of source maps for prettier stack traces (increases build time)
-    widenClientFileUpload: true,
+//     // Upload a larger set of source maps for prettier stack traces (increases build time)
+//     widenClientFileUpload: true,
 
-    // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-    // This can increase your server load as well as your hosting bill.
-    // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-    // side errors will fail.
-    tunnelRoute: '/monitoring',
+//     // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+//     // This can increase your server load as well as your hosting bill.
+//     // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+//     // side errors will fail.
+//     tunnelRoute: '/monitoring',
 
-    // Automatically tree-shake Sentry logger statements to reduce bundle size
-    disableLogger: true,
+//     // Automatically tree-shake Sentry logger statements to reduce bundle size
+//     disableLogger: true,
 
-    // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-    // See the following for more information:
-    // https://docs.sentry.io/product/crons/
-    // https://vercel.com/docs/cron-jobs
-    automaticVercelMonitors: true,
-});
+//     // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+//     // See the following for more information:
+//     // https://docs.sentry.io/product/crons/
+//     // https://vercel.com/docs/cron-jobs
+//     automaticVercelMonitors: true,
+// });
