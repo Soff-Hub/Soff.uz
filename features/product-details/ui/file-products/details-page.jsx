c7788130827b655eb52df8e3cab2fc7aa8d37456
Mirsaid Mirakhmedov <mir21.07.2004@gmail.com>
2026-01-15 15:19:06 +0500

    update: comments and redesign

--- features/product-details/ui/file-products/details-page.jsx
+++ features/product-details/ui/file-products/details-page.jsx
@@ -5,7 +5,7 @@ import SellerProfile from '../seller-profile/seller-profile';
 import Description from '../actions/description';
 import Tags from '../actions/tags';
 import Link from 'next/link';
-import { CommentList } from '~/features/comments/ui/commentList';
+import CommentList from '~/components/product-comments';
 import CommentFormWrapper from '~/features/comments/ui/commentWrapper';
 
 function FileProductsDetails({ product }) {
@@ -54,7 +54,7 @@ function FileProductsDetails({ product }) {
 
                         <SellerProfile product={product} />
                     </div>
-                    <div>
+                    <div style={{ marginBottom: '32px' }}>
                         <CommentFormWrapper
                             id={product.id}
                             slug={product.slug}
